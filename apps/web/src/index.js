f869f748fb936e6a332c3e820fd98adc51ae6cdf
thecodrr <thecodrr@protonmail.com>
2019-11-27 18:03:01 +0500

    api: move storage to interfaces

--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Storage from "notes-core/sync/storage";
+import StorageInterface from "./interfaces/storage";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export const storage = new Storage(StorageInterface);
