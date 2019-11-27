57894e72430427c6ba2b20af5512db9ea056f201
thecodrr <thecodrr@protonmail.com>
2019-11-27 17:29:24 +0500

    add storage interface for storage api

---
+++ apps/web/src/utils/storage.js
@@ -0,0 +1,23 @@
+async function read(key) {
+  return new Promise((resolve, reject) =>
+    resolve(window.localStorage.getItem(key))
+  );
+}
+async function write(key, data) {
+  return new Promise((resolve, reject) =>
+    resolve(window.localStorage.setItem(key, data))
+  );
+}
+function remove(key) {
+  window.localStorage.removeItem(key);
+}
+function clear() {
+  window.localStorage.clear();
+}
+
+export default {
+  read,
+  write,
+  remove,
+  clear
+};
