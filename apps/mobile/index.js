d11ba83c93854831262652bfdc39149157118b25
ammarahm-ed <ammarahmed6506@gmail.com>
2020-01-16 19:54:26 +0500

    add Provider for global state

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -3,7 +3,17 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App, {db} from './App';
 import {name as appName} from './app.json';
+import React, {useEffect} from 'react';
+import {Provider} from './src/provider';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppProvider = () => {
+  return (
+    <Provider>
+      <App />
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => AppProvider);
