b350f60f9df47cbd72c0539cfdacbb5187d90870
ammarahm-ed <ammarahmed6506@gmail.com>
2020-01-22 02:48:30 +0500

    add volume button debugmode

--- apps/mobile/android/app/src/main/java/com/notes/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/notes/MainActivity.java
@@ -1,13 +1,22 @@
 package com.notes;
 
 import com.facebook.react.ReactActivity;
-
+import android.view.KeyEvent;
 public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+    @Override
+  public boolean onKeyUp(int keyCode, KeyEvent event) {
+      if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && this.getReactInstanceManager() != null) {
+          this.getReactInstanceManager().showDevOptionsDialog();
+          return true;
+      }
+      return super.onKeyUp(keyCode, event);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "Notes";
