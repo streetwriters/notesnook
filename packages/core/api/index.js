979de6faf070c4d5f00672a99b8a3ca63b581044
Waqar Ahmed <waqar.17a@gmail.com>
2020-02-21 21:39:27 +0500

    Feat: expose tags/colors in api

--- packages/core/api/index.js
+++ packages/core/api/index.js
@@ -1,6 +1,7 @@
 import Notes from "../collections/notes";
 import Notebooks from "../collections/notebooks";
 import Trash from "../collections/trash";
+import Tags from "../collections/tags";
 import User from "../models/user";
 import Sync from "./sync";
 
@@ -13,9 +14,13 @@ class Database {
     this.notes = new Notes(this.context);
     this.trash = new Trash(this.context);
     this.user = new User(this.context);
+    this.tags = new Tags(this.context, "tags");
+    this.colors = new Tags(this.context, "colors");
     await this.notes.init(this.notebooks, this.trash);
     await this.notebooks.init(this.notes, this.trash);
     await this.trash.init(this.notes, this.notebooks);
+    await this.tags.init();
+    await this.colors.init();
     this.syncer = new Sync(this);
   }
 
