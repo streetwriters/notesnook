38485f9cd45ea02d26810639b50c17320f6f2ac0
thecodrr <thecodrr@protonmail.com>
2020-02-29 11:09:38 +0500

    refactor: use subtle.crypto instead of CryptoJS

--- apps/web/src/interfaces/storage.js
+++ apps/web/src/interfaces/storage.js
@@ -1,5 +1,8 @@
 import localforage from "localforage";
-import CryptoJS from "crypto-js";
+import { encode, decode } from "../utils/base64";
+import { hexToBuffer, bufferToHex } from "../utils/hex";
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
 
 localforage.config({
   name: "Notesnook",
@@ -19,42 +22,32 @@ function clear() {
   return localforage.clear();
 }
 
-function encrypt(password, data) {
-  return getPBKDF2Key(password, "salt")
-    .then(key =>
-      CryptoJS.AES.encrypt(data, key, {
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-      })
-    )
-    .then(encryptedData => ({
-      cipher: encryptedData.ciphertext.toString(CryptoJS.enc.Base64),
-      iv: encryptedData.iv.toString(CryptoJS.enc.Hex)
-    }));
+const randArray = new Uint8Array(16);
+async function encrypt(password, data) {
+  var algo = {
+    name: "AES-CBC",
+    length: 256,
+    iv: crypto.getRandomValues(randArray)
+  };
+  var key = await getPBKDF2Key(password, "salt");
+  var encoded = encoder.encode(data);
+  var cipher = await crypto.subtle.encrypt(algo, key, encoded);
+  return {
+    cipher: encode(cipher),
+    iv: bufferToHex(algo.iv)
+  };
 }
 
-function decrypt(password, data) {
-  return getPBKDF2Key(password, "salt")
-    .then(key =>
-      CryptoJS.AES.decrypt(data.cipher, key, {
-        iv: CryptoJS.enc.Hex.parse(data.iv),
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-      })
-    )
-    .then(decryptedText => decryptedText.toString(CryptoJS.enc.Utf8));
-}
+async function decrypt(password, data) {
+  var algo = {
+    name: "AES-CBC",
+    length: 256,
+    iv: hexToBuffer(data.iv)
+  };
+  var key = await getPBKDF2Key(password, "salt");
+  var decrypted = await crypto.subtle.decrypt(algo, key, decode(data.cipher));
 
-const params = {
-  keySize: 256 / 32,
-  hasher: CryptoJS.algo.SHA512,
-  iterations: 5000
-};
-
-function getPBKDF2Key(password, salt) {
-  return new Promise(resolve =>
-    resolve(CryptoJS.PBKDF2(password, salt, params))
-  );
+  return decoder.decode(decrypted);
 }
 
 export default {
@@ -65,3 +58,26 @@ export default {
   encrypt,
   decrypt
 };
+
+async function getPBKDF2Key(password, salt) {
+  var algo = {
+    name: "PBKDF2",
+    hash: "SHA-512",
+    salt: encoder.encode(salt),
+    iterations: 5000
+  };
+  var derived = { name: "AES-CBC", length: 256 };
+  var encoded = encoder.encode(password);
+  var key = await crypto.subtle.importKey(
+    "raw",
+    encoded,
+    { name: "PBKDF2" },
+    false,
+    ["deriveKey"]
+  );
+
+  return crypto.subtle.deriveKey(algo, key, derived, false, [
+    "encrypt",
+    "decrypt"
+  ]);
+}
