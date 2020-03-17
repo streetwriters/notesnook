e8a33e58d6e8a3dcbc461d9cb4cc91a92696b128
thecodrr <thecodrr@protonmail.com>
2020-03-17 15:41:01 +0500

    sync: change url

--- packages/core/utils/constants.js
+++ packages/core/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = "http://localhost:4000/";
+export const HOST = "https://notesnook.azurewebsites.net/";
 export const HEADERS = {
   agent: "nn/1.0.0",
   "Content-Type": "application/json"
