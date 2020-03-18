7bdbde60ae3fe98723f6023c7fd3a2790aed3be7
thecodrr <thecodrr@protonmail.com>
2020-03-18 14:06:51 +0500

    chore: change ip

--- packages/core/utils/constants.js
+++ packages/core/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = "https://notesnook.azurewebsites.net/";
+export const HOST = "http://192.168.10.11:4000/";
 export const HEADERS = {
   agent: "nn/1.0.0",
   "Content-Type": "application/json"
