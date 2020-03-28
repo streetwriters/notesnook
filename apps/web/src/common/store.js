0e9fcaf54255f3b77ec47dfbb0a24ccacde35690
thecodrr <thecodrr@protonmail.com>
2020-03-28 13:51:18 +0500

    feat: make zustand store logic ready for classes

--- apps/web/src/common/store.js
+++ apps/web/src/common/store.js
@@ -1,13 +1,25 @@
-import produce from "immer";
+import produce, { immerable } from "immer";
 import create from "zustand";
 
 function immer(config) {
   return function(set, get, api) {
-    return config(fn => set(produce(fn)), get, api);
+    const obj = config(
+      fn =>
+        set(() =>
+          produce(get(), state => {
+            fn(state);
+          })
+        ),
+      get,
+      api
+    );
+    obj[immerable] = true;
+    return obj;
   };
 }
 
 function createStore(store) {
+  store = store.new ? store.new.bind(store) : store;
   return create(immer(store));
 }
 
