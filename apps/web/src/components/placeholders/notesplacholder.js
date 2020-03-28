24f559c4318d96cd4a834b953957d279743a097d
thecodrr <thecodrr@protonmail.com>
2020-03-28 23:44:01 +0500

    ui: make notesplaceholder cleaner

--- apps/web/src/components/placeholders/notesplacholder.js
+++ apps/web/src/components/placeholders/notesplacholder.js
@@ -2,18 +2,14 @@ import React from "react";
 import { Text, Flex, Box } from "rebass";
 import Placeholder from "./index";
 import * as Icon from "../icons";
-import { getRandom } from "../../utils/random";
 
-const colors = ["hover", "primary", "dimPrimary", "shade"];
-const icons = [Icon.Vault, Icon.Pin, Icon.Star];
-const titles = ["Assignment #4", "Git Workflow", "Project Aurora"];
-const words = Array(25).fill(0);
+const icons = [Icon.Vault, Icon.Star];
+const lines = Array(5).fill(0);
 
 function NotesPlaceholder() {
   return (
     <Placeholder
-      items={titles}
-      label="Notes you write will appear here"
+      items={[0, 1]}
       renderItem={(_, index) => {
         const Icon = icons[index];
         return (
@@ -28,22 +24,23 @@ function NotesPlaceholder() {
               borderRadius: "default"
             }}
           >
+            <Flex flex="0.06 1 auto" bg="primary" sx={{ borderRadius: 25 }} />
             <Flex flex="1 1 auto" flexDirection="column" p={1}>
               <Flex alignItems="center" justifyContent="space-between">
                 <Text variant="title" color="primary" mb={1}>
-                  {titles[index]}
+                  Title
                 </Text>
                 <Icon size={20} color="dimPrimary" />
               </Flex>
-              <Flex flexWrap="wrap">
-                {words.map(() => (
+              <Flex flexDirection="column">
+                {lines.map(() => (
                   <Box
-                    width={getRandom(20, 50)}
+                    width={"100%"}
                     py={"4px"}
                     mr={1}
                     my={"3px"}
                     sx={{ borderRadius: "25px" }}
-                    bg={colors[getRandom(0, 3)]}
+                    bg={"dimPrimary"}
                   />
                 ))}
               </Flex>
