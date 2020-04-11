8912c35509208acfbd7d91991c5339a612c58953
thecodrr <thecodrr@protonmail.com>
2020-04-11 17:06:17 +0500

    feat: remove crypto apis from frontend

--- apps/web/src/interfaces/storage.js
+++ apps/web/src/interfaces/storage.js
@@ -1,16 +1,12 @@
 import localforage from "localforage";
 import { extendPrototype } from "localforage-getitems";
-import { encode, decode } from "../utils/base64";
-import { hexToBuffer, bufferToHex } from "../utils/hex";
 import sort from "fast-sort";
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
 
 extendPrototype(localforage);
 
 localforage.config({
   name: "Notesnook",
-  driver: [localforage.INDEXEDDB, localforage.WEBSQL, localforage.LOCALSTORAGE]
+  driver: [localforage.INDEXEDDB, localforage.WEBSQL, localforage.LOCALSTORAGE],
 });
 
 function read(key) {
@@ -34,63 +30,10 @@ function clear() {
   return localforage.clear();
 }
 
-const randArray = new Uint8Array(16);
-async function encrypt(password, data) {
-  var algo = {
-    name: "AES-CBC",
-    length: 256,
-    iv: crypto.getRandomValues(randArray)
-  };
-  var key = await getPBKDF2Key(password, "salt");
-  var encoded = encoder.encode(data);
-  var cipher = await crypto.subtle.encrypt(algo, key, encoded);
-  return {
-    cipher: encode(cipher),
-    iv: bufferToHex(algo.iv)
-  };
-}
-
-async function decrypt(password, data) {
-  var algo = {
-    name: "AES-CBC",
-    length: 256,
-    iv: hexToBuffer(data.iv)
-  };
-  var key = await getPBKDF2Key(password, "salt");
-  var decrypted = await crypto.subtle.decrypt(algo, key, decode(data.cipher));
-
-  return decoder.decode(decrypted);
-}
-
 export default {
   read,
   readMulti,
   write,
   remove,
   clear,
-  encrypt,
-  decrypt
 };
-
-async function getPBKDF2Key(password, salt) {
-  var algo = {
-    name: "PBKDF2",
-    hash: "SHA-512",
-    salt: encoder.encode(salt),
-    iterations: 5000
-  };
-  var derived = { name: "AES-CBC", length: 256 };
-  var encoded = encoder.encode(password);
-  var key = await crypto.subtle.importKey(
-    "raw",
-    encoded,
-    { name: "PBKDF2" },
-    false,
-    ["deriveKey"]
-  );
-
-  return crypto.subtle.deriveKey(algo, key, derived, false, [
-    "encrypt",
-    "decrypt"
-  ]);
-}
