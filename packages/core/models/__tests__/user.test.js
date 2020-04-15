10786002904a24a028757069fff6b4391260dc9e
thecodrr <thecodrr@protonmail.com>
2020-04-16 03:41:43 +0500

    ci: fix user tests

--- packages/core/models/__tests__/user.test.js
+++ packages/core/models/__tests__/user.test.js
@@ -1,6 +1,6 @@
 import User from "../user";
 import { enableFetchMocks, disableFetchMocks } from "jest-fetch-mock";
-import StorageInterface from "../../__mocks__/storage.mock";
+import { StorageInterface, databaseTest } from "../../__tests__/utils/index";
 
 const SUCCESS_LOGIN_RESPONSE = {
   access_token: "access_token",
@@ -22,130 +22,130 @@ beforeEach(() => {
   StorageInterface.clear();
 });
 
-test("no user should be returned when not logged in", async () => {
-  const user = new User(StorageInterface);
-  expect(await user.get()).toBeUndefined();
-});
+test("no user should be returned when not logged in", () =>
+  databaseTest().then(async (db) => {
+    expect(await db.user.get()).toBeUndefined();
+  }));
 
-test("undefined user should not be set", async () => {
-  const user = new User(StorageInterface);
-  await user.set();
-  expect(await user.get()).toBeUndefined();
-});
+test("undefined user should not be set", () =>
+  databaseTest().then(async (db) => {
+    await db.user.set();
+    expect(await db.user.get()).toBeUndefined();
+  }));
 
-test("change email of user", async () => {
-  fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
-  const user = new User(StorageInterface);
-  await user.login("myuser", "mylogin");
-  await user.set({ email: "newemail@gmail.com" });
-  const dbuser = await user.get();
-  expect(dbuser.email).toBe("newemail@gmail.com");
-});
+test("change email of user", async () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
+    await db.user.login("myuser", "mylogin");
+    await db.user.set({ email: "newemail@gmail.com" });
+    const dbuser = await db.user.get();
+    expect(dbuser.email).toBe("newemail@gmail.com");
+  }));
 
-test("login user", async () => {
-  fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
-  const user = new User(StorageInterface);
-  await user.login("myuser", "mylogin");
-  const dbuser = await user.get();
-  expect(dbuser.username).toBe(SUCCESS_LOGIN_RESPONSE.payload.username);
-  expect(dbuser.email).toBe(SUCCESS_LOGIN_RESPONSE.payload.email);
-  expect(dbuser.refreshToken).toBe(SUCCESS_LOGIN_RESPONSE.refresh_token);
-  expect(dbuser.accessToken).toBe(SUCCESS_LOGIN_RESPONSE.access_token);
-  expect(dbuser.scopes).toBe(SUCCESS_LOGIN_RESPONSE.scopes);
-});
+test("login user", async () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
+    await db.user.login("myuser", "mylogin");
+    const dbuser = await db.user.get();
+    expect(dbuser.username).toBe(SUCCESS_LOGIN_RESPONSE.payload.username);
+    expect(dbuser.email).toBe(SUCCESS_LOGIN_RESPONSE.payload.email);
+    expect(dbuser.refreshToken).toBe(SUCCESS_LOGIN_RESPONSE.refresh_token);
+    expect(dbuser.accessToken).toBe(SUCCESS_LOGIN_RESPONSE.access_token);
+    expect(dbuser.scopes).toBe(SUCCESS_LOGIN_RESPONSE.scopes);
+  }));
 
-test("login user with wrong password", () => {
-  fetch.mockResponseOnce(
-    JSON.stringify({ error: "Username or password is incorrect." }),
-    { status: 400 }
-  );
-  const user = new User(StorageInterface);
-  return user.login("myuser", "wrongpassword").catch((err) => {
-    expect(err.message).toBe("Username or password is incorrect.");
-  });
-});
+test("login user with wrong password", () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(
+      JSON.stringify({ error: "Username or password is incorrect." }),
+      { status: 400 }
+    );
+    await expect(db.user.login("myuser", "wrongpassword")).rejects.toThrow(
+      /Username or password is incorrect./
+    );
+  }));
 
-test("failed login with unknown error", () => {
-  fetch.mockResponseOnce(JSON.stringify({}), { status: 400 });
-  const user = new User(StorageInterface);
-  return user.login("myuser", "wrongpassword").catch((err) => {
-    expect(err.message).toContain("Request failed with status code: ");
-  });
-});
+test("failed login with unknown error", () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(JSON.stringify({}), { status: 400 });
+    await expect(db.user.login("myuser", "wrongpassword")).rejects.toThrow(
+      /Request failed with status code: /
+    );
+  }));
 
-test("signup user", async () => {
-  fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
-  const user = new User(StorageInterface);
-  await user.signup(
-    SUCCESS_LOGIN_RESPONSE.payload.username,
-    SUCCESS_LOGIN_RESPONSE.payload.email,
-    "password"
-  );
-  const dbuser = await user.get();
-  expect(dbuser.username).toBe(SUCCESS_LOGIN_RESPONSE.payload.username);
-  expect(dbuser.email).toBe(SUCCESS_LOGIN_RESPONSE.payload.email);
-  expect(dbuser.refreshToken).toBe(SUCCESS_LOGIN_RESPONSE.refresh_token);
-  expect(dbuser.accessToken).toBe(SUCCESS_LOGIN_RESPONSE.access_token);
-  expect(dbuser.scopes).toBe(SUCCESS_LOGIN_RESPONSE.scopes);
-});
+test("signup user", () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
+    await db.user.signup(
+      SUCCESS_LOGIN_RESPONSE.payload.username,
+      SUCCESS_LOGIN_RESPONSE.payload.email,
+      "password"
+    );
+    const dbuser = await db.user.get();
+    expect(dbuser.username).toBe(SUCCESS_LOGIN_RESPONSE.payload.username);
+    expect(dbuser.email).toBe(SUCCESS_LOGIN_RESPONSE.payload.email);
+    expect(dbuser.refreshToken).toBe(SUCCESS_LOGIN_RESPONSE.refresh_token);
+    expect(dbuser.accessToken).toBe(SUCCESS_LOGIN_RESPONSE.access_token);
+    expect(dbuser.scopes).toBe(SUCCESS_LOGIN_RESPONSE.scopes);
+  }));
 
-test("logout user", async () => {
-  fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
-  const user = new User(StorageInterface);
-  await user.login("myuser", "mylogin");
-  const dbuser = await user.get();
-  expect(dbuser.username).toBe(SUCCESS_LOGIN_RESPONSE.payload.username);
-  await user.logout();
-  expect(await user.get()).toBeUndefined();
-});
+test("logout user", () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
+    await db.user.login("myuser", "mylogin");
+    const dbuser = await db.user.get();
+    expect(dbuser.username).toBe(SUCCESS_LOGIN_RESPONSE.payload.username);
+    await db.user.logout();
+    expect(await db.user.get()).toBeUndefined();
+  }));
 
-test("refresh user's token", async () => {
-  fetch.mockResponseOnce(
-    JSON.stringify({ ...SUCCESS_LOGIN_RESPONSE, expiry: -2000 })
-  );
-  const user = new User(StorageInterface);
-  await user.login("myuser", "mylogin");
-  fetch.mockResponseOnce(
-    JSON.stringify({
-      access_token: "new_token",
-      refresh_token: "new_refresh_token",
-    })
-  );
-  await user.token();
-  const dbuser = await user.get();
-  expect(dbuser.refreshToken).toBe("new_refresh_token");
-  expect(dbuser.accessToken).toBe("new_token");
-});
+test("refresh user's token", () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(
+      JSON.stringify({ ...SUCCESS_LOGIN_RESPONSE, expiry: -2000 })
+    );
+    await db.user.login("myuser", "mylogin");
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        access_token: "new_token",
+        refresh_token: "new_refresh_token",
+      })
+    );
+    await db.user.token();
+    const dbuser = await db.user.get();
+    expect(dbuser.refreshToken).toBe("new_refresh_token");
+    expect(dbuser.accessToken).toBe("new_token");
+  }));
 
-test("refreshing token when user has no access token should remove user", async () => {
-  fetch.mockResponseOnce(
-    JSON.stringify({
-      ...SUCCESS_LOGIN_RESPONSE,
-      expiry: -2000,
-      access_token: false,
-    })
-  );
-  const user = new User(StorageInterface);
-  await user.login("myuser", "mylogin");
-  await user.token();
-  const dbuser = await user.get();
-  expect(dbuser).toBeUndefined();
-});
+test("refreshing token when user has no access token should remove user", () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        ...SUCCESS_LOGIN_RESPONSE,
+        expiry: -2000,
+        access_token: false,
+      })
+    );
+    await db.user.login("myuser", "mylogin");
+    await db.user.token();
+    const dbuser = await db.user.get();
+    expect(dbuser).toBeUndefined();
+  }));
 
-test("refresh user's token when its not expired", async () => {
-  fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
-  const user = new User(StorageInterface);
-  await user.login("myuser", "mylogin");
-  expect(await user.token()).toBe("access_token");
-  const dbuser = await user.get();
-  expect(dbuser.refreshToken).toBe("refresh_token");
-  expect(dbuser.accessToken).toBe("access_token");
-});
+test("refresh user's token when its not expired", () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
+    await db.user.login("myuser", "mylogin");
+    expect(await db.user.token()).toBe("access_token");
+    const dbuser = await db.user.get();
+    expect(dbuser.refreshToken).toBe("refresh_token");
+    expect(dbuser.accessToken).toBe("access_token");
+  }));
 
-test("refresh token for non existent user should do nothing", async () => {
-  fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
-  const user = new User(StorageInterface);
-  expect(await user.token()).toBeUndefined();
-  const dbuser = await user.get();
-  expect(dbuser).toBeUndefined();
-});
+test("refresh token for non existent user should do nothing", () =>
+  databaseTest().then(async (db) => {
+    fetch.mockResponseOnce(JSON.stringify(SUCCESS_LOGIN_RESPONSE));
+    expect(await db.user.token()).toBeUndefined();
+    const dbuser = await db.user.get();
+    expect(dbuser).toBeUndefined();
+  }));
