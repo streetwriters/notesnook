fdd37df726d287dc91ead3a36e46fe888292e335
thecodrr <thecodrr@protonmail.com>
2020-08-06 09:43:44 +0500

    feat: conditional api host for debugging

--- packages/core/utils/constants.js
+++ packages/core/utils/constants.js
@@ -1,4 +1,7 @@
-export const HOST = "https://api.notesnook.com/";
+export const HOST =
+  process.env.NODE_ENV === "production"
+    ? "https://api.notesnook.com/"
+    : "0.0.0.0:8000";
 export const HEADERS = {
   agent: "nn/1.0.0",
   origin: "notesnook.com",
