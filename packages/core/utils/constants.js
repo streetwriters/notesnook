8e9bf61ee2e53e02d68083772c4a954bff71bf8a
thecodrr <thecodrr@protonmail.com>
2020-08-06 10:01:09 +0500

    fix: local HOST url

--- packages/core/utils/constants.js
+++ packages/core/utils/constants.js
@@ -1,7 +1,7 @@
 export const HOST =
   process.env.NODE_ENV === "production"
     ? "https://api.notesnook.com/"
-    : "http://0.0.0.0:8000";
+    : "http://0.0.0.0:8000/";
 export const HEADERS = {
   agent: "nn/1.0.0",
   origin: "notesnook.com",
