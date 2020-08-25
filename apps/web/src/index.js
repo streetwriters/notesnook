80b5a2baa92fbc0847813e03f572fabd9e9a4e45
thecodrr <thecodrr@protonmail.com>
2020-08-25 09:13:37 +0500

    fix: load app even if there is network error

--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -9,12 +9,15 @@ import * as serviceWorker from "./serviceWorker";
 import Modal from "react-modal";
 import { db } from "./common";
 
-db.init().then(() => {
-  Modal.setAppElement("#root");
-  ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<span>Loading...</span>, document.getElementById("root"));
+db.init()
+  .catch(console.error)
+  .finally(() => {
+    Modal.setAppElement("#root");
+    ReactDOM.render(<App />, document.getElementById("root"));
 
-  // If you want your app to work offline and load faster, you can change
-  // unregister() to register() below. Note this comes with some pitfalls.
-  // Learn more about service workers: https://bit.ly/CRA-PWA
-  serviceWorker.unregister();
-});
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+  });
