5d65ed12096c110b8a70f655e509dbfd108c5e37
thecodrr <thecodrr@protonmail.com>
2020-08-25 08:54:47 +0500

    feat: remove the manual client-side subscription handling

--- apps/web/src/common/upgrade.js
+++ apps/web/src/common/upgrade.js
@@ -1,20 +1,10 @@
-import { db } from "./index";
-
-window.onload = async function (e) {
-  const refno = getParameterByName("refno");
-  if (refno) {
-    console.log(refno);
-    await db.user.upgrade(refno);
-    window.location.hash = "#";
-  }
-};
-
 function upgrade(user) {
   const { TwoCoInlineCart: cart } = window;
   if (!cart) return;
   cart.setup.setMerchant("250327951921"); // your Merchant code
   cart.billing.setEmail(user.email); // customer email address
   cart.shipping.setEmail(user.email); // customer Delivery email
+  cart.cart.setExternalCustomerReference(user.Id);
   cart.cart.setTest(true);
 
   cart.products.add({
@@ -24,17 +14,6 @@ function upgrade(user) {
 
   cart.cart.setCartLockedFlag(true);
   cart.cart.checkout().then(console.log); // start checkout process
-}
-//http://localhost:3000/#/?refno=121822593&total=6.99&total-currency=USD&signature=06b250570afca7ccd6cb54d4d1dc63a7c4c3e1adf28be4cdb57a9f4569bfbbce
-
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 export { upgrade };
