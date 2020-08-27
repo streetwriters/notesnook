5a0c85fd4d05d2286d0fd9a35d23a6384e758812
thecodrr <thecodrr@protonmail.com>
2020-08-27 13:13:43 +0500

    feat: use cryptographically secure item ids

--- packages/core/utils/id.js
+++ packages/core/utils/id.js
@@ -1,6 +1,5 @@
-export default function() {
-    var timestamp = (new Date().getTime() / 1000 | 0).toString(16);    
-    return timestamp + 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, function() {
-        return (Math.random() * 16 | 0).toString(16);
-    }).toLowerCase();
-  };
+import cryptoRandom from "crypto-random-string";
+
+export default function () {
+  return cryptoRandom({ length: 16, type: "hex" }).toLowerCase();
+}
