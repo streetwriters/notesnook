fcd828a23b678beacd469e77317201c8e7db464a
ammarahm-ed <ammarahmed6506@gmail.com>
2020-09-13 11:44:25 +0500

    fix appearance module not working on android

--- apps/mobile/android/app/src/main/java/com/notesnook/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/notesnook/MainActivity.java
@@ -1,6 +1,9 @@
 package com.streetwriters.notesnook;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent; 
+import android.content.res.Configuration;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +11,12 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+ @Override
+public void onConfigurationChanged(Configuration newConfig) {
+  super.onConfigurationChanged(newConfig);
+  getReactInstanceManager().onConfigurationChanged(this, newConfig);
+}
+
   @Override
   protected String getMainComponentName() {
     return "Notesnook";
