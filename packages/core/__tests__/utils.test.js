fe369e67715f6856e09de39714ac9925c538a3b7
thecodrr <thecodrr@protonmail.com>
2020-09-15 10:11:56 +0500

    test: update utils test to test the new grouping logic

--- packages/core/__tests__/utils.test.js
+++ packages/core/__tests__/utils.test.js
@@ -1,9 +1,9 @@
 import { groupBy } from "../utils";
 
 test("groupBy should work", () => {
-  let ret = groupBy([1, 2, 3, 1, 1, 1, 2, 2, 2, 3, 3, 3], x => x.toString());
-  expect(ret.length).toBe(3);
-  expect(ret[0].title).toBe("1");
-  expect(ret[1].title).toBe("2");
-  expect(ret[2].title).toBe("3");
+  let ret = groupBy([1, 2, 3, 1, 1, 1, 2, 2, 2, 3, 3, 3], (x) => x.toString());
+  expect(ret.length).toBe(15);
+  expect(ret.some((i) => i.title === "1")).toBeTruthy();
+  expect(ret.some((i) => i.title === "2")).toBeTruthy();
+  expect(ret.some((i) => i.title === "3")).toBeTruthy();
 });
