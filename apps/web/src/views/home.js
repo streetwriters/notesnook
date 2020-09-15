a293a2f57724539b773f386baa772fcfa629ea5a
thecodrr <thecodrr@protonmail.com>
2020-09-15 09:29:43 +0500

    fix: no placeholder appearing when home is empty

--- apps/web/src/views/home.js
+++ apps/web/src/views/home.js
@@ -5,6 +5,7 @@ import Note from "../components/note";
 import { useStore, store } from "../stores/note-store";
 import { useStore as useEditorStore } from "../stores/editor-store";
 import ListContainer from "../components/list-container";
+import NotesPlaceholder from "../components/placeholders/notesplacholder";
 
 function Home() {
   useEffect(() => store.refresh(), []);
@@ -18,6 +19,7 @@ function Home() {
       item={(index, item) => (
         <Note index={index} pinnable={false} item={item} />
       )}
+      placeholder={NotesPlaceholder}
       button={{ content: "Make a new note", onClick: () => newSession() }}
     >
       <GroupList
