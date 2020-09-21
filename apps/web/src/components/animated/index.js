808f2534629d10176fa5a13539683a076a2d6271
thecodrr <thecodrr@protonmail.com>
2020-09-21 10:20:21 +0500

    feat: reduce framer-motion bundle size

--- apps/web/src/components/animated/index.js
+++ apps/web/src/components/animated/index.js
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { m as motion } from "framer-motion";
 import * as Rebass from "rebass";
 
 export default {
--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -8,12 +8,18 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Modal from "react-modal";
 import { db } from "./common";
+import { MotionConfig, AnimationFeature } from "framer-motion";
 
 db.init()
   .catch(console.error)
   .finally(() => {
     Modal.setAppElement("#root");
-    ReactDOM.render(<App />, document.getElementById("root"));
+    ReactDOM.render(
+      <MotionConfig features={[AnimationFeature]}>
+        <App />
+      </MotionConfig>,
+      document.getElementById("root")
+    );
 
     // If you want your app to work offline and load faster, you can change
     // unregister() to register() below. Note this comes with some pitfalls.
