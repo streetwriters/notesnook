0bedb01fe64e166449f8a5b9dc1712750e487c89
ammarahm-ed <ammarahmed6506@gmail.com>
2020-09-24 23:02:30 +0500

    remove Key prop from Button

--- apps/mobile/src/components/Button/index.js
+++ apps/mobile/src/components/Button/index.js
@@ -14,16 +14,14 @@ export const Button = ({
   title = '',
   icon,
   color = 'accent',
-  key
 }) => {
   const [state, dispatch] = useTracked();
-  const {colors, tags, premiumUser} = state;
+  const {colors} = state;
   const usedColor = 'accent' ? colors.accent : color;
 
   return (
     <PressableButton
       onPress={onPress}
-      key={key}
       color={grayed ? colors.nav : usedColor}
       selectedColor={grayed ? colors.nav : usedColor}
       alpha={grayed ? (!colors.night ? -0.04 : 0.04) : -0.1}
