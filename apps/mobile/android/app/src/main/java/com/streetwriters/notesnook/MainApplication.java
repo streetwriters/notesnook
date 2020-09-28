fc3ae69edb5e7e8324fe934843e3300c1af80882
ammarahm-ed <ammarahmed6506@gmail.com>
2020-09-28 09:29:09 +0500

    implement e2e detox testing

--- apps/mobile/android/app/src/main/java/com/streetwriters/notesnook/MainApplication.java
+++ apps/mobile/android/app/src/main/java/com/streetwriters/notesnook/MainApplication.java
@@ -10,8 +10,9 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import androidx.multidex.MultiDexApplication;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
---
+++ apps/mobile/e2e/firstTest.e2e.js
@@ -0,0 +1,10 @@
+
+  it('app loaded', async () => {
+    await expect(element(by.id('mobile_main_view'))).toBeVisible();
+  })
+
+  it('open_editor', async () => {
+    await expect(element(by.id('mobile_main_view'))).toBeVisible();
+  })
+
+
