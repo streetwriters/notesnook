b3b8d6715d444613517f5f8e58fb36cbc1aa43e9
thecodrr <thecodrr@protonmail.com>
2020-10-03 11:59:20 +0500

    feat: add verification checks to backup & restore

--- packages/core/__tests__/backup.test.js
+++ packages/core/__tests__/backup.test.js
@@ -17,24 +17,29 @@ beforeEach(async () => {
 test("export backup", () =>
   noteTest().then(() =>
     notebookTest().then(async ({ db }) => {
-      const exp = await db.backup.export();
-      expect(JSON.parse(exp).t).toBeGreaterThan(0);
+      const exp = await db.backup.export("node");
+      let backup = JSON.parse(exp);
+      expect(backup.type).toBe("node");
+      expect(backup.date).toBeGreaterThan(0);
     })
   ));
 
 test("export encrypted backup", () =>
   noteTest().then(() =>
     notebookTest().then(async ({ db }) => {
-      const exp = await db.backup.export(true);
-      expect(JSON.parse(exp).iv).toBe("some iv");
+      const exp = await db.backup.export("node", true);
+      let backup = JSON.parse(exp);
+      expect(backup.type).toBe("node");
+      expect(backup.date).toBeGreaterThan(0);
+      expect(backup.data.iv).toBe("some iv");
     })
   ));
 
 test("import backup", () =>
   noteTest().then(() =>
     notebookTest().then(async ({ db, id }) => {
-      const exp = await db.backup.export();
-      await db.context.clear();
+      const exp = await db.backup.export("node");
+      StorageInterface.clear();
       await db.backup.import(exp);
       expect(db.notebooks.notebook(id).data.id).toBe(id);
     })
@@ -43,9 +48,22 @@ test("import backup", () =>
 test("import encrypted backup", () =>
   noteTest().then(() =>
     notebookTest().then(async ({ db, id }) => {
-      const exp = await db.backup.export(true);
-      await db.context.clear();
+      const exp = await db.backup.export("node", true);
+      StorageInterface.clear();
       await db.backup.import(exp);
       expect(db.notebooks.notebook(id).data.id).toBe(id);
+    })
+  ));
+
+test("import tempered backup", () =>
+  noteTest().then(() =>
+    notebookTest().then(async ({ db, id }) => {
+      const exp = await db.backup.export("node");
+      StorageInterface.clear();
+      const backup = JSON.parse(exp);
+      backup.data.hello = "world";
+      await expect(
+        db.backup.import(JSON.stringify(backup))
+      ).rejects.toThrowError(/tempered/);
     })
   ));
--- packages/core/database/backup.js
+++ packages/core/database/backup.js
@@ -1,3 +1,8 @@
+import Hashes from "jshashes";
+const sha256 = new Hashes.SHA256();
+
+const invalidKeys = ["user", "t"];
+const validTypes = ["mobile", "web", "node"];
 export default class Backup {
   /**
    *
@@ -7,28 +12,77 @@ export default class Backup {
     this._db = db;
   }
 
-  async export(encrypt = false) {
-    const keys = await this._db.context.getAllKeys();
+  /**
+   *
+   * @param {"web"|"mobile"|"node"} type
+   * @param {boolean} encrypt
+   */
+  async export(type, encrypt = false) {
+    if (!validTypes.some((t) => t === type))
+      throw new Error("Invalid type. It must be one of 'mobile' or 'web'.");
+
+    const keys = (await this._db.context.getAllKeys()).filter(
+      (key) => !(key in invalidKeys)
+    );
+
     const db = Object.fromEntries(await this._db.context.readMulti(keys));
+    db.h = sha256.hex(JSON.stringify(db));
+
     if (encrypt) {
       const key = await this._db.user.key();
-      return JSON.stringify(
-        await this._db.context.encrypt(key, JSON.stringify(db))
-      );
+      return JSON.stringify({
+        type,
+        date: Date.now(),
+        data: await this._db.context.encrypt(key, JSON.stringify(db)),
+      });
     }
-    return JSON.stringify(db);
+
+    return JSON.stringify({
+      type,
+      date: Date.now(),
+      data: db,
+    });
   }
 
+  /**
+   *
+   * @param {string} data the backup data
+   */
   async import(data) {
+    if (!data) return;
+
     let backup = JSON.parse(data);
+
+    if (!this._validate(backup)) throw new Error("Invalid backup.");
+
+    let db = backup.data;
     //check if we have encrypted data
-    if (backup.salt && backup.iv) {
+    if (db.salt && db.iv) {
       const key = await this._db.user.key();
-      backup = JSON.parse(await this._db.context.decrypt(key, backup));
+      db = JSON.parse(await this._db.context.decrypt(key, db));
     }
-    for (let key in backup) {
-      let value = backup[key];
+
+    if (!this._verify(db))
+      throw new Error("Backup file has been tempered, aborting...");
+
+    for (let key in db) {
+      let value = db[key];
       await this._db.context.write(key, value);
     }
+  }
+
+  _validate(backup) {
+    return (
+      !!backup.date &&
+      !!backup.data &&
+      !!backup.type &&
+      validTypes.some((t) => t === backup.type)
+    );
+  }
+
+  _verify(db) {
+    const hash = db.h;
+    delete db.h;
+    return hash == sha256.hex(JSON.stringify(db));
   }
 }
