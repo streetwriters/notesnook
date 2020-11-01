e0f70bc28bc164e976704d5ef7b2d43a0b10347a
thecodrr <thecodrr@protonmail.com>
2020-11-01 10:04:48 +0500

    feat: adjust body font size

--- apps/web/src/theme/font/fontsize.js
+++ apps/web/src/theme/font/fontsize.js
@@ -5,7 +5,7 @@ class FontSizeFactory {
       input: 16 * scaleFactor,
       title: 18 * scaleFactor,
       subtitle: 16 * scaleFactor,
-      body: 16 * scaleFactor,
+      body: 14 * scaleFactor,
       menu: 14 * scaleFactor,
       subBody: 12 * scaleFactor,
     };
