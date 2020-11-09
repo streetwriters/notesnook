7e7aab14f266c913ae57e40c3b78b5f43fa2eab3
ammarahm-ed <ammarahmed6506@gmail.com>
2020-11-09 12:20:25 +0500

    add support to open intent from context menu

---
+++ apps/mobile/android/app/src/main/java/com/streetwriters/notesnook/IntentActivity.java
@@ -0,0 +1,19 @@
+package com.streetwriters.notesnook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class IntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = this.getIntent();
+        intent.setClass(getBaseContext(),MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
