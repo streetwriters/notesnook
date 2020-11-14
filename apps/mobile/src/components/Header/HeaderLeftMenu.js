26962ac5f545829da73a0622b29c20596966f1e4
ammarahm-ed <ammarahmed6506@gmail.com>
2020-11-14 10:09:54 +0500

    remove backHandler fro header

--- apps/mobile/src/components/Header/HeaderLeftMenu.js
+++ apps/mobile/src/components/Header/HeaderLeftMenu.js
@@ -1,48 +1,21 @@
-import React, {useEffect} from 'react';
-import {useTracked} from '../../provider';
-import {SIZE} from '../../utils/SizeUtils';
-import {DDS} from '../../services/DeviceDetection';
-import {ActionIcon} from '../ActionIcon';
+import React from 'react';
+import { useTracked } from '../../provider';
+import { DDS } from '../../services/DeviceDetection';
 import NavigationService from '../../services/Navigation';
-import {eSendEvent} from '../../services/EventManager';
-import {eClearSearch} from '../../utils/Events';
-import {BackHandler} from 'react-native';
+import { SIZE } from '../../utils/SizeUtils';
+import { ActionIcon } from '../ActionIcon';
 
 export const HeaderLeftMenu = () => {
   const [state] = useTracked();
   const {colors, headerMenuState, searchResults} = state;
 
   const onLeftButtonPress = () => {
-    if (searchResults.results.length > 0) {
-      eSendEvent(eClearSearch);
-      return;
-    }
     if (headerMenuState) {
       NavigationService.openDrawer();
       return;
     }
     NavigationService.goBack();
   };
-
-  const onBackPress = () => {
-    if (searchResults.results.length > 0) {
-      eSendEvent(eClearSearch);
-      return true;
-    }
-    return false;
-  };
-
-  useEffect(() => {
-    if (searchResults.results.length > 0) {
-      BackHandler.addEventListener('hardwareBackPress', onBackPress);
-    } else {
-      BackHandler.removeEventListener('hardwareBackPress', onBackPress);
-    }
-
-    return () => {
-      BackHandler.removeEventListener('hardwareBackPress', onBackPress);
-    };
-  }, [searchResults.results]);
 
   return (
     <>
@@ -59,11 +32,7 @@ export const HeaderLeftMenu = () => {
             marginRight: 25,
           }}
           onPress={onLeftButtonPress}
-          name={
-            !headerMenuState || searchResults.results.length > 0
-              ? 'arrow-left'
-              : 'menu'
-          }
+          name={!headerMenuState ? 'arrow-left' : 'menu'}
           size={SIZE.xxxl}
           color={colors.pri}
           iconStyle={{
