66a00f8534f197d4bab72a8b30ebc3fc91ea8f1f
thecodrr <thecodrr@protonmail.com>
2020-11-17 12:34:31 +0500

    feat: implement secure cryptokey storage

--- apps/web/src/interfaces/storage.js
+++ apps/web/src/interfaces/storage.js
@@ -36,6 +36,34 @@ function getAllKeys() {
   return localforage.keys();
 }
 
+async function deriveCryptoKey(name, data) {
+  const { password, salt } = data;
+  if (!password) throw new Error("Invalid data provided to deriveCryptoKey.");
+
+  const keyData = await crypto.deriveKey(password, salt, true);
+  console.log("keyData", keyData);
+  if (window.indexedDB) {
+    const pbkdfKey = await derivePBKDF2Key(password);
+    await write(name, pbkdfKey);
+    const cipheredKey = await aesEncrypt(pbkdfKey, keyData);
+    console.log("cipheredKey", cipheredKey);
+    await write(`${name}@_k`, cipheredKey);
+  } else {
+    await write(`${name}@_k`, keyData);
+  }
+}
+
+async function getCryptoKey(name) {
+  if (window.indexedDB) {
+    const pbkdfKey = await read(name);
+    const cipheredKey = await read(`${name}@_k`);
+    if (!pbkdfKey || !cipheredKey) return;
+    return await aesDecrypt(pbkdfKey, cipheredKey);
+  } else {
+    return await read(`${name}@_k`);
+  }
+}
+
 export default {
   read,
   readMulti,
@@ -43,7 +71,66 @@ export default {
   remove,
   clear,
   getAllKeys,
-  deriveKey: crypto.deriveKey,
+  deriveCryptoKey,
+  getCryptoKey,
   encrypt: crypto.encrypt,
   decrypt: crypto.decrypt,
 };
+
+let enc = new TextEncoder();
+let dec = new TextDecoder();
+async function derivePBKDF2Key(password) {
+  const key = await window.crypto.subtle.importKey(
+    "raw",
+    enc.encode(password),
+    "PBKDF2",
+    false,
+    ["deriveKey"]
+  );
+
+  let salt = window.crypto.getRandomValues(new Uint8Array(16));
+  return await window.crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt,
+      iterations: 100000,
+      hash: "SHA-256",
+    },
+    key,
+    { name: "AES-GCM", length: 256 },
+    true,
+    ["encrypt", "decrypt"]
+  );
+}
+
+async function aesEncrypt(cryptoKey, data) {
+  let iv = window.crypto.getRandomValues(new Uint8Array(12));
+
+  const cipher = await window.crypto.subtle.encrypt(
+    {
+      name: "AES-GCM",
+      iv: iv,
+    },
+    cryptoKey,
+    enc.encode(data)
+  );
+
+  return {
+    iv,
+    cipher,
+  };
+}
+
+async function aesDecrypt(cryptoKey, data) {
+  const { iv, cipher } = data;
+
+  const plainText = await window.crypto.subtle.decrypt(
+    {
+      name: "AES-GCM",
+      iv: iv,
+    },
+    cryptoKey,
+    cipher
+  );
+  return dec.decode(plainText);
+}
