0d331977b5ff44a4b5f2805cfdb9f8fcdcf0883b
ammarahm-ed <ammarahmed6506@gmail.com>
2020-11-18 18:17:46 +0500

    add background color to container

--- apps/mobile/src/components/Container/index.js
+++ apps/mobile/src/components/Container/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {KeyboardAvoidingView, Platform, SafeAreaView} from 'react-native';
+import { useTracked } from '../../provider';
 import SelectionHeader from '../SelectionHeader';
 import {ContainerTopSection} from './ContainerTopSection';
 
 export const Container = ({children, root}) => {
-
+  const [state] = useTracked();
+  const {colors, } = state;
   return (
     <KeyboardAvoidingView behavior="padding" enabled={Platform.OS === 'ios'}>
       <SafeAreaView
         style={{
           height: '100%',
+          backgroundColor:colors.bg
         }}>
         <SelectionHeader />
         <ContainerTopSection root={root} />
