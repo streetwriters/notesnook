c01442c733fb73dc09e881a84758e9914257e12f
ammarahm-ed <ammarahmed6506@gmail.com>
2020-11-20 01:16:13 +0500

    add editorRef

--- apps/mobile/src/utils/Refs.js
+++ apps/mobile/src/utils/Refs.js
@@ -5,3 +5,4 @@ export const inputRef = createRef();
 export const sideMenuOverlayRef = createRef();
 export const rootNavigatorRef = createRef();
 export const tabBarRef = createRef();
+export const editorRef = createRef();
