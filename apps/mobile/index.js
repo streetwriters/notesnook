661794fd7e230f23af20b68f47f515c6387030c3
ammarahm-ed <ammarahmed6506@gmail.com>
2020-11-23 11:35:10 +0500

    remove unecessary top level imports

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,5 +1,3 @@
-import 'react-native-gesture-handler';
-import 'react-native-get-random-values';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
