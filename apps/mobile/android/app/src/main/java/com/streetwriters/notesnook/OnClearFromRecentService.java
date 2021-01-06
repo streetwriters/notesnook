515cf745380e1b8b8543069728a673b6fc50a6da
ammarahm-ed <ammarahmed6506@gmail.com>
2021-01-06 14:56:19 +0500

    clear logging

--- apps/mobile/android/app/src/main/java/com/streetwriters/notesnook/OnClearFromRecentService.java
+++ apps/mobile/android/app/src/main/java/com/streetwriters/notesnook/OnClearFromRecentService.java
@@ -14,19 +14,17 @@ public class OnClearFromRecentService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("ClearFromRecentService", "Service Started");
+    
         return START_NOT_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("ClearFromRecentService", "Service Destroyed");
     }
 
     @Override
     public void onTaskRemoved(Intent rootIntent) {
-        Log.e("ClearFromRecentService", "END");
         MMKV mmkv = MMKV.mmkvWithID("default",MMKV.SINGLE_PROCESS_MODE);
         mmkv.removeValueForKey("appState");
         stopSelf();
