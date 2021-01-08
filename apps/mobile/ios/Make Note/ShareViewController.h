0a5b1303d5daa3087c3f2c0b1b2550af886f1b64
ammarahm-ed <ammarahmed6506@gmail.com>
2021-01-08 12:23:55 +0500

    add new share extension for ios

---
+++ apps/mobile/ios/Make Note/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  Make Note
+//
+//  Created by Ammar Ahmed on 07/01/2021.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
---
+++ apps/mobile/ios/Make Note/ShareViewController.m
@@ -0,0 +1,33 @@
+//
+//  ShareViewController.m
+//  Make Note
+//
+//  Created by Ammar Ahmed on 07/01/2021.
+//
+
+#import "ShareViewController.h"
+
+@interface ShareViewController ()
+
+@end
+
+@implementation ShareViewController
+
+- (BOOL)isContentValid {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return YES;
+}
+
+- (void)didSelectPost {
+    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
+}
+
+- (NSArray *)configurationItems {
+    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+    return @[];
+}
+
+@end
