26ee4d38456e24f95b10a3c10e7fa9a321638761
ammarahm-ed <ammarahmed6506@gmail.com>
2021-01-08 12:33:13 +0500

    add shareActivity

---
+++ apps/mobile/android/app/src/main/java/com/streetwriters/notesnook/ShareActivity.java
@@ -0,0 +1,10 @@
+package com.streetwriters.notesnook;
+import com.facebook.react.ReactActivity;
+
+public class ShareActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "NotesnookShare";
+    }
+
+}
---
+++ apps/mobile/android/app/src/main/java/com/streetwriters/notesnook/ShareApplication.java
@@ -0,0 +1,45 @@
+package com.streetwriters.notesnook;
+
+import com.streetwriters.notesnook.BuildConfig;
+
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.List;
+
+import androidx.multidex.MultiDexApplication;
+
+
+public class ShareApplication extends MultiDexApplication implements ReactApplication {
+    private final ReactNativeHost mReactNativeHost =
+            new ReactNativeHost(this) {
+                @Override
+                public boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
+
+                @Override
+                protected List<ReactPackage> getPackages() {
+                    @SuppressWarnings("UnnecessaryLocalVariable")
+                    List<ReactPackage> packages = new PackageList(this).getPackages();
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    // packages.add(new MyReactNativePackage());
+                    packages.add(new NNativeModulePackage());
+                    return packages;
+                }
+
+                @Override
+                protected String getJSMainModuleName() {
+                    return "index";
+                }
+            };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+
+}
