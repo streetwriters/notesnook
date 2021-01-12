7b2b1fc20f8a9130ffe4db9d9972fad186d647bf
thecodrr <thecodrr@protonmail.com>
2021-01-13 00:01:35 +0500

    fix: toggle selection mode to false on navigate

--- apps/web/src/utils/use-routes.js
+++ apps/web/src/utils/use-routes.js
@@ -1,10 +1,14 @@
 import { useLocation } from "wouter";
 import makeMatcher from "wouter/matcher";
 import { navigate } from "../navigation";
+import { store as selectionStore } from "../stores/selection-store";
 
 export default function useRoutes(routes, options) {
   const [location] = useLocation();
   const matcher = makeMatcher();
+
+  // TODO move this to an extension function
+  selectionStore.toggleSelectionMode(false);
 
   for (var key in routes) {
     const [match, params] = matcher(key, location);
