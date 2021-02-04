2a1a5d737de4d088abd44752f5a5512815f1e3d3
ammarahm-ed <ammarahmed6506@gmail.com>
2021-02-04 14:33:19 +0500

    migrate to tinyMCE

--- apps/mobile/android/app/src/main/assets/listeners.js
+++ apps/mobile/android/app/src/main/assets/listeners.js
@@ -1,30 +1,13 @@
-let titleInput = isTablet ? 'titleInput' : 'simpleTitleInput';
-let infoBar = isTablet ? '.info-bar' : '.info-bar-alt';
+let titleInput = document.getElementById('titleInput');
+let infoBar = '.info-bar';
 let info = null;
+
 function attachTitleInputListeners() {
-  titleInput = isTablet ? 'titleInput' : 'simpleTitleInput';
-  infoBar = isTablet ? '.info-bar' : '.info-bar-alt';
+  infoBar = '.info-bar';
   document.addEventListener(
     'DOMContentLoaded',
     () => {
       autosize();
-      document
-        .querySelector('.app-main')
-        .addEventListener('scroll', (event) => {
-          if (scrollTimer) {
-            clearTimeout(scrollTimer);
-            scrollTimer = null;
-          }
-          scrollTimer = setTimeout(() => {
-            window.ReactNativeWebView.postMessage(
-              JSON.stringify({
-                visible: document.querySelector('.app-main').scrollTop,
-                title: document.getElementById(titleInput).value,
-                type: 'scroll',
-              }),
-            );
-          }, 100);
-        });
     },
     false,
   );
@@ -36,34 +19,34 @@ function attachTitleInputListeners() {
     onTitleChange();
   };
 
-  document.getElementById(titleInput).onkeypress = function (evt) {
+  titleInput.onkeypress = function (evt) {
     if (evt.keyCode === 13 || evt.which === 13) {
       evt.preventDefault();
       editor.focus();
-      editor.setSelection(editor.getText().length - 1, 0);
       onTitleChange();
       return false;
     }
   };
 
-  document.getElementById(titleInput).onfocus = function (evt) {
-    window.ReactNativeWebView.postMessage(
-      JSON.stringify({
-        type: 'focus',
-        value: 'title',
-      }),
-    );
-  };
+  titleInput.addEventListener('focus', function (evt) {
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({
+          type: 'focus',
+          value: 'title',
+        }),
+      );
+    }
+  });
 
-  document.getElementById(titleInput).onkeydown = function (evt) {
+  titleInput.onkeydown = function (evt) {
     onTitleChange(evt);
   };
 
-  document.getElementById(titleInput).onchange = function (evt) {
+  titleInput.onchange = function (evt) {
     autosize();
   };
-
-  document.getElementById(titleInput).onkeyup = function (evt) {
+  titleInput.onkeyup = function (evt) {
     onTitleChange(evt);
   };
 }
@@ -71,278 +54,32 @@ function attachTitleInputListeners() {
 function onTitleChange(ele) {
   let titleMessage = {
     type: 'title',
-    value: document.getElementById(titleInput).value,
+    value: titleInput.value,
   };
   info = document.querySelector(infoBar);
   info.querySelector('#infowords').innerText =
-    editor.getText().split(' ').length + ' words';
+    editor.plugins.wordcount.getCount() + ' words';
+
   autosize();
   if (titleMessage && typeof titleMessage.value === 'string') {
-    window.ReactNativeWebView.postMessage(JSON.stringify(titleMessage));
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView?.postMessage(JSON.stringify(titleMessage));
+    }
   }
 }
 
 function autosize() {
-  if (isTablet) {
-    let ele = document.getElementById('textCopy');
-    ele.innerHTML = document
-      .getElementById(titleInput)
-      .value.replace(/\n/g, '<br/>');
-    //console.log(document.getElementById('titlebar').scrollHeight);
-    let newHeight = document.getElementById('titlebar').scrollHeight;
-    let css = document.createElement('style');
-    css.type = 'text/css';
-    let node = `
-      .ql-container {
-        box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
-        margin: 0px;
-        position: relative;
-        height:calc(100% - ${newHeight + 65}px) !important
+  let ele = document.getElementById('textCopy');
+  ele.innerHTML = titleInput.value.replace(/\n/g, '<br/>');
+  let newHeight = document.getElementById('titlebar').scrollHeight;
+  let css = document.createElement('style');
+  css.type = 'text/css';
+  let node = `
+      .tox-tinymce {
+        min-height:calc(100vh - ${newHeight}px) !important;
         };
    `;
-    css.appendChild(document.createTextNode(node));
-    document.getElementsByTagName('head')[0].appendChild(css);
-  }
-}
-
-function attachEditorListeners() {
-  titleInput = isTablet ? 'titleInput' : 'simpleTitleInput';
-  infoBar = isTablet ? '.info-bar' : '.info-bar-alt';
-
-  editor.on('selection-change', function () {
-    window.ReactNativeWebView.postMessage(
-      JSON.stringify({
-        type: 'focus',
-        value: 'editor',
-      }),
-    );
-  });
-
-  function isWhitespace(ch) {
-    let whiteSpace = false;
-    if (ch === ' ' || ch === '\t' || ch === '\n') {
-      whiteSpace = true;
-    }
-    return whiteSpace;
-  }
-
-  let deltaTimeout = null;
-  let historyTimeout = null;
-
-  editor.on('text-change', function (delta, oldDelta, source) {
-    var regex = /https?:\/\/[^\s]+$/;
-
-    if (source === 'custom') {
-      return;
-    }
-
-    if (
-      delta.ops.length === 2 &&
-      delta.ops[0].retain &&
-      isWhitespace(delta.ops[1].insert)
-    ) {
-      var endRetain = delta.ops[0].retain;
-      var text = editor.getText().substr(0, endRetain);
-      var match = text.toLowerCase().match(regex);
-
-      if (match !== null) {
-        var url = match[0];
-
-        var ops = [];
-        if (endRetain > url.length) {
-          ops.push({retain: endRetain - url.length});
-        }
-
-        ops = ops.concat([
-          {delete: url.length},
-          {insert: url, attributes: {link: url}},
-        ]);
-
-        editor.updateContents({
-          ops: ops,
-        });
-      }
-    }
-    info = document.querySelector(infoBar);
-    let infowords = info.querySelector('#infowords');
-    if (infowords) {
-      infowords.innerText = editor.getText().split(' ').length + ' words';
-    }
-
-    if (deltaTimeout) {
-      clearTimeout(deltaTimeout);
-      deltaTimeout = null;
-    }
-
-    deltaTimeout = setTimeout(() => {
-      let msg = JSON.stringify({
-        data: editor.getContents().ops,
-        type: 'delta',
-      });
-      window.ReactNativeWebView.postMessage(msg);
-    }, 50);
-
-    if (historyTimeout) {
-      clearTimeout(historyTimeout);
-      historyTimeout = null;
-    }
-
-    historyTimeout = setTimeout(() => {
-      let history = JSON.stringify({
-        type: 'history',
-        undo: editor.history.stack.undo.length,
-        redo: editor.history.stack.redo.length,
-      });
-      window.ReactNativeWebView.postMessage(history);
-    }, 1000);
-  });
-}
-
-function attachMessageListener() {
-  titleInput = isTablet ? 'titleInput' : 'simpleTitleInput';
-  infoBar = isTablet ? '.info-bar' : '.info-bar-alt';
-
-  document.addEventListener('message', (data) => {
-    let message = JSON.parse(data.data);
-    let type = message.type;
-    let value;
-    if (message.value && message.type !== 'nomenu') {
-      value = message.value;
-    } else {
-      value = message.value;
-    }
-    switch (type) {
-      case 'reset': {
-        editor.history.clear();
-        editor.setText('', 'custom');
-        document.getElementById(titleInput).value = '';
-        document.getElementById(titleInput).blur();
-        editor.blur();
-        document.getElementById(titleInput).blur();
-        window.blur();
-
-        info = document.querySelector(infoBar);
-        info.querySelector('#infodate').innerText = '';
-        info.querySelector('#infosaved').innerText = '';
-        info.querySelector('#infowords').innerText = '';
-        autosize();
-        editor.getModule('imageResize').hideOverlay();
-        break;
-      }
-      case 'keyboard':
-        var range = editor.getSelection();
-
-        if (range) {
-          if (range.length === 0) {
-            let correction = isTablet ? 265 : 110;
-            setTimeout(() => {
-              document.querySelector('.app-main').scrollTo({
-                top:
-                  editor.getBounds(editor.getSelection().index).bottom -
-                  (window.innerHeight - correction),
-                behavior: 'smooth',
-              });
-            }, 200);
-          }
-        }
-        break;
-      case 'blur':
-        document.getElementById(titleInput).blur();
-        editor.blur();
-        window.blur();
-        break;
-      case 'undo':
-        editor.history.undo();
-        break;
-      case 'redo':
-        editor.history.redo();
-        break;
-      case 'clearHistory':
-        editor.history.clear();
-        break;
-      case 'dateEdited':
-        linfo = document.querySelector(infoBar);
-        info.querySelector('#infodate').innerText = value;
-        break;
-      case 'saving':
-        info = document.querySelector(infoBar);
-        info.querySelector('#infosaved').innerText = value;
-        break;
-      case 'text':
-        editor.setText(value, 'custom');
-
-        setTimeout(() => {
-          info = document.querySelector(infoBar);
-          info.querySelector('#infowords').innerText =
-            editor.getText().split(' ').length + ' words';
-          info.querySelector('#infosaved').innerText = 'Saved';
-        }, 100);
-
-        break;
-      case 'clearEditor':
-        editor.setSelection(0, 0, 'user');
-        editor.setText('', 'custom');
-        break;
-      case 'clearTitle':
-        document.getElementById(titleInput).value = '';
-        break;
-      case 'focusEditor':
-        editor.focus();
-        break;
-      case 'focusTitle':
-        document.getElementById(titleInput).focus();
-        autosize();
-        break;
-      case 'nomenu':
-        let isenabled = value;
-        //let width = window.innerWidth;
-        let titleIn = document.getElementById('titlebar');
-        if (isenabled) {
-          //titleIn.style.width = width;
-          titleIn.style['padding-left'] = 12;
-          titleIn.style['padding-right'] = window.innerWidth * 0.4;
-        } else {
-          //titleIn.style.width = width - 120;
-          titleIn.style['padding-left'] = 60;
-          titleIn.style['padding-right'] = window.innerWidth * 0.4;
-        }
-        break;
-      case 'title':
-        document.getElementById(titleInput).value = JSON.parse(data.data).value;
-        info = document.querySelector(infoBar);
-        info.querySelector('#infowords').innerText =
-          editor.getText().split(' ').length + ' words';
-        autosize();
-        break;
-      case 'theme':
-        pageTheme.colors = value;
-        setTheme();
-        break;
-
-      case 'delta':
-        const content = value;
-        editor.setContents(content, 'custom');
-
-        setTimeout(() => {
-          info = document.querySelector(infoBar);
-          info.querySelector('#infowords').innerText =
-            editor.getText().split(' ').length + ' words';
-          info.querySelector('#infosaved').innerText = 'Saved';
-
-          document.body.scrollTop = 0; // For Safari
-          document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-        }, 100);
-        autosize();
-        break;
-      case 'html':
-        editor.setContents(editor.clipboard.convert(value, 'custom'), 'silent');
-        /*  setTimeout(() => {
-                   editor.setSelection(editor.getText().length - 1, 0);
-                 }, 0); */
-        break;
-      default:
-        break;
-    }
-  });
+  console.log('updating height');
+  css.appendChild(document.createTextNode(node));
+  document.getElementsByTagName('head')[0].appendChild(css);
 }
---
+++ apps/mobile/html/Web.bundle/site/init.js
@@ -0,0 +1,169 @@
+attachTitleInputListeners();
+autosize();
+function reactNativeEventHandler(type, value) {
+  if (window.ReactNativeWebView) {
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({
+        type: type,
+        value: value,
+      }),
+    );
+  }
+}
+
+let changeTimer = null;
+function init_tiny(size) {
+  tinymce.init({
+    selector: '#tiny_textarea',
+    menubar: false,
+    min_height: size,
+    skin_url: 'dist/skins/notesnook',
+    content_css: 'dist/skins/notesnook',
+    plugins: [
+      'mychecklist advlist autolink lists link image charmap preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media imagetools table paste help wordcount autoresize directionality',
+    ],
+    toolbar: false,
+    paste_data_images: true,
+    images_upload_handler: function (blobInfo, success, failure) {
+      success('data:' + blobInfo.blob().type + ';base64,' + blobInfo.base64());
+    },
+    statusbar: false,
+    contextmenu: false,
+    imagetools_toolbar: 'rotateleft rotateright | flipv fliph',
+    placeholder: 'Start writing your note here',
+    object_resizing: true,
+    font_formats:
+      'Times New Roman=times new roman,times;' +
+      'Serif=serif;' +
+      'Sans=sans-serif;' +
+      'Classic=courier new;' +
+      'Mono=monospace;',
+    init_instance_callback: function (edit) {
+      editor = edit;
+      setTheme();
+      reactNativeEventHandler('status', true);
+      editor.on('SelectionChange', function (e) {
+        clearTimeout(changeTimer);
+        changeTimer = setTimeout(() => {
+          selectchange();
+          reactNativeEventHandler('tiny', editor.getContent());
+          reactNativeEventHandler('history', {
+            undo: editor.undoManager.hasUndo(),
+            redo: editor.undoManager.hasRedo(),
+          });
+        }, 5);
+      });
+      editor.on('focus', () => {
+        reactNativeEventHandler('focus', 'editor');
+      });
+      editor.on('Change', function (e) {
+        clearTimeout(changeTimer);
+        changeTimer = setTimeout(() => {
+          selectchange();
+          reactNativeEventHandler('tiny', editor.getContent());
+          reactNativeEventHandler('history', {
+            undo: editor.undoManager.hasUndo(),
+            redo: editor.undoManager.hasRedo(),
+          });
+        }, 5);
+      });
+      editor.on('ExecCommand', function (e) {
+        reactNativeEventHandler(e);
+      });
+    },
+  });
+}
+
+function getNodeColor(element) {
+  if (element.style.color && element.style.color !== '') {
+    return element.style.color;
+  }
+  return null;
+}
+
+function getNodeBg(element) {
+  if (element.style.backgroundColor && element.style.backgroundColor !== '') {
+    return element.style.backgroundColor;
+  }
+  return null;
+}
+
+function selectchange() {
+  info = document.querySelector('.info-bar');
+  info.querySelector('#infowords').innerText =
+    editor.plugins.wordcount.getCount() + ' words';
+
+  let formats = Object.keys(editor.formatter.get());
+  let currentFormats = {};
+  editor.formatter
+    .matchAll(formats)
+    .forEach((format) => (currentFormats[format] = true));
+
+  let node = editor.selection.getNode();
+  currentFormats.hilitecolor = getNodeBg(node);
+  currentFormats.forecolor = getNodeColor(node);
+
+  if (!currentFormats.hilitecolor || !currentFormats.forecolor) {
+    for (var i = 0; i < node.children.length; i++) {
+      let item = editor.selection.getNode().children.item(i);
+      currentFormats.hilitecolor = getNodeBg(item);
+      currentFormats.forecolor = getNodeColor(item);
+    }
+  }
+  let range = editor.selection.getRng();
+
+  currentFormats.current = {
+    index: range.startOffset,
+    length: range.endOffset - range.startOffset,
+  };
+  currentFormats.fontsize = editor.selection.getNode().style.fontSize;
+
+  if (currentFormats.fontsize === '') {
+    currentFormats.fontsize = '12pt';
+
+    if (currentFormats.h2) {
+      currentFormats.fontsize = '18pt';
+    }
+    if (currentFormats.h3) {
+      currentFormats.fontsize = '14pt';
+    }
+    if (currentFormats.h4) {
+      currentFormats.fontsize = '12pt';
+    }
+    if (currentFormats.h5) {
+      currentFormats.fontsize = '10pt';
+    }
+    if (currentFormats.h6) {
+      currentFormats.fontsize = '8pt';
+    }
+  }
+  if (node.nodeName === 'A') {
+    currentFormats.link = node.getAttribute('href');
+  }
+
+  currentFormats.fontname = editor.selection.getNode().style.fontFamily;
+
+  if (/^(LI|UL|OL|DL)$/.test(node.nodeName)) {
+    let listElm = editor.selection.getNode();
+    if (listElm.nodeName === 'LI') {
+      listElm = editor.dom.getParent(listElm, 'ol,ul');
+    }
+
+    let style = editor.dom.getStyle(listElm, 'listStyleType');
+    if (style === '') {
+      style = 'default';
+    }
+    if (listElm.nodeName === 'OL') {
+      currentFormats.ol = style;
+    } else {
+      if (listElm.className === 'tox-checklist') {
+        currentFormats.cl = true;
+      } else {
+        currentFormats.ul = style;
+      }
+    }
+  }
+  reactNativeEventHandler('selectionchange', currentFormats);
+}
