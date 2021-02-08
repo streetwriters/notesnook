0f280b88e9a4926afe01ae87bf4b8ebd27fd0cb7
ammarahm-ed <ammarahmed6506@gmail.com>
2021-02-08 10:30:42 +0500

    fix Scroll Into View

--- apps/mobile/html/Web.bundle/site/init.js
+++ apps/mobile/html/Web.bundle/site/init.js
@@ -72,9 +72,10 @@ function init_tiny(size) {
       });
       editor.on('ScrollIntoView', (e) => {
         e.preventDefault();
-        let offset = e.elm.offsetTop + 60;
-        console.log(e)
-        document.scrollingElement.scrollTo({top:offset})
+        e.elm.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest',
+        });
       });
 
       editor.on('keyup', onChange);
