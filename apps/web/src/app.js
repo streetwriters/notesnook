16c312cddce9e8e13564cae87e982086eebcf79c
thecodrr <thecodrr@protonmail.com>
2021-02-16 01:17:32 +0500

    fix: ReactDOM is not defined

--- apps/web/src/app.js
+++ apps/web/src/app.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import "./app.css";
 import { Flex } from "rebass";
 import ThemeProvider from "./components/theme-provider";
