9bd649468e60ca142c1e04679efc8838f13a0b22
thecodrr <thecodrr@protonmail.com>
2021-02-15 19:35:12 +0500

    feat: use new logo in splash screen

--- apps/web/src/views/splash.js
+++ apps/web/src/views/splash.js
@@ -1,21 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Box, Flex, Text } from "rebass";
-import ThemeProvider from "../components/theme-provider";
+import { Flex } from "rebass";
 import Animated from "../components/animated";
+import Logo from "../assets/notesnook-logo.png";
 import { db } from "../common";
-import ProgressBar from "../components/progress-bar";
-import { getRandom } from "../utils/random";
 import { EV, EVENTS } from "notes-core/common";
 import { showToast } from "../utils/toast";
 import { captureException } from "@sentry/react";
-
-const loadingLines = [
-  "Ejecting bumfuzzled notes",
-  "Clearing up taradiddle vaccuums",
-  "Lollygagging all parachutes",
-  "Absquatulating the note engines",
-  "Personalizing nincompoop notebooks",
-];
 
 function Splash(props) {
   const [loading, setLoading] = useState(true);
@@ -47,52 +37,26 @@ function Splash(props) {
   }, [loading, animationEnded, props]);
 
   return (
-    <ThemeProvider>
-      <Flex
-        flexDirection="column"
-        bg="bgSecondary"
-        height="100%"
-        justifyContent="center"
-        alignItems="center"
-        overflow="hidden"
-      >
-        <Box>
-          <Flex
-            sx={{
-              fontSize: [42, 42, 52],
-              color: "primary",
-              fontWeight: "bold",
-              fontFamily: "body",
-            }}
-          >
-            <Animated.Text
-              backgroundColor="bgPrimary"
-              overflow={"hidden"}
-              x={0}
-            >
-              Notes
-            </Animated.Text>
-            <Animated.Flex backgroundColor="" overflow={"hidden"}>
-              <Animated.Text
-                initial={{ x: -100 * 1.2 }}
-                animate={{ x: 0 }}
-                transition={{ ease: "easeOut", duration: 1 }}
-              >
-                nook
-              </Animated.Text>
-            </Animated.Flex>
-          </Flex>
-          <ProgressBar
-            progress={100}
-            width={"100%"}
-            onLoadingEnd={() => setAnimationEnded(true)}
-          />
-        </Box>
-        <Text color="text" fontSize="title" mt={2}>
-          {loadingLines[getRandom(0, loadingLines.length)]}
-        </Text>
-      </Flex>
-    </ThemeProvider>
+    <Flex
+      flexDirection="column"
+      bg="#f0f0f0"
+      height="100%"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+    >
+      <Animated.Image
+        src={Logo}
+        width={150}
+        animate={{ scale: 1.05 }}
+        transition={{
+          repeat: 1,
+          repeatType: "reverse",
+          duration: 1,
+        }}
+        onAnimationComplete={() => setAnimationEnded(true)}
+      />
+    </Flex>
   );
 }
 export default Splash;
