64795228415730db7d4382a28fadf542603eab6d
thecodrr <thecodrr@protonmail.com>
2021-02-18 20:31:11 +0500

    fix: render null when nothing is returned in hashrouter

--- apps/web/src/components/hash-router/index.js
+++ apps/web/src/components/hash-router/index.js
@@ -1,10 +1,8 @@
-import React from "react";
 import useHashRoutes from "../../utils/use-hash-routes";
 import hashroutes from "../../navigation/hash-routes";
-import EditorPlaceholder from "../editor/-placeholder";
 
 function HashRouter() {
   const routeResult = useHashRoutes(hashroutes);
-  return routeResult || <EditorPlaceholder />;
+  return routeResult || null;
 }
 export default HashRouter;
