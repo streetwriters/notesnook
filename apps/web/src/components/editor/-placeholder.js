51358219d5e149d99e16e8c844404caa9ea6d971
thecodrr <thecodrr@protonmail.com>
2021-02-19 18:10:52 +0500

    feat: dark logo

--- apps/web/src/components/editor/-placeholder.js
+++ apps/web/src/components/editor/-placeholder.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Logo from "../../assets/logo.svg";
+import LogoDark from "../../assets/logo-dark.svg";
 import { Button, Flex, Image, Text } from "rebass";
 import { hashNavigate } from "../../navigation";
 import * as Icon from "../icons";
+import { useStore as useThemeStore } from "../../stores/theme-store";
 
 function EditorPlaceholder() {
+  const theme = useThemeStore((store) => store.theme);
   return (
     <Flex
       flexDirection="column"
@@ -11,7 +15,7 @@ function EditorPlaceholder() {
       justifyContent="center"
       alignItems="center"
     >
-      <Image src="./logo.svg" width={150} />
+      <Image src={theme === "dark" ? LogoDark : Logo} width={150} />
       <Text variant="body" textAlign="center" mt={2}>
         Please create or open a note to start editing.
       </Text>
