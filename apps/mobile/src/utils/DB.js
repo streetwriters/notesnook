ad073c7870e1ba11df88ea5951cc275d51633b63
ammarahm-ed <ammarahmed6506@gmail.com>
2021-02-23 11:47:43 +0500

    use prod server in dev mode

--- apps/mobile/src/utils/DB.js
+++ apps/mobile/src/utils/DB.js
@@ -9,12 +9,16 @@ export const db = new Database(
   Storage,
   Platform.OS === 'ios' ? EventSource : AndroidEventSource,
 );
-
-
-db.host( __DEV__ ? {
-  API_HOST: 'http://192.168.10.6:5264',
+/**
+ *   API_HOST: 'http://192.168.10.6:5264',
   AUTH_HOST: 'http://192.168.10.6:8264',
   SSE_HOST: 'http://192.168.10.6:7264',
+ */
+
+db.host( __DEV__ ? {
+  API_HOST: 'https://api.notesnook.com',
+  AUTH_HOST: 'https://auth.streetwriters.co',
+  SSE_HOST: 'https://events.streetwriters.co',
 } : {
   API_HOST: 'https://api.notesnook.com',
   AUTH_HOST: 'https://auth.streetwriters.co',
