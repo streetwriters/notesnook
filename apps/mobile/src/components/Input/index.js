325f78d22ad889bd349742ab92dd814bffabe502
ammarahm-ed <ammarahmed6506@gmail.com>
2021-03-06 14:38:26 +0500

    fix dialog templates

--- apps/mobile/src/components/Input/index.js
+++ apps/mobile/src/components/Input/index.js
@@ -47,10 +47,10 @@ const Input = ({
   const [showError, setShowError] = useState(false);
   const [errorList, setErrorList] = useState({
     SHORT_PASS: true,
-    NO_ABC: true,
-    NO_CAPS_ABC: true,
-    NO_NUM: true,
-    SPECIAL: true,
+  //  NO_ABC: true,
+  //  NO_CAPS_ABC: true,
+  //  NO_NUM: true,
+  //  SPECIAL: true,
   });
 
   const color = error
