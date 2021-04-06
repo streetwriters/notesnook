5d9136d30aeedfb875478d1c6c8e6ff0471d6e7e
thecodrr <thecodrr@protonmail.com>
2021-04-06 11:55:23 +0500

    feat: add default notes

--- apps/web/src/common/db.js
+++ apps/web/src/common/db.js
@@ -1,12 +1,14 @@
 import StorageInterface from "../interfaces/storage";
 import EventSource from "eventsource";
+import Config from "../utils/config";
+import http from "notes-core/utils/http";
 
 /**
  * @type {import("notes-core/api").default}
  */
 var db;
 function initializeDatabase() {
-  return import("notes-core/api").then(({ default: Database }) => {
+  return import("notes-core/api").then(async ({ default: Database }) => {
     db = new Database(StorageInterface, EventSource);
     // db.host({
     //   API_HOST: "https://api.notesnook.com",
@@ -20,16 +22,44 @@ function initializeDatabase() {
       SSE_HOST: "http://localhost:7264",
     });
 
-    return db.init();
+    await db.init();
+
+    if (!isAppHydrated()) {
+      setAppHydrated();
+      loadDefaultNotes(db);
+    }
   });
 }
 
 export { db, initializeDatabase };
 
-// export const db = new Database(StorageInterface, EventSource);
+function isAppHydrated() {
+  return Config.get("hydrated", false);
+}
 
-// // db.host({
-// //   API_HOST: "http://192.168.10.7:5264",
-// //   AUTH_HOST: "http://192.168.10.7:8264",
-// //   SSE_HOST: "http://192.168.10.7:7264",
-// // });
+function setAppHydrated() {
+  return Config.set("hydrated", true);
+}
+
+async function loadDefaultNotes(db) {
+  const notes = await http.get("/notes/index.json");
+  if (!notes) return;
+  let autoOpenId;
+  for (let note of notes) {
+    const content = await http.get(note.webContent);
+    let id = await db.notes.add({
+      title: note.title,
+      headline: note.headline,
+      localOnly: true,
+      content: { type: "tiny", data: content },
+    });
+    if (note.autoOpen) autoOpenId = id;
+  }
+
+  if (autoOpenId) quickNavigate(`/notes/${autoOpenId}/edit`);
+}
+
+function quickNavigate(url) {
+  window.history.replaceState(null, null, `#${url}`);
+  dispatchEvent(new HashChangeEvent("hashchange"));
+}
