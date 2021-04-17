f1a0831829cae1b4d9517b62c195f9295e37f749
ammarahm-ed <ammarahmed6506@gmail.com>
2021-04-18 00:47:18 +0500

    update version to 1372

--- apps/mobile/src/utils/index.js
+++ apps/mobile/src/utils/index.js
@@ -83,7 +83,7 @@ export const InteractionManager = {
   runAfterInteractions: (func, time = 400) => setTimeout(func, time),
 };
 
-export const APP_VERSION = 1371;
+export const APP_VERSION = 1372;
 
 export async function setSetting(settings, name, value) {
   let s = {...settings};
