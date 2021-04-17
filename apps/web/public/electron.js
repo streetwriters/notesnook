f43d6fee3cfc8b2b5e9431d39b0d983042166d8e
thecodrr <thecodrr@protonmail.com>
2021-04-17 13:23:12 +0500

    feat: improve electron app build

--- apps/web/public/electron.js
+++ apps/web/public/electron.js
@@ -1,29 +1,52 @@
-const { app, BrowserWindow } = require("electron");
-const serve = require("electron-serve");
-
-const loadURL = serve({
-  directory: __dirname,
-  scheme: "nn",
-  isCorsEnabled: false,
-});
+const { app, BrowserWindow, protocol } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const path = require("path");
+const url = require("url");
+const os = require("os");
 
 let mainWindow;
 
 async function createWindow() {
   mainWindow = new BrowserWindow({
     autoHideMenuBar: true,
+    icon: path.join(
+      __dirname,
+      os.platform() === "win32" ? "favicon.ico" : "favicon-32x32.png"
+    ),
   });
 
   mainWindow.maximize();
 
-  await loadURL(mainWindow);
+  // await loadURL(mainWindow);
+  mainWindow.loadURL(
+    url.format({
+      pathname:
+        "index.html" /* Attention here: origin is path.join(__dirname, 'index.html') */,
+      protocol: "file",
+      slashes: true,
+    })
+  );
 
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
 }
 
-app.on("ready", createWindow);
+app.commandLine.appendSwitch("lang", "en-US");
+app.on("ready", () => {
+  protocol.interceptFileProtocol(
+    "file",
+    (request, callback) => {
+      const url = request.url.substr(7); /* all urls start with 'file://' */
+      callback({ path: path.normalize(`${__dirname}/${url}`) });
+    },
+    (err) => {
+      if (err) console.error("Failed to register protocol");
+    }
+  );
+  autoUpdater.checkForUpdatesAndNotify().catch((e) => console.log(e));
+  createWindow();
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
