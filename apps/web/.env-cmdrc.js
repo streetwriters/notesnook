3d1e720632f06c76fd45748fe0e1de8790fda8ae
thecodrr <thecodrr@protonmail.com>
2021-04-24 19:44:01 +0500

    chore: use env-cmd for cross-platform env vars

---
+++ apps/web/.env-cmdrc.js
@@ -0,0 +1,17 @@
+const { execSync } = require("child_process");
+
+const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
+module.exports = {
+  all: {
+    REACT_APP_GIT_HASH: gitHash,
+  },
+  dev: {
+    REACT_APP_CI: true,
+  },
+  web: {
+    REACT_APP_PLATFORM: "web",
+  },
+  desktop: {
+    REACT_APP_PLATFORM: "desktop",
+  },
+};
