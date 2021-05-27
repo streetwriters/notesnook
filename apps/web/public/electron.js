c5a783cb085c0ea44ba1442bae8e3b9b2f1dd49d
thecodrr <thecodrr@protonmail.com>
2021-05-27 12:28:29 +0500

    feat: override https scheme for serving app on electron

--- apps/web/public/electron.js
+++ apps/web/public/electron.js
@@ -1,12 +1,24 @@
-const { app, BrowserWindow, protocol, ipcMain } = require("electron");
+const { app, protocol, BrowserWindow, ipcMain } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const path = require("path");
-const url = require("url");
+const fs = require("fs");
 const os = require("os");
-const isDev = require("electron-is-dev");
+const fetch = require("node-fetch").default;
+const FILE_NOT_FOUND = -6;
+
 try {
   require("electron-reloader")(module);
 } catch (_) {}
+
+const isDev = isDevelopment();
+const extensionToMimeType = {
+  html: "text/html",
+  json: "application/json",
+  js: "application/javascript",
+  css: "text/css",
+  svg: "image/svg+xml",
+  png: "image/png",
+};
 
 let mainWindow;
 
@@ -18,8 +30,8 @@ async function createWindow() {
       os.platform() === "win32" ? "app.ico" : "favicon-72x72.png"
     ),
     webPreferences: {
-      devTools: isDev,
-      nodeIntegration: true,
+      devTools: true, // isDev,
+      nodeIntegration: false, //true,
       preload: __dirname + "/preload.js",
     },
   });
@@ -31,14 +43,7 @@ async function createWindow() {
 
   // await loadURL(mainWindow);
   mainWindow.loadURL(
-    isDev
-      ? process.env.ELECTRON_START_URL
-      : url.format({
-          pathname:
-            "index.html" /* Attention here: origin is path.join(__dirname, 'index.html') */,
-          protocol: "file",
-          slashes: true,
-        })
+    isDev ? process.env.ELECTRON_START_URL : "https://notesnook"
   );
 
   mainWindow.on("closed", () => {
@@ -48,16 +53,45 @@ async function createWindow() {
 
 app.commandLine.appendSwitch("lang", "en-US");
 app.on("ready", () => {
-  protocol.interceptFileProtocol(
-    "file",
-    (request, callback) => {
-      const url = request.url.substr(7); /* all urls start with 'file://' */
-      callback({ path: path.normalize(`${__dirname}/${url}`) });
+  protocol.interceptStreamProtocol(
+    "https",
+    async (request, callback) => {
+      const url = new URL(request.url);
+      if (url.hostname === "notesnook") {
+        const absoluteFilePath = path.normalize(
+          `${__dirname}${url.pathname === "/" ? "/index.html" : url.pathname}`
+        );
+        const filePath = getPath(absoluteFilePath);
+        if (!filePath) {
+          callback({ error: FILE_NOT_FOUND });
+          return;
+        }
+        const fileExtension = path.extname(filePath).replace(".", "");
+
+        const data = fs.createReadStream(filePath);
+        callback({
+          data,
+          mimeType: extensionToMimeType[fileExtension],
+        });
+      } else {
+        const response = await fetch(request.url, {
+          ...request,
+          body: !!request.uploadData ? request.uploadData[0].bytes : null,
+          headers: { ...request.headers, origin: "https://app.notesnook.com/" },
+        });
+        callback({
+          data: response.body,
+          headers: response.headers,
+          mimeType: response.headers.get("Content-Type"),
+          statusCode: response.status,
+        });
+      }
     },
     (err) => {
       if (err) console.error("Failed to register protocol");
     }
   );
+
   autoUpdater.checkForUpdatesAndNotify().catch((e) => console.log(e));
   createWindow();
 });
@@ -77,3 +111,27 @@ app.on("activate", () => {
 ipcMain.on("fromRenderer", (event, ...args) => {
   console.log(event, ...args);
 });
+
+function isDevelopment() {
+  if (typeof electron === "string") {
+    throw new TypeError("Not running in an Electron environment!");
+  }
+
+  const isEnvSet = "ELECTRON_IS_DEV" in process.env;
+  const getFromEnv = Number.parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
+  return isEnvSet ? getFromEnv : !app.isPackaged;
+}
+
+function getPath(filePath) {
+  try {
+    const result = fs.statSync(filePath);
+
+    if (result.isFile()) {
+      return filePath;
+    }
+
+    if (result.isDirectory()) {
+      return getPath(path.join(filePath, "index.html"));
+    }
+  } catch (_) {}
+}
