df6e6ce8c9b23d2e369f8914bae42a18613c062c
thecodrr <thecodrr@protonmail.com>
2021-05-27 12:27:21 +0500

    feat: no service worker on desktop

--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -25,4 +25,5 @@ initializeDatabase().then(() => {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+if (process.env.REACT_APP_PLATFORM !== "desktop") serviceWorker.register();
+else serviceWorker.unregister();
