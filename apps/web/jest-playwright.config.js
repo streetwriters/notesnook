c836f33900e3445206c07ba39bea61e77716c630
thecodrr <thecodrr@protonmail.com>
2021-05-31 08:16:01 +0500

    ci: test first on chromium instead of firefox

--- apps/web/jest-playwright.config.js
+++ apps/web/jest-playwright.config.js
@@ -3,6 +3,7 @@ const IS_CI = true; // !!process.env.CI;
 module.exports = {
   launchOptions: {
     headless: IS_CI,
+    timeout: 10000,
   },
   serverOptions: {
     command: `yarn debug`,
@@ -10,6 +11,6 @@ module.exports = {
     launchTimeout: 10000,
     debug: true,
   },
-  browsers: IS_CI ? ["firefox", "chromium", "webkit"] : ["firefox"],
+  browsers: IS_CI ? ["chromium", "firefox", "webkit"] : ["firefox"],
   devices: [],
 };
