460e42fe692ab733df616f94432f7d7a1de81a4b
ammarahm-ed <ammarahmed6506@gmail.com>
2021-06-07 08:30:20 +0500

    update app version

--- apps/mobile/src/utils/index.js
+++ apps/mobile/src/utils/index.js
@@ -89,7 +89,7 @@ export const InteractionManager = {
   runAfterInteractions: (func, time = 300) => setTimeout(func, time),
 };
 
-export const APP_VERSION = 1375;
+export const APP_VERSION = 1376;
 
 export async function setSetting(settings, name, value) {
   let s = {...settings};
