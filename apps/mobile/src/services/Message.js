8027cb6e9e29fa09c6ecb6fdac9b523845dd77b0
ammarahm-ed <ammarahmed6506@gmail.com>
2021-06-07 09:51:00 +0500

    fix message board not showing

--- apps/mobile/src/services/Message.js
+++ apps/mobile/src/services/Message.js
@@ -1,60 +1,49 @@
-import { Actions } from '../provider/Actions';
 import { useMessageStore } from '../provider/stores';
 import { eOpenLoginDialog } from '../utils/Events';
 import { eSendEvent } from './EventManager';
 import PremiumService from './PremiumService';
 
 const loginMessage = {
-  type: Actions.MESSAGE_BOARD_STATE,
-  state: {
-    visible: true,
-    message: 'You are not logged in',
-    actionText: 'Login to encrypt and sync notes.',
-    onPress: () => {
-      eSendEvent(eOpenLoginDialog);
-    },
-    data: {},
-    icon: 'account-outline',
-    type: 'normal',
-  }
-}
+  visible: true,
+  message: 'You are not logged in',
+  actionText: 'Login to encrypt and sync notes.',
+  onPress: () => {
+    eSendEvent(eOpenLoginDialog);
+  },
+  data: {},
+  icon: 'account-outline',
+  type: 'normal',
+};
 
 export function setLoginMessage() {
-  useMessageStore.getState().setMessage(loginMessage)
-
+  useMessageStore.getState().setMessage(loginMessage);
 }
 
 const emailMessage = {
-  type: Actions.MESSAGE_BOARD_STATE,
-  state: {
-    visible: true,
-    message: 'Email not confirmed',
-    actionText: 'Please confrim your email to encrypt and sync notes.',
-    onPress: () => {
-      PremiumService.showVerifyEmailDialog();
-    },
-    data: {},
-    icon: 'alert',
-    type: 'error',
+  visible: true,
+  message: 'Email not confirmed',
+  actionText: 'Please confrim your email to encrypt and sync notes.',
+  onPress: () => {
+    PremiumService.showVerifyEmailDialog();
   },
-}
+  data: {},
+  icon: 'alert',
+  type: 'error',
+};
 
 export function setEmailVerifyMessage() {
-  useMessageStore.getState().setMessage(emailMessage)
+  useMessageStore.getState().setMessage(emailMessage);
 }
 
 const noMessage = {
-  type: Actions.MESSAGE_BOARD_STATE,
-  state: {
-    visible: false,
-    message: '',
-    actionText: '',
-    onPress: null,
-    data: {},
-    icon: 'account-outline',
-  },
-}
+  visible: false,
+  message: '',
+  actionText: '',
+  onPress: null,
+  data: {},
+  icon: 'account-outline',
+};
 
 export function clearMessage() {
-  useMessageStore.getState().setMessage(noMessage)
+  useMessageStore.getState().setMessage(noMessage);
 }
