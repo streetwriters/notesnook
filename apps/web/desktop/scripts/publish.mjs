d907f432668b5a98b9f457bf620456a502c7bf67
thecodrr <thecodrr@protonmail.com>
2021-07-07 01:33:43 +0500

    ci: use platform specific path seperator

--- apps/web/desktop/scripts/publish.mjs
+++ apps/web/desktop/scripts/publish.mjs
@@ -1,15 +1,17 @@
+const { sep } = require("path");
+
 console.log(`Script directory:`, __dirname);
 
-cd(`${__dirname}/../../`);
+cd(`${__dirname}${sep}..${sep}..${sep}`);
 
 await $`yarn install`;
 
 await $`yarn build:desktop`;
 
-cd(`${__dirname}/../`);
+cd(`${__dirname}${sep}..${sep}`);
 
-await $`yarn copyfiles -a ../build ./build`;
+await $`yarn copyfiles -a ..${sep}build .${sep}build`;
 
 await $`yarn bundle`;
 
-await $`electron-builder -c.extraMetadata.main=./build/electron.js`;
+await $`electron-builder -c.extraMetadata.main=.${sep}build${sep}electron.js`;
