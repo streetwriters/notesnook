d3b106094e48ba54ff53657952bede1ed1eb0f8c
thecodrr <thecodrr@protonmail.com>
2021-07-06 12:13:35 +0500

    refactor: migrate in serial

--- packages/core/database/migrator.js
+++ packages/core/database/migrator.js
@@ -3,31 +3,30 @@ import { migrations } from "../migrations";
 
 class Migrator {
   async migrate(collections, get, version) {
-    await Promise.all(
-      collections.map(async (collection) => {
-        if (!collection.index || !collection.dbCollection) return;
-        for (var i = 0; i < collection.index.length; ++i) {
-          let id = collection.index[i];
-          let item = get(id);
-          if (!item) {
-            continue;
-          }
-
-          if (item.deleted && !item.type) {
-            await collection.dbCollection?._collection?.addItem(item);
-            continue;
-          }
-          const migrate = migrations[version][item.type || collection.type];
-          if (migrate) item = migrate(item);
-
-          if (!!collection.dbCollection.merge) {
-            await collection.dbCollection.merge(item);
-          } else {
-            await collection.dbCollection.add(item);
-          }
+    for (let collection of collections) {
+      if (!collection.index || !collection.dbCollection) continue;
+      for (var i = 0; i < collection.index.length; ++i) {
+        let id = collection.index[i];
+        let item = get(id);
+        if (!item) {
+          continue;
         }
-      })
-    );
+
+        if (item.deleted && !item.type) {
+          await collection.dbCollection?._collection?.addItem(item);
+          continue;
+        }
+        const migrate = migrations[version][item.type || collection.type];
+        if (migrate) item = migrate(item);
+        item.migrated = true;
+
+        if (!!collection.dbCollection.merge) {
+          await collection.dbCollection.merge(item);
+        } else {
+          await collection.dbCollection.add(item);
+        }
+      }
+    }
     return true;
   }
 }
