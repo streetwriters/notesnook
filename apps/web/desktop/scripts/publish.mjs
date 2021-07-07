4ab9b867d58018824feb4ef1b9ea309d3e189717
thecodrr <thecodrr@protonmail.com>
2021-07-07 08:18:17 +0500

    ci: build only once for all three platforms

--- apps/web/desktop/scripts/publish.mjs
+++ apps/web/desktop/scripts/publish.mjs
@@ -1,18 +1,18 @@
 const { sep } = require("path");
-const mv = require("mvdir");
+// const mv = require("mvdir");
 
 console.log(`Script directory:`, __dirname);
 
-cd(`${__dirname}${sep}..${sep}..${sep}`);
+// cd(`${__dirname}${sep}..${sep}..${sep}`);
 
-await $`yarn install`;
+// await $`yarn install`;
 
-await $`yarn build:desktop`;
+// await $`yarn build:desktop`;
 
-cd(`${__dirname}${sep}..${sep}`);
+// cd(`${__dirname}${sep}..${sep}`);
 
-const err = await mv(`..${sep}build`, `.${sep}build`);
-if (err) throw new Error("Error moving build directory.");
+// const err = await mv(`..${sep}build`, `.${sep}build`);
+// if (err) throw new Error("Error moving build directory.");
 
 await $`esbuild .${sep}electron.js .${sep}preload.js --minify --external:electron --external:fsevents --bundle --outdir=.${sep}build --platform=node`;
 
