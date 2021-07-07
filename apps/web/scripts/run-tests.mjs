6a6b1aebe46876fe4dbabf2300deff20caff909a
thecodrr <thecodrr@protonmail.com>
2021-07-07 14:22:49 +0500

    ci: ignore args when running tests

--- apps/web/scripts/run-tests.mjs
+++ apps/web/scripts/run-tests.mjs
@@ -9,7 +9,7 @@ res.stdout.on("data", async (data) => {
   const message = data.toString();
   if (message.includes("create a production build, use yarn build.")) {
     console.log("Running on", res.child.pid, process.pid, process.ppid);
-    const testRes = await $`yarn playwright test ${args.join(" ")}`;
+    const testRes = await $`yarn playwright test`;
     process.exit(testRes.exitCode);
   }
 });
