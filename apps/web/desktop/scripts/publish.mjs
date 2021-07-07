e2f33f6f4ece355499039b2d62cc2a89bdf24c7c
thecodrr <thecodrr@protonmail.com>
2021-07-07 14:10:16 +0500

    ci: let action-electron-builder do the work

--- apps/web/desktop/scripts/publish.mjs
+++ apps/web/desktop/scripts/publish.mjs
@@ -1,20 +1,20 @@
-const { sep } = require("path");
-// const mv = require("mvdir");
+// const { sep } = require("path");
+// // const mv = require("mvdir");
 
-console.log(`Script directory:`, __dirname);
+// console.log(`Script directory:`, __dirname);
 
-// cd(`${__dirname}/../../`);
+// // cd(`${__dirname}/../../`);
 
-// await $`yarn install`;
+// // await $`yarn install`;
 
-// await $`yarn build:desktop`;
+// // await $`yarn build:desktop`;
 
-// cd(`${__dirname}/../`);
+// // cd(`${__dirname}/../`);
 
-// const err = await mv(`../build`, `./build`);
-// if (err) throw new Error("Error moving build directory.");
-await $`ls ./build`;
+// // const err = await mv(`../build`, `./build`);
+// // if (err) throw new Error("Error moving build directory.");
+// await $`ls ./build`;
 
-await $`esbuild ./electron.js ./preload.js --minify --external:electron --external:fsevents --bundle --outdir=./build --platform=node`;
+// await $`esbuild ./electron.js ./preload.js --minify --external:electron --external:fsevents --bundle --outdir=./build --platform=node`;
 
-await $`electron-builder -c.extraMetadata.main=./build/electron.js`;
+// await $`electron-builder -c.extraMetadata.main=./build/electron.js`;
