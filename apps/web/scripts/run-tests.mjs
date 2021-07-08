ef2a83c5755c3634964ecb26a35d33a099047e48
thecodrr <thecodrr@protonmail.com>
2021-07-08 13:23:39 +0500

    ci: properly kill react-server on tests complete

--- apps/web/scripts/run-tests.mjs
+++ apps/web/scripts/run-tests.mjs
@@ -1,15 +1,30 @@
+const find = require("find-process");
+
 const { p, u, f } = argv;
 
 const args = [];
 if (u) args.push("-u");
 if (f) args.push(f);
 
-const res = $`yarn debug -p ${p || 3000}`;
+const res = nothrow($`yarn debug -p ${p || 3000}`);
 res.stdout.on("data", async (data) => {
   const message = data.toString();
-  if (message.includes("create a production build, use yarn build.")) {
-    console.log("Running on", res.child.pid, process.pid, process.ppid);
-    const testRes = await $`yarn playwright test`;
+  if (
+    message.includes("create a production build, use yarn build.") ||
+    message.includes("Compiled with warnings")
+  ) {
+    const testRes = await $`yarn playwright test ${args.join(" ")}`;
+    await killServer();
     process.exit(testRes.exitCode);
   }
 });
+
+async function killServer() {
+  const nodeProcesses = await find("name", "node");
+  const reactServerProcesses = nodeProcesses.filter((p) =>
+    p.cmd.includes("react-scripts")
+  );
+  for (let rprocess of reactServerProcesses) {
+    process.kill(rprocess.pid, "SIGINT");
+  }
+}
