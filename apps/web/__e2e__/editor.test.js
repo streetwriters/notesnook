d707d02cbd5d558fab22c4a831e6555f297f0aec
thecodrr <thecodrr@protonmail.com>
2021-07-20 11:27:25 +0500

    test: more editor tests

--- apps/web/__e2e__/editor.test.js
+++ apps/web/__e2e__/editor.test.js
@@ -16,9 +16,63 @@ test("focus mode", async () => {
 
   await page.click(getTestId("focus-mode"));
 
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(500);
 
   expect(
     await page.screenshot({ fullPage: true, quality: 100, type: "jpeg" })
   ).toMatchSnapshot("focus-mode.jpg", { threshold: 99 });
+});
+
+test("dark mode in focus mode", async () => {
+  await createNote(NOTE, "notes");
+
+  await page.click(getTestId("focus-mode"));
+
+  await page.waitForTimeout(500);
+
+  await page.click(getTestId("dark-mode"));
+
+  await page.waitForTimeout(1000);
+
+  expect(
+    await page.screenshot({ fullPage: true, quality: 100, type: "jpeg" })
+  ).toMatchSnapshot("dark-focus-mode.jpg", { threshold: 99 });
+
+  await page.click(getTestId("dark-mode"));
+
+  await page.waitForTimeout(1000);
+
+  expect(
+    await page.screenshot({ fullPage: true, quality: 100, type: "jpeg" })
+  ).toMatchSnapshot("light-focus-mode.jpg", { threshold: 99 });
+});
+
+test("full screen in focus mode", async () => {
+  await createNote(NOTE, "notes");
+
+  await page.click(getTestId("focus-mode"));
+
+  await page.waitForTimeout(500);
+
+  await page.click(getTestId("enter-fullscreen"));
+
+  await page.waitForTimeout(100);
+
+  await page.click(getTestId("exit-fullscreen"));
+});
+
+test("normal mode from focus mode", async () => {
+  await createNote(NOTE, "notes");
+
+  await page.click(getTestId("focus-mode"));
+
+  await page.waitForTimeout(500);
+
+  await page.click(getTestId("normal-mode"));
+
+  await page.waitForTimeout(1000);
+
+  expect(
+    await page.screenshot({ fullPage: true, quality: 100, type: "jpeg" })
+  ).toMatchSnapshot("normal-mode-from-focus-mode.jpg", { threshold: 99 });
 });
