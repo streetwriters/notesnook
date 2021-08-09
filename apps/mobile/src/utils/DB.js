d45f7f29c964caef8087e3d44edcdf2636db2c8f
ammarahm-ed <ammarahmed6506@gmail.com>
2021-08-09 14:04:28 +0500

    update notes-core

--- apps/mobile/src/utils/DB.js
+++ apps/mobile/src/utils/DB.js
@@ -4,24 +4,20 @@ import EventSource from 'rn-eventsource';
 import AndroidEventSource from './event-source';
 import Database from 'notes-core/api/index';
 
-
-
 export const db = new Database(
   Storage,
   Platform.OS === 'ios' ? EventSource : AndroidEventSource,
 );
-/**
- *   API_HOST: 'http://192.168.10.6:5264',
-  AUTH_HOST: 'http://192.168.10.6:8264',
-  SSE_HOST: 'http://192.168.10.6:7264',
- */
 
 db.host( __DEV__ ? {
   API_HOST: 'https://api.notesnook.com',
   AUTH_HOST: 'https://auth.streetwriters.co',
   SSE_HOST: 'https://events.streetwriters.co',
+  SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co',
 } : {
   API_HOST: 'https://api.notesnook.com',
   AUTH_HOST: 'https://auth.streetwriters.co',
   SSE_HOST: 'https://events.streetwriters.co',
+  SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co',
+
 });
