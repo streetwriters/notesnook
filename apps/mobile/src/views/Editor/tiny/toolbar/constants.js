fde187e03d54c498c78eebded2e28fb370b28112
ammarahm-ed <ammarahmed6506@gmail.com>
2021-09-14 13:15:13 +0500

    fix rgba value not converting to hex

--- apps/mobile/src/views/Editor/tiny/toolbar/constants.js
+++ apps/mobile/src/views/Editor/tiny/toolbar/constants.js
@@ -49,27 +49,59 @@ export async function reFocusEditor() {
   }
 }
 
+function trim(str) {
+  return str.replace(/^\s+|\s+$/gm, '');
+}
+
 export function rgbToHex(color) {
-  color = '' + color;
-  if (!color || color.indexOf('rgb') < 0) {
-    return;
+  if (!color.startsWith('rgba')) {
+    color = '' + color;
+    if (!color || color.indexOf('rgb') < 0) {
+      return;
+    }
+
+    if (color.charAt(0) == '#') {
+      return color;
+    }
+
+    var nums = /(.*?)rgb\((\d+),\s*(\d+),\s*(\d+)\)/i.exec(color),
+      r = parseInt(nums[2], 10).toString(16),
+      g = parseInt(nums[3], 10).toString(16),
+      b = parseInt(nums[4], 10).toString(16);
+
+    return (
+      '#' +
+      ((r.length == 1 ? '0' + r : r) +
+        (g.length == 1 ? '0' + g : g) +
+        (b.length == 1 ? '0' + b : b))
+    );
+  } else {
+    let rgba = color;
+    var inParts = rgba.substring(rgba.indexOf('(')).split(','),
+      r = parseInt(trim(inParts[0].substring(1)), 10),
+      g = parseInt(trim(inParts[1]), 10),
+      b = parseInt(trim(inParts[2]), 10),
+      a = parseFloat(
+        trim(inParts[3].substring(0, inParts[3].length - 1))
+      ).toFixed(2);
+    var outParts = [
+      r.toString(16),
+      g.toString(16),
+      b.toString(16),
+      Math.round(a * 255)
+        .toString(16)
+        .substring(0, 2)
+    ];
+
+    // Pad single-digit output values
+    outParts.forEach(function (part, i) {
+      if (part.length === 1) {
+        outParts[i] = '0' + part;
+      }
+    });
+
+    return '#' + outParts.join('');
   }
-
-  if (color.charAt(0) == '#') {
-    return color;
-  }
-
-  var nums = /(.*?)rgb\((\d+),\s*(\d+),\s*(\d+)\)/i.exec(color),
-    r = parseInt(nums[2], 10).toString(16),
-    g = parseInt(nums[3], 10).toString(16),
-    b = parseInt(nums[4], 10).toString(16);
-
-  return (
-    '#' +
-    ((r.length == 1 ? '0' + r : r) +
-      (g.length == 1 ? '0' + g : g) +
-      (b.length == 1 ? '0' + b : b))
-  );
 }
 
 export const INPUT_MODE = {
@@ -204,5 +236,5 @@ export const TOOLBAR_ICONS = {
   imagefloatright: 'format-float-right',
   imagefloatnone: 'format-float-none',
   'line-break': 'keyboard-return',
-  code:"code-tags"
+  code: 'code-tags'
 };
