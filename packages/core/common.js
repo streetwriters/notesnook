21d5ca3cc29fb151843ee0717a98810fff03fedd
thecodrr <thecodrr@protonmail.com>
2021-10-16 09:07:18 +0500

    fix: remove async from sendAttachmentsProgressEvent

--- packages/core/common.js
+++ packages/core/common.js
@@ -10,7 +10,7 @@ export async function sendCheckUserStatusEvent(type) {
   return results.some((r) => r.type === type && r.result === true);
 }
 
-export async function sendAttachmentsProgressEvent(type, total, current) {
+export function sendAttachmentsProgressEvent(type, total, current) {
   EV.publish(EVENTS.attachmentsLoading, {
     type,
     total,
