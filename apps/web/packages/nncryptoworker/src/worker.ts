09942e7bc8b6bba5d21ac6f202a7b44eb4d6d1a8
thecodrr <thecodrr@protonmail.com>
2021-10-22 09:44:24 +0500

    chore: always import nncrypto dynamically

--- apps/web/packages/nncryptoworker/src/worker.ts
+++ apps/web/packages/nncryptoworker/src/worker.ts
@@ -1,20 +1,59 @@
-import { NNCrypto } from "nncrypto";
-import { SerializedKey } from "nncrypto/dist/src/types";
+import {
+  Cipher,
+  OutputFormat,
+  Plaintext,
+  SerializedKey,
+} from "nncrypto/dist/src/types";
 import { expose } from "comlink";
 import WorkerStream from "./workerstream";
+import { NNCrypto } from "nncrypto";
 
-const crypto = new NNCrypto();
+var crypto: NNCrypto | null = null;
+async function loadNNCrypto(): Promise<NNCrypto> {
+  if (crypto) return crypto;
+  const { NNCrypto } = await import("nncrypto");
+  return (crypto = new NNCrypto());
+}
 
 const module = {
-  exportKey: crypto.exportKey.bind(crypto),
-  deriveKey: crypto.deriveKey.bind(crypto),
-  encrypt: crypto.encrypt.bind(crypto),
-  decrypt: crypto.decrypt.bind(crypto),
-  hash: crypto.hash.bind(crypto),
-  createEncryptionStream: (id: string, key: SerializedKey) => {
+  exportKey: async function (password: string, salt: string) {
+    const crypto = await loadNNCrypto();
+    return crypto.exportKey(password, salt);
+  },
+  deriveKey: async function (password: string, salt: string) {
+    const crypto = await loadNNCrypto();
+    return crypto.deriveKey(password, salt);
+  },
+  hash: async function (password: string, salt: string) {
+    const crypto = await loadNNCrypto();
+    return crypto.hash(password, salt);
+  },
+  encrypt: async function (
+    key: SerializedKey,
+    plaintext: Plaintext,
+    outputFormat?: OutputFormat
+  ) {
+    const crypto = await loadNNCrypto();
+    return crypto.encrypt(key, plaintext, outputFormat);
+  },
+  decrypt: async function (
+    key: SerializedKey,
+    cipherData: Cipher,
+    outputFormat?: OutputFormat
+  ) {
+    const crypto = await loadNNCrypto();
+    return crypto.decrypt(key, cipherData, outputFormat);
+  },
+  createEncryptionStream: async function (id: string, key: SerializedKey) {
+    const crypto = await loadNNCrypto();
     return crypto.createEncryptionStream(key, new WorkerStream(id));
   },
-  createDecryptionStream: (id: string, iv: string, key: SerializedKey) => {
+  createDecryptionStream: async function (
+    id: string,
+    iv: string,
+    key: SerializedKey
+  ) {
+    const crypto = await loadNNCrypto();
     return crypto.createDecryptionStream(iv, key, new WorkerStream(id));
   },
 };
