75f56f07f416b67cc926b1a6f82a8dc6b999a9c1
thecodrr <thecodrr@protonmail.com>
2021-10-26 12:26:45 +0500

    fix: ensure only 1 worker is created per app instance

--- apps/web/src/interfaces/nncrypto.stub.js
+++ apps/web/src/interfaces/nncrypto.stub.js
@@ -1,13 +1,11 @@
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import "worker-loader?filename=static/workers/nncrypto.worker.js!nncryptoworker/dist/src/worker.js";
+
 const WORKER_PATH = "/static/workers/nncrypto.worker.js";
 
 async function loadNNCrypto() {
   const hasWorker = "Worker" in window || "Worker" in global;
   if (hasWorker) {
-    // eslint-disable-next-line import/no-webpack-loader-syntax
-    await import(
-      "worker-loader?filename=static/workers/nncrypto.worker.js!nncryptoworker/dist/src/worker.js"
-    );
-
     const { NNCryptoWorker } = await import("nncryptoworker");
     return NNCryptoWorker;
   } else {
@@ -17,12 +15,35 @@ async function loadNNCrypto() {
 }
 
 var instance = null;
+
 /**
  *
  * @returns {Promise<import("nncrypto/src/interfaces").INNCrypto>}
  */
-export async function getNNCrypto() {
+export function getNNCrypto() {
   if (instance) return instance;
-  const NNCrypto = await loadNNCrypto();
-  return (instance = new NNCrypto(WORKER_PATH));
+  return queueify(async () => {
+    const NNCrypto = await loadNNCrypto();
+    instance = new NNCrypto(WORKER_PATH);
+    return instance;
+  });
+}
+
+var processing = false;
+var queue = [];
+async function queueify(action) {
+  if (processing)
+    return new Promise((resolve) => {
+      queue.push(resolve);
+    });
+
+  processing = true;
+  const result = await action();
+  processing = false;
+
+  while (queue.length > 0) {
+    const resolve = queue.pop();
+    resolve(result);
+  }
+  return result;
 }
