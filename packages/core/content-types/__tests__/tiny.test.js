9e2efd632dfb9f1378c0a5b61af97c88748cd5ea
thecodrr <thecodrr@protonmail.com>
2021-11-03 18:44:26 +0500

    test: add test for Tiny.insertMedia

--- packages/core/content-types/__tests__/tiny.test.js
+++ packages/core/content-types/__tests__/tiny.test.js
@@ -10,3 +10,13 @@ test("img src is empty after extract attachments", async () => {
   expect(result.data).not.toContain(`src="data:image/png;`);
   expect(result.data).not.toContain(`src=`);
 });
+
+test("img src is present after insert attachments", async () => {
+  const tiny = new Tiny(IMG_CONTENT);
+  const result = await tiny.extractAttachments(async () => {
+    return { key: "hello", metadata: {} };
+  });
+  const tiny2 = new Tiny(result.data);
+  const result2 = await tiny2.insertMedia(() => "i am a data");
+  expect(result2).toContain(`src="i am a data"`);
+});
