6790f43b9a95abe2ca961379b667b04681f4b76e
thecodrr <thecodrr@protonmail.com>
2021-11-03 08:53:41 +0500

    chore: bump version to 1.6.0

--- apps/web/src/utils/useVersion.js
+++ apps/web/src/utils/useVersion.js
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import { db } from "../common/db";
-import config from "./config";
+// import { useEffect, useState } from "react";
+// import { db } from "../common/db";
+// import config from "./config";
 
 var APP_VERSION = {
   formatted: format(
-    1550,
+    1600,
     process.env.REACT_APP_GIT_HASH,
     process.env.REACT_APP_PLATFORM
   ),
-  numerical: 1550,
+  numerical: 1600,
   appUpdated: false,
   appUpdateable: false,
   changelog: undefined,
@@ -18,16 +18,16 @@ var APP_VERSION = {
 var CACHED_VERSION = undefined;
 
 function useVersion() {
-  const [version, setVersion] = useState(APP_VERSION);
+  // const [version, setVersion] = useState(APP_VERSION);
 
-  useEffect(() => {
-    (async function () {
-      const version = await getVersion();
-      setVersion(version);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async function () {
+  //     const version = await getVersion();
+  //     setVersion(version);
+  //   })();
+  // }, []);
 
-  return [version, APP_VERSION];
+  return [APP_VERSION, APP_VERSION];
 }
 export default useVersion;
 
@@ -52,36 +52,36 @@ export function getCachedVersion() {
   return CACHED_VERSION;
 }
 
-export async function getVersion() {
-  try {
-    var app_version = config.get("app_version");
-    if (!app_version) {
-      app_version = APP_VERSION;
-      config.set("app_version", APP_VERSION);
-    }
-    if (APP_VERSION.fetched) return CACHED_VERSION;
+// export async function getVersion() {
+//   try {
+//     var app_version = config.get("app_version");
+//     if (!app_version) {
+//       app_version = APP_VERSION;
+//       config.set("app_version", APP_VERSION);
+//     }
+//     if (APP_VERSION.fetched) return CACHED_VERSION;
 
-    const version = await db.version();
-    if (!version) return APP_VERSION;
+//     const version = await db.version();
+//     if (!version) return APP_VERSION;
 
-    const changelog = version.web_changelog || version.changelog;
-    CACHED_VERSION = {
-      formatted: format(version.web),
-      numerical: version.web,
-      appUpdated:
-        version.web > app_version.numerical &&
-        APP_VERSION.numerical === version.web,
-      appUpdateable: version.web > APP_VERSION.numerical,
-      updateSeverity: version.severity,
-      changelog: !changelog ? undefined : changelog,
-      fetched: true,
-    };
-    if (CACHED_VERSION.appUpdated) {
-      config.set("app_version", CACHED_VERSION);
-    }
-    return CACHED_VERSION;
-  } catch (e) {
-    console.error(e);
-  }
-  return APP_VERSION;
-}
+//     const changelog = version.web_changelog || version.changelog;
+//     CACHED_VERSION = {
+//       formatted: format(version.web),
+//       numerical: version.web,
+//       appUpdated:
+//         version.web > app_version.numerical &&
+//         APP_VERSION.numerical === version.web,
+//       appUpdateable: version.web > APP_VERSION.numerical,
+//       updateSeverity: version.severity,
+//       changelog: !changelog ? undefined : changelog,
+//       fetched: true,
+//     };
+//     if (CACHED_VERSION.appUpdated) {
+//       config.set("app_version", CACHED_VERSION);
+//     }
+//     return CACHED_VERSION;
+//   } catch (e) {
+//     console.error(e);
+//   }
+//   return APP_VERSION;
+// }
