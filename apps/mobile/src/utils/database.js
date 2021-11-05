3520ca680313a9e894726f96b65a0c22194919c0
ammarahm-ed <ammarahmed6506@gmail.com>
2021-11-05 11:55:03 +0500

    remove rn-eventsource lib

--- apps/mobile/src/utils/database.js
+++ apps/mobile/src/utils/database.js
@@ -1,6 +1,6 @@
 import {Platform} from 'react-native';
 import Storage from './storage';
-import EventSource from 'rn-eventsource';
+import EventSource from './even-source-ios';
 import AndroidEventSource from './event-source';
 import Database from 'notes-core/api/index';
 import filesystem from './filesystem';
@@ -14,10 +14,14 @@ export const db = new Database(
 db.host(
   __DEV__
     ? {
-        API_HOST: 'http://192.168.10.29:5264',
-        AUTH_HOST: 'http://192.168.10.29:8264',
-        SSE_HOST: 'http://192.168.10.29:7264',
-        SUBSCRIPTIONS_HOST: 'http://192.168.10.29:9264'
+      API_HOST: 'https://api.notesnook.com',
+        AUTH_HOST: 'https://auth.streetwriters.co',
+        SSE_HOST: 'https://events.streetwriters.co',
+        SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co'
+        // API_HOST: 'http://192.168.10.29:5264',
+        // AUTH_HOST: 'http://192.168.10.29:8264',
+        // SSE_HOST: 'http://192.168.10.29:7264',
+        // SUBSCRIPTIONS_HOST: 'http://192.168.10.29:9264'
       }
     : {
         API_HOST: 'https://api.notesnook.com',
