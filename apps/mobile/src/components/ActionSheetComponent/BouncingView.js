dab36aa01039386116035424216e7c96d38196c3
ammarahm-ed <ammarahmed6506@gmail.com>
2021-11-08 15:05:48 +0500

    animate dialogs on open

--- apps/mobile/src/components/ActionSheetComponent/BouncingView.js
+++ apps/mobile/src/components/ActionSheetComponent/BouncingView.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import Animated, { Easing, timing } from 'react-native-reanimated';
 
-export const BouncingView = ({children}) => {
+export const BouncingView = ({children,style,duration=600}) => {
   const scale = Animated.useValue(0.9);
  
   useEffect(() => {
     scale.setValue(0.9);
     timing(scale, {
       toValue: 1,
-      duration: 600,
+      duration:duration,
       easing: Easing.elastic(1)
     }).start();
     return () => {
@@ -18,13 +18,13 @@ export const BouncingView = ({children}) => {
 
   return (
     <Animated.View
-      style={{
+      style={[{
         transform: [
           {
             scale: scale
           }
         ]
-      }}>
+      },style]}>
       {children}
     </Animated.View>
   );
