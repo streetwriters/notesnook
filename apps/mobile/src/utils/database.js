1b7f1b6c7fbde450e271a8049da3a630b092d447
ammarahm-ed <ammarahmed6506@gmail.com>
2021-11-20 17:16:11 +0500

    update tinymce

--- apps/mobile/src/utils/database.js
+++ apps/mobile/src/utils/database.js
@@ -14,14 +14,14 @@ export const db = new Database(
 db.host(
   __DEV__
     ? {
-      API_HOST: 'https://api.notesnook.com',
-        AUTH_HOST: 'https://auth.streetwriters.co',
-        SSE_HOST: 'https://events.streetwriters.co',
-        SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co'
-        // API_HOST: 'http://192.168.10.29:5264',
-        // AUTH_HOST: 'http://192.168.10.29:8264',
-        // SSE_HOST: 'http://192.168.10.29:7264',
-        // SUBSCRIPTIONS_HOST: 'http://192.168.10.29:9264'
+      // API_HOST: 'https://api.notesnook.com',
+      //   AUTH_HOST: 'https://auth.streetwriters.co',
+      //   SSE_HOST: 'https://events.streetwriters.co',
+      //   SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co'
+        API_HOST: 'http://192.168.10.29:5264',
+        AUTH_HOST: 'http://192.168.10.29:8264',
+        SSE_HOST: 'http://192.168.10.29:7264',
+        SUBSCRIPTIONS_HOST: 'http://192.168.10.29:9264'
       }
     : {
         API_HOST: 'https://api.notesnook.com',
