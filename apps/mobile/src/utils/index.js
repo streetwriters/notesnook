3e6e33062b4b963d83b19477ef6d5676c2350ccd
ammarahm-ed <ammarahmed6506@gmail.com>
2021-12-06 21:16:35 +0500

    bump version to 1.6.91

--- apps/mobile/src/utils/index.js
+++ apps/mobile/src/utils/index.js
@@ -16,7 +16,7 @@ import {MMKV} from './mmkv';
 import {tabBarRef} from './Refs';
 import {SIZE} from './SizeUtils';
 
-export const APP_VERSION = 1690;
+export const APP_VERSION = 1691;
 
 export const Tracker = ackeeTracker.create('https://sa.streetwriters.co', {
   ignoreLocalhost: true
