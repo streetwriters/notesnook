78f6d183860313c153b627bfc2e1f4a000e77453
thecodrr <thecodrr@protonmail.com>
2021-12-09 09:18:40 +0500

    fix: do not remove br element in empty p tags

--- packages/tinymce-plugins/contenthandler/filters.js
+++ packages/tinymce-plugins/contenthandler/filters.js
@@ -3,8 +3,9 @@ const ATTRIBUTE_DELETE_FILTERS = [
   "[data-mce-src]",
   "[data-mce-style]",
   "[data-mce-type]",
+  "[data-progress]",
 ];
-const ELEMENT_DELETE_FILTERS = ["[data-mce-bogus]", "[data-mce-bookmark]"];
+const ELEMENT_DELETE_FILTERS = ["[data-mce-bogus]:not(br[data-mce-bogus])"];
 const ELEMENT_MODIFY_FILTERS = ["img[src]"];
 
 export const QUERY = [
@@ -19,6 +20,7 @@ export const ATTRIBUTES = {
     "data-mce-src",
     "data-mce-style",
     "data-mce-type",
+    "data-progress",
   ],
   elementDelete: ["data-mce-bogus", "data-mce-bookmark"],
 };
