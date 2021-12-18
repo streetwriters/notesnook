919710c0699d26ba3315e217286329ccecbdc540
thecodrr <thecodrr@protonmail.com>
2021-12-18 10:03:26 +0500

    refactor: seperate importer app & lib

--- packages/importer/app/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
--- packages/importer/lib/src/utils/hasher.ts
+++
@@ -1,9 +0,0 @@
-export interface IHasher {
-  type: string;
-  hash(data: Uint8Array | Buffer): Promise<string>;
-}
-
-const encoder = new TextEncoder();
-export function calculateHash(obj: any, hasher: IHasher): Promise<string> {
-  return hasher.hash(encoder.encode(JSON.stringify(obj)));
-}
