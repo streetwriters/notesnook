de16df60b00f1f04461ae323f99fe5e2d7f9f2da
ammarahm-ed <ammarahmed6506@gmail.com>
2021-12-22 09:17:16 +0500

    add type property in general sheet buttons

--- apps/mobile/src/components/GeneralSheet/index.js
+++ apps/mobile/src/components/GeneralSheet/index.js
@@ -153,7 +153,7 @@ const GeneralSheet = ({context}) => {
               key={item.accentText}
               title={item.actionText}
               icon={item.icon && item.icon}
-              type="accent"
+              type={item.type || "accent"}
               height={50}
               style={{
                 marginBottom: 10
