a72917159c7a3c9726139aab60d5896315740aab
ammarahm-ed <ammarahmed6506@gmail.com>
2021-12-23 09:43:00 +0500

    add name capturing groups regex transformer in babel

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-   "transform-remove-console"
+   //"transform-remove-console",
+   "@babel/plugin-transform-named-capturing-groups-regex"
   ]
 };
