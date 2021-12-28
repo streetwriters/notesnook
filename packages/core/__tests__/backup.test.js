3353d2235e12a992f29b30bfc0593622a9195ca5
thecodrr <thecodrr@protonmail.com>
2021-12-28 14:50:04 +0500

    feat: add migration for table & checklists

--- packages/core/__tests__/backup.test.js
+++ packages/core/__tests__/backup.test.js
@@ -5,6 +5,7 @@ import {
   notebookTest,
 } from "./utils";
 import v52Backup from "./__fixtures__/backup.v5.2.json";
+import v52BackupCopy from "./__fixtures__/backup.v5.2.copy.json";
 
 beforeEach(() => {
   StorageInterface.clear();
@@ -64,65 +65,81 @@ test("import tempered backup", () =>
     })
   ));
 
-describe.each([["v5.2", v52Backup]])(
-  "testing backup version: %s",
-  (version, data) => {
-    test(`import ${version} backup`, () => {
-      return databaseTest().then(async (db) => {
-        await db.backup.import(JSON.stringify(data));
+describe.each([
+  ["v5.2", v52Backup],
+  ["v5.2 copy", v52BackupCopy],
+])("testing backup version: %s", (version, data) => {
+  test(`import ${version} backup`, () => {
+    return databaseTest().then(async (db) => {
+      await db.backup.import(JSON.stringify(data));
 
-        expect(db.settings.raw.id).toBeDefined();
-        expect(db.settings.raw.dateModified).toBeDefined();
-        expect(db.settings.raw.dateEdited).toBeUndefined();
+      expect(db.settings.raw.id).toBeDefined();
+      expect(db.settings.raw.dateModified).toBeDefined();
+      expect(db.settings.raw.dateEdited).toBeUndefined();
 
+      expect(
+        db.notes.all.every((v) => {
+          const doesNotHaveContent = v.contentId && !v.content;
+          const doesNotHaveColors = !v.colors && (!v.color || v.color.length);
+          const hasTopicsInAllNotebooks =
+            !v.notebooks ||
+            v.notebooks.every((nb) => !!nb.id && !!nb.topics && !nb.topic);
+          const hasDateModified = v.dateModified > 0;
+          return (
+            doesNotHaveContent &&
+            !v.notebook &&
+            hasTopicsInAllNotebooks &&
+            doesNotHaveColors &&
+            hasDateModified
+          );
+        })
+      ).toBeTruthy();
+
+      expect(
+        db.notebooks.all.every((v) => v.title != null && v.dateModified > 0)
+      ).toBeTruthy();
+
+      expect(
+        db.attachments.all.every((v) => v.dateModified > 0 && !v.dateEdited)
+      ).toBeTruthy();
+
+      const allContent = await db.content.all();
+      expect(
+        allContent.every((v) => v.type === "tiny" || v.deleted)
+      ).toBeTruthy();
+      expect(allContent.every((v) => !v.persistDateEdited)).toBeTruthy();
+      expect(allContent.every((v) => v.dateModified > 0)).toBeTruthy();
+
+      expect(
+        allContent.every(
+          (v) =>
+            !v.data.includes("tox-checklist") &&
+            !v.data.includes("tox-checklist--checked")
+        )
+      ).toBeTruthy();
+
+      const tableContent = allContent.find((a) => a.data.includes("<table"));
+      if (tableContent)
         expect(
-          db.notes.all.every((v) => {
-            const doesNotHaveContent = v.contentId && !v.content;
-            const doesNotHaveColors = !v.colors && (!v.color || v.color.length);
-            const hasTopicsInAllNotebooks =
-              !v.notebooks ||
-              v.notebooks.every((nb) => !!nb.id && !!nb.topics && !nb.topic);
-            const hasDateModified = v.dateModified > 0;
-            return (
-              doesNotHaveContent &&
-              !v.notebook &&
-              hasTopicsInAllNotebooks &&
-              doesNotHaveColors &&
-              hasDateModified
-            );
-          })
-        ).toBeTruthy();
-
-        expect(
-          db.notebooks.all.every((v) => v.title != null && v.dateModified > 0)
-        ).toBeTruthy();
-
-        expect(
-          db.attachments.all.every((v) => v.dateModified > 0 && !v.dateEdited)
-        ).toBeTruthy();
-
-        const allContent = await db.content.all();
-        expect(
-          allContent.every((v) => v.type === "tiny" || v.deleted)
-        ).toBeTruthy();
-        expect(allContent.every((v) => !v.persistDateEdited)).toBeTruthy();
-        expect(allContent.every((v) => v.dateModified > 0)).toBeTruthy();
-      });
+          tableContent.data.includes(
+            `<div class="table-container" contenteditable="false">`
+          )
+        );
     });
+  });
 
-    test(`verify indices of ${version} backup`, () => {
-      return databaseTest().then(async (db) => {
-        await db.backup.import(JSON.stringify(data));
+  test(`verify indices of ${version} backup`, () => {
+    return databaseTest().then(async (db) => {
+      await db.backup.import(JSON.stringify(data));
 
-        verifyIndex(data, db, "notes", "notes");
-        verifyIndex(data, db, "notebooks", "notebooks");
-        verifyIndex(data, db, "content", "content");
-        verifyIndex(data, db, "attachments", "attachments");
-        // verifyIndex(data, db, "trash", "trash");
-      });
+      verifyIndex(data, db, "notes", "notes");
+      verifyIndex(data, db, "notebooks", "notebooks");
+      verifyIndex(data, db, "content", "content");
+      verifyIndex(data, db, "attachments", "attachments");
+      // verifyIndex(data, db, "trash", "trash");
     });
-  }
-);
+  });
+});
 
 function verifyIndex(backup, db, backupCollection, collection) {
   if (!backup.data[backupCollection]) return;
--- packages/core/migrations.js
+++ packages/core/migrations.js
@@ -1,3 +1,5 @@
+import { parseHTML } from "./utils/html-parser";
+
 export const migrations = {
   5.0: {},
   5.1: {},
@@ -7,7 +9,14 @@ export const migrations = {
     tag: replaceDateEditedWithDateModified(true),
     attachment: replaceDateEditedWithDateModified(true),
     trash: replaceDateEditedWithDateModified(),
-    tiny: replaceDateEditedWithDateModified(),
+    tiny: (item) => {
+      item = replaceDateEditedWithDateModified()(item);
+
+      if (!item.data || item.data.iv) return item;
+
+      item.data = removeToxClassFromChecklist(wrapTablesWithDiv(item.data));
+      return item;
+    },
     settings: replaceDateEditedWithDateModified(true),
   },
   5.3: {
@@ -28,4 +37,31 @@ function replaceDateEditedWithDateModified(removeDateEditedProperty = false) {
     delete item.persistDateEdited;
     return item;
   };
+}
+
+function wrapTablesWithDiv(html) {
+  const document = parseHTML(html);
+  const tables = document.getElementsByTagName("table");
+  for (let table of tables) {
+    table.setAttribute("contenteditable", "true");
+    table.replaceWith(
+      `<div class="table-container" contenteditable="false">${table.outerHTML}</div>`
+    );
+  }
+  return document.outerHTML || document.body.innerHTML;
+}
+
+function removeToxClassFromChecklist(html) {
+  const document = parseHTML(html);
+  const checklists = document.querySelectorAll(
+    ".tox-checklist,.tox-checklist--checked"
+  );
+
+  for (let item of checklists) {
+    if (item.classList.contains("tox-checklist--checked"))
+      item.classList.replace("tox-checklist--checked", "checked");
+    else if (item.classList.contains("tox-checklist"))
+      item.classList.replace("tox-checklist", "checklist");
+  }
+  return document.outerHTML || document.body.innerHTML;
 }
