4a6e3cf7749cd8a1f4561976640c3624afb7f779
thecodrr <thecodrr@protonmail.com>
2021-12-30 12:12:43 +0500

    fix: disable fetch timeout

--- packages/core/utils/http.js
+++ packages/core/utils/http.js
@@ -63,10 +63,9 @@ async function handleResponse(response) {
 
 async function request(url, token, method) {
   return handleResponse(
-    await fetchWithTimeout(url, {
+    await fetch(url, {
       method,
       headers: getAuthorizationHeader(token),
-      timeout: 20 * 1000,
     })
   );
 }
@@ -79,14 +78,13 @@ async function bodyRequest(
   contentType = "application/x-www-form-urlencoded"
 ) {
   return handleResponse(
-    await fetchWithTimeout(url, {
+    await fetch(url, {
       method,
       body: transformer(data, contentType),
       headers: {
         ...getAuthorizationHeader(token),
         "Content-Type": contentType,
       },
-      timeout: 20 * 1000,
     })
   );
 }
@@ -118,26 +116,26 @@ function errorTransformer(errorJson) {
   }
 }
 
-/**
- *
- * @param {RequestInfo} resource
- * @param {RequestInit} options
- * @returns
- */
-async function fetchWithTimeout(resource, options = {}) {
-  try {
-    const { timeout = 8000 } = options;
+// /**
+//  *
+//  * @param {RequestInfo} resource
+//  * @param {RequestInit} options
+//  * @returns
+//  */
+// async function fetchWithTimeout(resource, options = {}) {
+//   try {
+//     const { timeout = 8000 } = options;
 
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
-    const response = await fetch(resource, {
-      ...options,
-      signal: controller.signal,
-    });
-    clearTimeout(id);
-    return response;
-  } catch (e) {
-    if (e.name === "AbortError") throw new Error("Request timed out.");
-    throw e;
-  }
-}
+//     const controller = new AbortController();
+//     const id = setTimeout(() => controller.abort(), timeout);
+//     const response = await fetch(resource, {
+//       ...options,
+//       signal: controller.signal,
+//     });
+//     clearTimeout(id);
+//     return response;
+//   } catch (e) {
+//     if (e.name === "AbortError") throw new Error("Request timed out.");
+//     throw e;
+//   }
+// }
