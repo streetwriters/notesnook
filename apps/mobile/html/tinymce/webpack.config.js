91d2e6d10a60951a8b811e43e3030b12a453c4c3
ammarahm-ed <ammarahmed6506@gmail.com>
2022-01-05 13:00:36 +0500

    update tinymce

--- apps/mobile/html/tinymce/webpack.config.js
+++ apps/mobile/html/tinymce/webpack.config.js
@@ -14,7 +14,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader:"babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
       },
     ],
   },
