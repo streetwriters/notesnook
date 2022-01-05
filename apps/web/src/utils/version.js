18d740d84f10153ee09d5d30e49e4c2bab1efcf6
thecodrr <thecodrr@protonmail.com>
2022-01-05 13:32:10 +0500

    feat: get version changelog from github

--- apps/web/src/utils/version.js
+++ apps/web/src/utils/version.js
@@ -1,3 +1,5 @@
+import showdown from "showdown";
+
 export const appVersion = {
   formatted: format(
     process.env.REACT_APP_VERSION,
@@ -53,4 +55,28 @@ export function getServiceWorkerVersion(serviceWorker) {
     serviceWorker.postMessage({ type: "GET_VERSION" });
   });
 }
+
+var converter = new showdown.Converter();
+converter.setFlavor("github");
+
+export async function getChangelog(tag) {
+  try {
+    if (!tag) return;
+
+    const url = `https://api.github.com/repos/streetwriters/notesnook/releases/tags/v${tag}`;
+    const response = await fetch(url, {
+      headers: { Accept: "application/json" },
+    });
+    if (!response.ok) return "No changelog found.";
+
+    const release = await response.json();
+    if (!release) return "No changelog found.";
+
+    const { body } = release;
+
+    const html = converter.makeHtml(body);
+    return html;
+  } catch {
+    return "No changelog found.";
+  }
 }
