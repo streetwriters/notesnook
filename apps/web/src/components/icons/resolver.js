ababd02d2c9f2cec4a3660515ac20740f2d71036
thecodrr <thecodrr@protonmail.com>
2022-01-10 14:06:07 +0500

    chore: bump version to 1.7.5

--- apps/web/src/components/icons/resolver.js
+++ apps/web/src/components/icons/resolver.js
@@ -3,7 +3,6 @@ const { toTitleCase, toCamelCase } = require("../../utils/string");
 var icons = undefined;
 export function getIconFromAlias(alias) {
   if (!alias) return;
-
   const iconName = toTitleCase(toCamelCase(alias));
   if (!icons) icons = require("./index");
   return icons[iconName];
