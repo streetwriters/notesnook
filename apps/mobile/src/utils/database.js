c656c35ac2cdc6ef406cf0ac0c367483bff900db
ammarahm-ed <ammarahmed6506@gmail.com>
2022-01-17 10:10:04 +0500

    chore: add issue host in debug

--- apps/mobile/src/utils/database.js
+++ apps/mobile/src/utils/database.js
@@ -17,7 +17,8 @@ db.host(
         API_HOST: 'https://api.notesnook.com',
         AUTH_HOST: 'https://auth.streetwriters.co',
         SSE_HOST: 'https://events.streetwriters.co',
-        SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co'
+        SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co',
+        ISSUES_HOST: 'https://issues.streetwriters.co'
         // API_HOST: 'http://192.168.10.29:5264',
         // AUTH_HOST: 'http://192.168.10.29:8264',
         // SSE_HOST: 'http://192.168.10.29:7264',
@@ -28,6 +29,7 @@ db.host(
         API_HOST: 'https://api.notesnook.com',
         AUTH_HOST: 'https://auth.streetwriters.co',
         SSE_HOST: 'https://events.streetwriters.co',
-        SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co'
+        SUBSCRIPTIONS_HOST: 'https://subscriptions.streetwriters.co',
+        ISSUES_HOST: 'https://issues.streetwriters.co'
       }
 );
