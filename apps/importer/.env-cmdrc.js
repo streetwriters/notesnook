e2bd1cd1f195678956254afecefba45daec7c58c
thecodrr <thecodrr@protonmail.com>
2022-01-21 23:37:42 +0500

    chore(importer-web): bump version to 1.1.0

--- apps/importer/.env-cmdrc.js
+++ apps/importer/.env-cmdrc.js
@@ -1,9 +1,15 @@
 const { version } = require("./package.json");
+const fs = require("fs");
+const path = require("path");
 
 const APP_VERSION = version;
+const CHANGELOG = fs.readFileSync(path.join(__dirname, "changelog.txt"), {
+  encoding: "utf8",
+});
 module.exports = {
   all: {
     REACT_APP_VERSION: APP_VERSION,
+    REACT_APP_CHANGELOG: CHANGELOG,
   },
   release: {
     GENERATE_SOURCEMAP: false,
