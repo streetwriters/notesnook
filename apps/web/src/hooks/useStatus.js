5ae8ec77741b23dcc917522184efa291af6fc29c
thecodrr <thecodrr@protonmail.com>
2022-01-24 11:29:42 +0500

    fix: use immer when updating statuses

--- apps/web/src/hooks/useStatus.js
+++ apps/web/src/hooks/useStatus.js
@@ -1,20 +1,25 @@
 import create from "zustand";
+import produce from "immer";
 
 const useStatusStore = create((set) => ({
   statuses: {},
   updateStatus: ({ key, status, progress, icon }) =>
-    set((state) => {
-      if (!key) return;
-      const { statuses } = state;
-      const statusText = status || statuses[key]?.status;
-      statuses[key] = { key, status: statusText, progress, icon };
-    }),
+    set(
+      produce((state) => {
+        if (!key) return;
+        const { statuses } = state;
+        const statusText = status || statuses[key]?.status;
+        statuses[key] = { key, status: statusText, progress, icon };
+      })
+    ),
   removeStatus: (key) =>
-    set((state) => {
-      const { statuses } = state;
-      if (!key || !statuses[key]) return;
-      delete statuses[key];
-    }),
+    set(
+      produce((state) => {
+        const { statuses } = state;
+        if (!key || !statuses[key]) return;
+        delete statuses[key];
+      })
+    ),
 }));
 
 export default function useStatus() {
