dcd43b3ba3fa738199f3c9cc6cfce624b4b6369d
ammarahm-ed <ammarahmed6506@gmail.com>
2022-01-31 12:08:12 +0500

    update tinymce plugins

--- apps/mobile/html/tinymce/src/index.js
+++ apps/mobile/html/tinymce/src/index.js
@@ -1,34 +1,34 @@
-import "tinymce/tinymce";
-import "tinymce/icons/default";
-import "tinymce/themes/silver";
-import "tinymce/plugins/autoresize";
-import "tinymce/plugins/wordcount";
-import "tinymce/plugins/table";
-import "tinymce/plugins/media";
-import "tinymce/plugins/searchreplace";
-import "tinymce/plugins/image";
-import "tinymce/plugins/link";
-import "tinymce/plugins/lists";
-import "tinymce/plugins/autolink";
-import "tinymce/plugins/advlist";
-import "tinymce/plugins/imagetools";
-import "tinymce/plugins/directionality";
-import "tinymce/plugins/hr";
-import "tinymce/plugins/noneditable";
-import "tinymce/plugins/textpattern";
-import {processPastedContent} from "@streetwritersco/tinymce-plugins/codeblock";
-import "@streetwritersco/tinymce-plugins/codeblock";
-import "@streetwritersco/tinymce-plugins/checklist";
-import "@streetwritersco/tinymce-plugins/paste";
-import "@streetwritersco/tinymce-plugins/inlinecode"
-import "@streetwritersco/tinymce-plugins/keyboardquirks";
-import "@streetwritersco/tinymce-plugins/attachmentshandler";
-import "@streetwritersco/tinymce-plugins/blockescape";
-import "@streetwritersco/tinymce-plugins/contenthandler";
-import "@streetwritersco/tinymce-plugins/bettertable";
+import 'tinymce/tinymce';
+import 'tinymce/icons/default';
+import 'tinymce/themes/silver';
+import 'tinymce/plugins/autoresize';
+import 'tinymce/plugins/wordcount';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/autolink';
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/imagetools';
+import 'tinymce/plugins/directionality';
+import 'tinymce/plugins/hr';
+import 'tinymce/plugins/noneditable';
+import 'tinymce/plugins/textpattern';
+import { processPastedContent } from '@streetwriters/tinymce-plugins/codeblock';
+import '@streetwriters/tinymce-plugins/codeblock';
+import '@streetwriters/tinymce-plugins/checklist';
+import '@streetwriters/tinymce-plugins/paste';
+import '@streetwriters/tinymce-plugins/inlinecode';
+import '@streetwriters/tinymce-plugins/keyboardquirks';
+import '@streetwriters/tinymce-plugins/attachmentshandler';
+import '@streetwriters/tinymce-plugins/blockescape';
+import '@streetwriters/tinymce-plugins/contenthandler';
+import '@streetwriters/tinymce-plugins/bettertable';
 
 require.context(
-  "file-loader?name=[path][name].[ext]&context=node_modules/tinymce!tinymce/skins",
+  'file-loader?name=[path][name].[ext]&context=node_modules/tinymce!tinymce/skins',
   true,
   /.*/
 );
