d588162d1879c66737215ceb1ec65a7b2fb280c9
ammarahm-ed <ammarahmed6506@gmail.com>
2022-01-31 14:31:04 +0500

    re-enable removing console.logs

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-   //"transform-remove-console",
-   "@babel/plugin-transform-named-capturing-groups-regex"
-  ]
+  plugins: ['transform-remove-console', '@babel/plugin-transform-named-capturing-groups-regex']
 };
