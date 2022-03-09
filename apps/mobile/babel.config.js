7944761827d1ba690f3b3477f9060f776fc7afe8
ammarahm-ed <ammarahmed6506@gmail.com>
2022-03-09 10:09:45 +0500

    chore: use seperate babel config for dev/prod

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,4 +1,19 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['transform-remove-console', '@babel/plugin-transform-named-capturing-groups-regex']
+let env = process.env.BABEL_ENV;
+const configs = {
+  env: {
+    development: {
+      presets: ['module:metro-react-native-babel-preset'],
+      plugins: ['@babel/plugin-transform-named-capturing-groups-regex']
+    },
+    production: {
+      presets: ['module:metro-react-native-babel-preset'],
+      plugins: ['transform-remove-console', '@babel/plugin-transform-named-capturing-groups-regex']
+    }
+  }
+};
+module.exports = function (api, opts) {
+  api.cache(true);
+  if (!env) env = 'production';
+  console.log('babel-env:', env);
+  return configs.env[env];
 };
