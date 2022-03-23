2b8243b5a219e04a8065aa39087763336e0e9972
ammarahm-ed <ammarahmed6506@gmail.com>
2022-03-23 19:58:21 +0500

    update new version features

--- apps/mobile/features.ts
+++ apps/mobile/features.ts
@@ -2,11 +2,7 @@ import { FeatureType } from './src/components/sheets/new-feature';
 
 export const features: FeatureType[] = [
   {
-    title: 'Create duplicate notes',
-    body: 'You can now create exact duplicates of a note. Go to Note Properties > Duplicate note to try'
-  },
-  {
-    title: 'Manage syncing of individual notes',
-    body: 'Mark a note as local only to remove it from other devices and disable syncing. Any changes will not be synced'
+    title: 'Two-factor authentication',
+    body: 'Improved security for your account. Go to Settings to enable 2FA'
   }
 ];
