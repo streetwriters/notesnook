03ba7c5e668aa66318ba765c0abf3b6ef7d6f5df
thecodrr <thecodrr@protonmail.com>
2022-04-06 02:18:15 +0500

    chore: add subscription type to User

--- packages/core/types.js
+++ packages/core/types.js
@@ -20,6 +20,16 @@
  *      primaryMethod: string,
  *      secondaryMethod: string,
  *      remainingValidCodes: number
+ *  },
+ *  subscription: {
+ *      appId: 0,
+ *      cancelURL: string | null,
+ *      expiry: number,
+ *      productId: string,
+ *      provider: 0 | 1 | 2 | 3,
+ *      start: number,
+ *      type: 0 | 1 | 2 | 5 | 6 | 7,
+ *      updateURL: string | null,
  *  }
  * }} User
  */
