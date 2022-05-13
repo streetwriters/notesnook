ec07cab365ea4617164f6a7cd8da29a2a90f6326
thecodrr <thecodrr@protonmail.com>
2022-05-13 17:37:46 +0500

    feat(importer): update standardnotes types

--- packages/importer/src/providers/standardnotes/types.ts
+++ packages/importer/src/providers/standardnotes/types.ts
@@ -1,18 +1,30 @@
-import type { AppDataField, BackupFile } from "@standardnotes/snjs";
-import { ComponentArea, EditorFeatureDescription, NoteType } from "@standardnotes/features";
+import {
+  AppDataField,
+  BackupFile,
+  BackupFileDecryptedContextualPayload,
+  ItemContent,
+} from "@standardnotes/snjs";
+import {
+  ComponentArea,
+  EditorFeatureDescription,
+  NoteType,
+} from "@standardnotes/features";
 import { ContentType } from "@standardnotes/common";
-import {RawPayload, NoteContent, TagContent, ComponentContent} from "@standardnotes/snjs";
+import { NoteContent, TagContent, ComponentContent } from "@standardnotes/snjs";
 
 enum ProtocolVersion {
   V001 = "001",
   V002 = "002",
   V003 = "003",
-  V004 = "004"
+  V004 = "004",
 }
-const ComponentDataDomain = 'org.standardnotes.sn.components';
+const ComponentDataDomain = "org.standardnotes.sn.components";
 const DefaultAppDomain = "org.standardnotes.sn";
 
-type SNBackupItem<TContentType extends ContentType, TContent> = RawPayload & {
+type SNBackupItem<
+  TContentType extends ContentType,
+  TContent extends ItemContent
+> = BackupFileDecryptedContextualPayload<TContent> & {
   content_type: TContentType;
   content: TContent & {
     appData: {
@@ -25,9 +37,7 @@ type SNBackupItem<TContentType extends ContentType, TContent> = RawPayload & {
 export type SNNote = SNBackupItem<ContentType.Note, NoteContent>;
 export type SNComponent = SNBackupItem<ContentType.Component, ComponentContent>;
 export type SNTag = SNBackupItem<ContentType.Tag, TagContent>;
-export type SNBackup = {
-  items: RawPayload[];
-} & BackupFile;
+export type SNBackup = BackupFile;
 export type EditorDescription = Pick<
   EditorFeatureDescription,
   "note_type" | "file_type"
@@ -41,7 +51,10 @@ export type Spreadsheet = {
 };
 
 export {
-  DefaultAppDomain, ComponentDataDomain, ProtocolVersion,
+  DefaultAppDomain,
+  ComponentDataDomain,
+  ProtocolVersion,
   ContentType,
-  ComponentArea, NoteType
+  ComponentArea,
+  NoteType,
 };
