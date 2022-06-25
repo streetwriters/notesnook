82be67613255905cbf5acacb1db30aadc1d7757b
thecodrr <thecodrr@protonmail.com>
2022-06-25 10:26:35 +0500

    feat: make toolbar configurable

--- packages/editor/dist/toolbar/toolbar.js
+++ packages/editor/dist/toolbar/toolbar.js
@@ -17,44 +17,44 @@ import { useEffect } from "react";
 import { useToolbarStore, } from "./stores/toolbar-store";
 import { ToolbarGroup } from "./components/toolbar-group";
 import { EditorContext, PopupRenderer, } from "../components/popup-presenter/popuprenderer";
+var DEFAULT_TOOLS = [
+    [
+        "insertBlock",
+        "tableSettings",
+        "imageSettings",
+        "embedSettings",
+        "attachmentSettings",
+        "linkSettings",
+        "codeRemove",
+    ],
+    [
+        "bold",
+        "italic",
+        "underline",
+        [
+            "strikethrough",
+            "code",
+            "subscript",
+            "superscript",
+            "highlight",
+            "textColor",
+        ],
+    ],
+    ["fontSize"],
+    ["headings", "fontFamily"],
+    ["numberedList", "bulletList"],
+    ["addLink"],
+    ["alignCenter", ["alignLeft", "alignRight", "alignJustify", "ltr", "rtl"]],
+    ["clearformatting"],
+];
 export function Toolbar(props) {
-    var editor = props.editor, theme = props.theme, location = props.location, isMobile = props.isMobile;
+    var editor = props.editor, theme = props.theme, location = props.location, isMobile = props.isMobile, _a = props.tools, tools = _a === void 0 ? DEFAULT_TOOLS : _a;
     var setIsMobile = useToolbarStore(function (store) { return store.setIsMobile; });
     var setToolbarLocation = useToolbarStore(function (store) { return store.setToolbarLocation; });
     useEffect(function () {
         setIsMobile(isMobile || false);
         setToolbarLocation(location);
     }, [isMobile, location]);
-    var tools = [
-        [
-            "insertBlock",
-            "tableSettings",
-            "imageSettings",
-            "embedSettings",
-            "attachmentSettings",
-            "linkSettings",
-            "codeRemove",
-        ],
-        [
-            "bold",
-            "italic",
-            "underline",
-            [
-                "strikethrough",
-                "code",
-                "subscript",
-                "superscript",
-                "highlight",
-                "textColor",
-            ],
-        ],
-        ["fontSize"],
-        ["headings", "fontFamily"],
-        ["numberedList", "bulletList"],
-        ["addLink"],
-        ["alignCenter", ["alignLeft", "alignRight", "alignJustify", "ltr", "rtl"]],
-        ["clearformatting"],
-    ];
     if (!editor)
         return null;
     return (_jsx(ThemeProvider, __assign({ theme: theme }, { children: _jsx(EditorContext.Provider, __assign({ value: editor }, { children: _jsxs(PopupRenderer, __assign({ editor: editor }, { children: [_jsx(Flex, __assign({ className: "editor-toolbar", sx: { flexWrap: ["nowrap", "wrap"], overflowX: ["auto", "hidden"] } }, { children: tools.map(function (tools) {
--- packages/editor/src/toolbar/toolbar.tsx
+++ packages/editor/src/toolbar/toolbar.tsx
@@ -23,23 +23,48 @@ import {
   EditorContext,
   PopupRenderer,
 } from "../components/popup-presenter/popuprenderer";
-// type Colors = {
-//   text: string;
-//   background: string;
-//   bgSecondary: string;
-//   primary: string;
-//   hover: string;
-//   border: string;
-// };
 
 type ToolbarProps = {
   theme: Theme;
   editor: Editor | null;
   location: ToolbarLocation;
   isMobile?: boolean;
+  tools?: ToolbarDefinition;
 };
+
+const DEFAULT_TOOLS: ToolbarDefinition = [
+  [
+    "insertBlock",
+    "tableSettings",
+    "imageSettings",
+    "embedSettings",
+    "attachmentSettings",
+    "linkSettings",
+    "codeRemove",
+  ],
+  [
+    "bold",
+    "italic",
+    "underline",
+    [
+      "strikethrough",
+      "code",
+      "subscript",
+      "superscript",
+      "highlight",
+      "textColor",
+    ],
+  ],
+  ["fontSize"],
+  ["headings", "fontFamily"],
+  ["numberedList", "bulletList"],
+  ["addLink"],
+  ["alignCenter", ["alignLeft", "alignRight", "alignJustify", "ltr", "rtl"]],
+  ["clearformatting"],
+];
+
 export function Toolbar(props: ToolbarProps) {
-  const { editor, theme, location, isMobile } = props;
+  const { editor, theme, location, isMobile, tools = DEFAULT_TOOLS } = props;
   const setIsMobile = useToolbarStore((store) => store.setIsMobile);
   const setToolbarLocation = useToolbarStore(
     (store) => store.setToolbarLocation
@@ -49,37 +74,6 @@ export function Toolbar(props: ToolbarProps) {
     setIsMobile(isMobile || false);
     setToolbarLocation(location);
   }, [isMobile, location]);
-
-  const tools: ToolbarDefinition = [
-    [
-      "insertBlock",
-      "tableSettings",
-      "imageSettings",
-      "embedSettings",
-      "attachmentSettings",
-      "linkSettings",
-      "codeRemove",
-    ],
-    [
-      "bold",
-      "italic",
-      "underline",
-      [
-        "strikethrough",
-        "code",
-        "subscript",
-        "superscript",
-        "highlight",
-        "textColor",
-      ],
-    ],
-    ["fontSize"],
-    ["headings", "fontFamily"],
-    ["numberedList", "bulletList"],
-    ["addLink"],
-    ["alignCenter", ["alignLeft", "alignRight", "alignJustify", "ltr", "rtl"]],
-    ["clearformatting"],
-  ];
 
   if (!editor) return null;
   return (
