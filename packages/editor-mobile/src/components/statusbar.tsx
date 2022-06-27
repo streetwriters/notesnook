75561ed03196c41595233de5c755034ff084cd66
Ammar Ahmed <ammarahmed6506@gmail.com>
2022-06-27 18:37:12 +0500

    feat: sticky statusbar

--- packages/editor-mobile/src/components/statusbar.tsx
+++ packages/editor-mobile/src/components/statusbar.tsx
@@ -1,17 +1,47 @@
-import { Editor } from "@tiptap/react";
-import { useRef, useState } from "react";
+import { Editor } from "notesnook-editor/dist/types";
+import React, { RefObject, useEffect, useRef, useState } from "react";
 
-export default function StatusBar({ editor }: { editor: Editor | null }) {
+export default function StatusBar({
+  editor,
+  container,
+}: {
+  editor: Editor | null;
+  container: RefObject<HTMLDivElement>;
+}) {
   const [status, setStatus] = useState({
     date: "",
     saved: "",
   });
+  const [sticky, setSticky] = useState(false);
+  const prevScroll = useRef(0);
+  const lastStickyChangeTime = useRef(0);
   const words = editor?.storage?.characterCount?.words() + " words";
   const statusBar = useRef({
     set: setStatus,
   });
   //@ts-ignore
   globalThis.statusBar = statusBar;
+
+  const onScroll = React.useCallback((event) => {
+    //@ts-ignore
+    const currentOffset = event.target.scrollTop;
+    if (Date.now() - lastStickyChangeTime.current < 300) return;
+    if (currentOffset > prevScroll.current) {
+      setSticky(false);
+    } else {
+      setSticky(true);
+    }
+    lastStickyChangeTime.current = Date.now();
+    prevScroll.current = currentOffset;
+  }, []);
+
+  useEffect(() => {
+    const node = container.current;
+    node?.addEventListener("scroll", onScroll);
+    return () => {
+      node?.removeEventListener("scroll", onScroll);
+    };
+  }, [onScroll, container]);
 
   const paragraphStyle = {
     marginTop: 0,
@@ -26,9 +56,13 @@ export default function StatusBar({ editor }: { editor: Editor | null }) {
       style={{
         flexDirection: "row",
         display: "flex",
-        height: 15,
+        height: sticky ? 20 : 15,
         paddingRight: 12,
         paddingLeft: 12,
+        position: sticky ? "sticky" : "relative",
+        top: 0,
+        backgroundColor: "var(--nn_bg)",
+        zIndex: 1,
       }}
     >
       <p style={paragraphStyle}>{words}</p>
