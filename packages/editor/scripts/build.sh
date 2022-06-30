1d5d2f159c70271fd18d8d65141f21070910d9eb
thecodrr <thecodrr@protonmail.com>
2022-06-30 17:44:27 +0500

    chore: copy katex fonts

--- packages/editor/scripts/build.sh
+++ packages/editor/scripts/build.sh
@@ -1,5 +1,7 @@
 cp ./node_modules/katex/dist/katex.min.css $(realpath ./styles/)
+cp -r ./node_modules/katex/dist/fonts/ $(realpath ./styles/)
 cp ./node_modules/prism-themes/themes/prism-dracula.min.css $(realpath ./styles/)/prism-theme.css
+
 ln -s $(realpath ./src/styles.css) $(realpath ./styles/)
 
 yarn tsc --watch
