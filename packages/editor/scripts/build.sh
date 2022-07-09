b64a3a0b296340311976ddeec83d4576f5b8e6a0
thecodrr <thecodrr@protonmail.com>
2022-07-09 21:26:32 +0500

    feat: use Hack font for monospace text

--- packages/editor/scripts/build.sh
+++ packages/editor/scripts/build.sh
@@ -1,7 +1,8 @@
 cp ./node_modules/katex/dist/katex.min.css $(realpath ./styles/)
 cp -r ./node_modules/katex/dist/fonts/ $(realpath ./styles/)
 cp ./node_modules/prism-themes/themes/prism-dracula.min.css $(realpath ./styles/)/prism-theme.css
-
-ln -s $(realpath ./src/styles.css) $(realpath ./styles/)
+cp ./node_modules/prism-themes/themes/prism-dracula.min.css $(realpath ./styles/)/prism-theme.css
+cp $(realpath ./src/fonts.css) $(realpath ./styles/)/fonts.css
+cp $(realpath ./src/styles.css) $(realpath ./styles/)/styles.css
 
 yarn tsc --watch
