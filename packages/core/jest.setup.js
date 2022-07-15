854ffaa8c44e7b1300543cfbb9cd51aa765db8df
thecodrr <thecodrr@protonmail.com>
2022-07-15 09:44:47 +0500

    test: use linkedom in jest for tests

--- packages/core/jest.setup.js
+++ packages/core/jest.setup.js
@@ -1,8 +1,10 @@
 var nodeCrypto = require("crypto");
 var dotenv = require("dotenv");
 const fetch = require("node-fetch");
+const linkedom = require("linkedom");
+globalThis.DOMParser = linkedom.DOMParser;
 globalThis.fetch = fetch;
-require("abortcontroller-polyfill/dist/polyfill-patch-fetch")
+require("abortcontroller-polyfill/dist/polyfill-patch-fetch");
 
 dotenv.config();
 
