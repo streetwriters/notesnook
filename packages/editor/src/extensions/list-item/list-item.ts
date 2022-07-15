bb4fd135a43e9bb02c68f195a483bbff5ef0bbb9
thecodrr <thecodrr@protonmail.com>
2022-07-15 18:41:22 +0500

    fix: allow tabbing inside codeblock that is in a list

--- packages/editor/src/extensions/list-item/list-item.ts
+++ packages/editor/src/extensions/list-item/list-item.ts
@@ -5,6 +5,16 @@ export const ListItem = TiptapListItem.extend({
   addKeyboardShortcuts() {
     return {
       ...this.parent?.(),
+      Tab: (props) => {
+        const { editor } = props;
+        const { state } = editor;
+        const { selection } = state;
+        const { $from } = selection;
+
+        if ($from.parent.type.name === "codeblock") return false;
+
+        return this.parent?.()?.Tab(props) || false;
+      },
       Backspace: ({ editor }) =>
         onBackspacePressed(editor, this.name, this.type),
     };
