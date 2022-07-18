d1589c0b808081107f61b98e17c1e377e6d87876
ammarahm-ed <ammarahmed6506@gmail.com>
2022-07-18 22:50:42 +0500

    chore: add new features ios

--- apps/mobile/features.ts
+++ apps/mobile/features.ts
@@ -1,3 +1,9 @@
 import { FeatureType } from './src/components/sheets/new-feature';
 
-export const features: FeatureType[] = [];
+export const features: FeatureType[] = [
+  {
+    title: 'Swipe to close editor',
+    body: 'Now you can swipe from the left edge to close the editor',
+    platform: 'ios'
+  }
+];
