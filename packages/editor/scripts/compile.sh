29a9f7da27d22aecf0a406c47f80c885059abeef
thecodrr <thecodrr@protonmail.com>
2022-07-27 18:28:25 +0500

    fix: use % instead of vw for setting max-width

--- packages/editor/scripts/compile.sh
+++ packages/editor/scripts/compile.sh
@@ -4,8 +4,6 @@ cp ./node_modules/katex/dist/katex.min.css $(realpath ./styles/)
 cp -r ./node_modules/katex/dist/fonts/ $(realpath ./styles/)
 cp ./node_modules/prism-themes/themes/prism-dracula.min.css $(realpath ./styles/)/prism-theme.css
 cp ./node_modules/prism-themes/themes/prism-dracula.min.css $(realpath ./styles/)/prism-theme.css
-cp $(realpath ./src/fonts.css) $(realpath ./styles/)/fonts.css
-cp $(realpath ./src/styles.css) $(realpath ./styles/)/styles.css
 
 yarn tsc --module commonjs --outDir ./dist/cjs --target es2015
 yarn tsc --module esnext --outDir ./dist/es --target es6
