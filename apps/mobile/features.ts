8c9d886119f077e90373f29e8888bb818cd5e650
ammarahm-ed <ammarahmed6506@gmail.com>
2022-08-08 11:17:54 +0500

    bump version to 2.1.6

--- apps/mobile/features.ts
+++ apps/mobile/features.ts
@@ -1,12 +1,3 @@
 import { FeatureType } from './src/components/sheets/new-feature';
 
-export const features: FeatureType[] = [
-  {
-    title: 'Configurable line spacing',
-    body: 'Many of you requested to be able to change line spacing on pressing enter. Now you can go to settings & configure line spacing in Editor section'
-  },
-  {
-    title: 'Disabled image compression',
-    body: 'Due to some performance reasons, previously image compression was enabled. From now on, images will be uploaded, as is in full quality.'
-  }
-];
+export const features: FeatureType[] = [];
--- apps/mobile/version.js
+++ apps/mobile/version.js
@@ -1,1 +1,1 @@
-export const APP_VERSION = 215;
+export const APP_VERSION = 216;
