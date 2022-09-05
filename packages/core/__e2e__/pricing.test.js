289e6eefb326eb402660c6a74bab54fd645adcc0
Abdullah Atta <abdullahatta@streetwriters.co>
2022-09-05 18:00:33 +0500

    core: move pricing test to e2e tests

--- packages/core/__e2e__/pricing.test.js
+++ packages/core/__e2e__/pricing.test.js
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import Pricing from "../pricing";
+import Pricing from "../api/pricing";
 
 test.each(["monthly", "yearly", undefined])(`get %s price`, async (period) => {
   const pricing = new Pricing();
