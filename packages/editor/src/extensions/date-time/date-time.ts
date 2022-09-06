0a3ef4bc0a0241881563edf4a7a15842653f5270
ammarahm-ed <ammarahmed6506@gmail.com>
2022-09-06 17:17:48 +0500

    editor: fix date is off by one month

--- packages/editor/src/extensions/date-time/date-time.ts
+++ packages/editor/src/extensions/date-time/date-time.ts
@@ -114,7 +114,7 @@ function currentDate() {
 
 function getISO8601Date(): string {
   const year = new Date().getFullYear();
-  const month = new Date().getMonth();
+  const month = new Date().getMonth() + 1;
   const day = new Date().getDate();
   return `${year}-${month}-${day}`;
 }
