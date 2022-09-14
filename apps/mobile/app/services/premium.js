708b0fc0f0f93f23f7359fdb5d4c22b0b706acf8
Abdullah Atta <abdullahatta@streetwriters.co>
2022-09-14 12:17:45 +0500

    mobile: clear `databaseSync` check references

--- apps/mobile/app/services/premium.js
+++ apps/mobile/app/services/premium.js
@@ -159,10 +159,6 @@ const onUserStatusCheck = async (type) => {
           desc: "With Notesnook Pro you can add notes to your vault and do so much more! Get it now."
         };
         break;
-      case CHECK_IDS.databaseSync:
-        message = null;
-        status = true;
-        break;
     }
 
     if (message) {
