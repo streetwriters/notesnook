0bd3310f23d1f8f1fc9518a2c5c801d4448f3b7b
Abdullah Atta <abdullahatta@streetwriters.co>
2022-10-20 14:08:25 +0500

    editor: load prism lang grammars from local files

--- packages/editor/src/extensions/code-block/loader.ts
+++ packages/editor/src/extensions/code-block/loader.ts
@@ -17,42 +17,15 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Syntax } from "refractor";
-
-const loadedLanguages: Record<string, Syntax | undefined> = {};
+const loadedLanguages: Record<string, boolean> = {};
 export function isLanguageLoaded(name: string) {
   return !!loadedLanguages[name];
 }
 export async function loadLanguage(shortName: string) {
-  if (loadedLanguages[shortName]) return loadedLanguages[shortName];
+  const { default: language } = await import(
+    `../../../languages/${shortName}.js`
+  );
 
-  const url = `https://esm.sh/refractor@4.7.0/lang/${shortName}.js?bundle=true`;
-  const result = await loadScript(shortName, url);
-  loadedLanguages[shortName] = result;
-  return result;
-}
-
-async function loadScript(id: string, url: string) {
-  return new Promise<Syntax>((resolve) => {
-    const callbackName = `on${id}Loaded`;
-    const script = document.createElement("script");
-    script.type = "module";
-    script.innerHTML = `
-    import LanguageDefinition from "${url}";
-    if (window["${callbackName}"]) {
-      window["${callbackName}"](LanguageDefinition)
-    }
-`;
-    (window as unknown as Record<string, unknown>)[callbackName] = (
-      lang: Syntax
-    ) => {
-      script.remove();
-      (window as unknown as Record<string, unknown>)[callbackName] = null;
-
-      resolve(lang);
-    };
-
-    // Append to the `head` element
-    document.head.appendChild(script);
-  });
+  loadedLanguages[shortName] = true;
+  return language;
 }
