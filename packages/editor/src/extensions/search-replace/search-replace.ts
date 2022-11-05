3c4df375f92b3523ff1f3985981c6ec87281373c
Abdullah Atta <abdullahatta@streetwriters.co>
2022-11-05 15:47:13 +0500

    Update packages/editor/src/extensions/search-replace/search-replace.ts
    Signed-off-by: Abdullah Atta <thecodrr@protonmail.com>

--- packages/editor/src/extensions/search-replace/search-replace.ts
+++ packages/editor/src/extensions/search-replace/search-replace.ts
@@ -385,9 +385,10 @@ function scrollIntoView(editor: Editor, from: number) {
   let { node: domNode } = editor.view.domAtPos(from);
   if (domNode.nodeType === Node.TEXT_NODE && domNode.parentNode)
     domNode = domNode.parentNode;
-  setTimeout(() => {
-    if (domNode instanceof HTMLElement) {
+  if (domNode instanceof HTMLElement) {
+    setTimeout(() => {
       domNode.scrollIntoView({ behavior: "smooth", block: "nearest" });
-    }
-  }, 0);
+    });
+  }
+ 
 }
