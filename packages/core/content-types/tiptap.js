aca3dd7a32d9f30540255967cb5721caba9e84b9
Abdullah Atta <abdullahatta@streetwriters.co>
2022-11-10 15:20:40 +0500

    core: use `html-to-text` library for better text conversion

--- packages/core/content-types/tiptap.js
+++ packages/core/content-types/tiptap.js
@@ -29,7 +29,6 @@ const splitter = /\W+/gm;
 export class Tiptap {
   constructor(data) {
     this.data = data;
-    this.text;
     this.document = parseHTML(data);
   }
 
@@ -38,7 +37,34 @@ export class Tiptap {
   }
 
   toTXT() {
-    return this.document.body.innerText;
+    return convert(this.data, {
+      wordwrap: 80,
+      preserveNewlines: true,
+      selectors: [
+        { selector: "table", format: "dataTable" },
+        { selector: "ul.checklist", format: "taskList" }
+      ],
+      formatters: {
+        taskList: (elem, walk, builder, formatOptions) => {
+          return list(elem, walk, builder, formatOptions, (elem) => {
+            return elem.attribs.class.includes("checked") ? " ✅ " : " ☐ ";
+          });
+        },
+        paragraph: (elem, walk, builder, formatOptions) => {
+          if (elem.parent && elem.parent.name === "li") {
+            walk(elem.children, builder);
+          } else {
+            builder.openBlock({
+              leadingLineBreaks: formatOptions.leadingLineBreaks || 2
+            });
+            walk(elem.children, builder);
+            builder.closeBlock({
+              trailingLineBreaks: formatOptions.trailingLineBreaks || 2
+            });
+          }
+        }
+      }
+    });
   }
 
   toMD() {
