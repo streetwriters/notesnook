4ab949b3988f700e1a0a6086b6976c3e0c12a0c7
ammarahm-ed <ammarahmed6506@gmail.com>
2023-01-25 14:59:23 +0500

    mobile: update script

--- apps/mobile/scripts/debug-dynamic-module.sh
+++ apps/mobile/scripts/debug-dynamic-module.sh
@@ -1,7 +1,8 @@
+tc() { set ${*,,} ; echo ${*^} ; }
 adb reverse tcp:8081 tcp:8081 || true
 rm -rf $(PWD)/native/android/app/build/outputs/bundle || true
-cd native/android && ./gradlew bundleDebug
+cd native/android && ./gradlew bundle$2
 cd ../../
-java -jar $HOME/bundletool/bundletool-all-1.13.2.jar build-apks --overwrite --local-testing --bundle $(PWD)/native/android/app/build/outputs/bundle/debug/app-debug.aab --output $(PWD)/native/android/app/build/outputs/bundle/debug/apkset.apks
-java -jar $HOME/bundletool/bundletool-all-1.13.2.jar install-apks --apks $(PWD)/native/android/app/build/outputs/bundle/debug/apkset.apks
+java -jar $HOME/bundletool/bundletool-all-1.13.2.jar build-apks --overwrite --local-testing --bundle $(PWD)/native/android/app/build/outputs/bundle/$1/app-$1.aab --output $(PWD)/native/android/app/build/outputs/bundle/$0/apkset.apks
+java -jar $HOME/bundletool/bundletool-all-1.13.2.jar install-apks --apks $(PWD)/native/android/app/build/outputs/bundle/$0/apkset.apks
 adb shell monkey -p com.streetwriters.notesnook 1
