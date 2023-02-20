e0b191b9a0e9454489dd0029150a54795044344d
Abdullah Atta <abdullahatta@streetwriters.co>
2023-02-20 15:21:07 +0500

    web: get rid of editor-placeholder (always show editor by default)

--- apps/web/src/navigation/hash-routes.js
+++ apps/web/src/navigation/hash-routes.js
@@ -44,12 +44,11 @@ import {
   showCreateTopicDialog
 } from "../common/dialog-controller";
 import { hashNavigate } from ".";
-import EditorPlaceholder from "../components/editor/-placeholder";
 import Editor from "../components/editor";
 
 const hashroutes = {
   "/": () => {
-    return !editorStore.get().session.state && <EditorPlaceholder />;
+    return !editorStore.get().session.state && <Editor noteId={0} nonce={-1} />;
   },
   "/email/verify": () => {
     showEmailVerificationDialog().then(afterAction);
