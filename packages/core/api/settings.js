2a0af6b95b8e9a58510aba83a2b8bda2141ef567
ammarahm-ed <ammarahmed6506@gmail.com>
2023-02-24 11:01:35 +0500

    core: add trash cleanup duration settings

--- packages/core/api/settings.js
+++ packages/core/api/settings.js
@@ -124,6 +124,18 @@ class Settings {
   getAlias(id) {
     return this._settings.aliases[id];
   }
+  /**
+   * Setting to -1 means never clear trash.
+   * @param {7 | 30 | 365 | -1} time
+   */
+  async setTrashCleanupInterval(time) {
+    this._settings.trashCleanupInterval = time;
+    await this._saveSettings();
+  }
+
+  getTrashCleanupInterval() {
+    return this._settings.trashCleanupInterval || 7;
+  }
 
   _initSettings(settings) {
     this._settings = {
@@ -134,6 +146,7 @@ class Settings {
       aliases: {},
       dateModified: 0,
       dateCreated: 0,
+      trashCleanupInterval: 7,
       ...(settings || {})
     };
   }
