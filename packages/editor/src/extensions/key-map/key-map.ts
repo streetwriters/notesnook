2bb77d9155e4b1b1a335bc595ac9b2b8b876dcb7
alihamuh <alihamuh@gmail.com>
2023-02-25 17:32:23 +0500

    editor: improve backspace handling with lists

--- packages/editor/src/extensions/key-map/key-map.ts
+++ packages/editor/src/extensions/key-map/key-map.ts
@@ -17,9 +17,13 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Extension } from "@tiptap/core";
+import { Editor, Extension } from "@tiptap/core";
 import { isInTable } from "@tiptap/pm/tables";
-import { isListActive } from "../../utils/prosemirror";
+import {
+  isListActive,
+  LIST_ITEM_NODE_TYPES,
+  LIST_NODE_TYPES
+} from "../../utils/prosemirror";
 
 export const KeyMap = Extension.create({
   name: "key-map",
@@ -33,7 +37,53 @@ export const KeyMap = Extension.create({
       "Shift-Tab": ({ editor }) => {
         if (isListActive(editor)) return false;
         return true;
+      },
+      Backspace: ({ editor }) => {
+        return joinUpWithLastListItem(editor);
       }
     };
   }
 });
+
+/**
+ * By default, in ProseMirror, if the cursor is right below a list,
+ * pressing Backspace creates a new empty list item at the end.
+ * This function corrects this behavior and moves the cursor to the
+ * end of the list.
+ */
+function joinUpWithLastListItem(editor: Editor) {
+  const { selection, doc } = editor.state;
+  const { $from, empty } = selection;
+
+  const parentPos = $from.pos - $from.parentOffset - 1;
+  const before = doc.resolve(parentPos).nodeBefore;
+
+  if (
+    empty &&
+    $from.parentOffset === 0 &&
+    before &&
+    LIST_NODE_TYPES.includes(before.type.name)
+  ) {
+    // The last list item can be at any depth and the number of joinBackwards
+    // depend on how deep it is.
+    let lastItemDepth = 0;
+    before.descendants((node, pos) => {
+      if (LIST_ITEM_NODE_TYPES.includes(node.type.name)) {
+        const { depth } = before.resolve(pos);
+        lastItemDepth = depth / 2;
+      }
+    });
+    // The depth of the last list item does not take into account
+    // the parent depth (i.e. 1) so we have to increment it.
+    lastItemDepth++;
+
+    let commands = editor.chain().joinBackward();
+    for (let i = 0; i < lastItemDepth; ++i) {
+      commands = commands.joinBackward().joinBackward();
+    }
+
+    return commands.run();
+  }
+
+  return false;
+}
