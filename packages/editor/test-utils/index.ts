092016f9423109adc209d0e11b2f559e24d8de19
Abdullah Atta <abdullahatta@streetwriters.co>
2023-02-28 12:44:11 +0500

    editor: do not build test files

--- packages/editor/test-utils/index.ts
+++ packages/editor/test-utils/index.ts
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import "@/tests.setup";
+import "./tests.setup";
 import { Editor, AnyExtension, Extensions } from "@tiptap/core";
 import StarterKit from "@tiptap/starter-kit";
 import { builders, NodeBuilder } from "prosemirror-test-builder";
