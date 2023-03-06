dfa718f8ead6914c1fabb7fce5f3479cea6644f4
Abdulrehman-Jafer <121712508+Abdulrehman-Jafer@users.noreply.github.com>
2023-03-06 14:53:35 +0500

    web: fix autocomplete text color for all inputs (#2057)
    Signed-off-by: Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>

--- apps/web/src/components/search/index.js
+++ apps/web/src/components/search/index.js
@@ -29,7 +29,7 @@ function SearchBox(props) {
       id="search"
       name="search"
       type="text"
-      sx={{ m: 0, mx: 1, mt: 1 }}
+      sx={{m: 0,mx: 1,mt: 1}}
       placeholder="Type your query here"
       onKeyDown={(e) => {
         if (e.key === "Enter") props.onSearch(e.target.value);
