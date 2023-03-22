74e8ef9debd0a3e22b2818b768565adf8b45c244
Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>
2023-03-22 10:53:57 +0500

    web: modified parse.ts
    Signed-off-by: Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>

--- apps/web/src/utils/parse.ts
+++ apps/web/src/utils/parse.ts
@@ -17,8 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-export function tryParse(val: string | null | undefined) {
-  if (!val) {
+export function tryParse(val: string) {
+  if (!val || val == "null" || val == "undefined") {
     return;
   }
   try {
