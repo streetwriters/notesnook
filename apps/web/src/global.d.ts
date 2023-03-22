966bedbb49f3eb7c288e5cdad4a926068081d192
Abdulrehman-Jafer <121712508+Abdulrehman-Jafer@users.noreply.github.com>
2023-03-22 10:42:49 +0500

    web: migrate platform.js to typescript (#2163)
    Signed-off-by: Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>

--- apps/web/src/global.d.ts
+++ apps/web/src/global.d.ts
@@ -32,6 +32,7 @@ type DesktopIntegrationSettings = {
 };
 
 declare interface Window {
+  os: typeof process.platform & "mas";
   config: {
     static spellChecker(): Promise<SpellCheckerOptions>;
     static desktopIntegration(): Promise<DesktopIntegrationSettings>;
