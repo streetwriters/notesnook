a46b86380200ec01fa07f72f638b21722d53908e
ammarahm-ed <ammarahmed6506@gmail.com>
2023-03-22 11:14:26 +0500

    core: return only attachment meta if attachment already exists

--- packages/core/collections/attachments.js
+++ packages/core/collections/attachments.js
@@ -288,7 +288,10 @@ export default class Attachments extends Collection {
   async save(data, mimeType) {
     const { hash } = await this._db.fs.hashBase64(data);
     const attachment = this.attachment(hash);
-    if (attachment) return attachment;
+    if (attachment)
+      return {
+        metadata: attachment.metadata
+      };
 
     const key = await this._db.attachments.generateKey();
     const metadata = await this._db.fs.writeEncryptedBase64(
