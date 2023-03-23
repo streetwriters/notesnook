5b3c889b7cd3c54b46bef6cabffc3522aac43610
Abdulrehman-Jafer <121712508+Abdulrehman-Jafer@users.noreply.github.com>
2023-03-23 17:03:23 +0500

    web: migrate css.js to typescript (#2165)
    Signed-off-by: Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>

--- apps/web/src/utils/css.ts
+++ apps/web/src/utils/css.ts
@@ -17,14 +17,16 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-export function removeCss(id) {
-  var link = document.getElementById(id);
-  link.remove();
+export function removeCss(id: string) {
+  const link = document.getElementById(id);
+  if(link){
+    link.remove();
+  }
 }
 
-export function injectCssSrc(id, src) {
-  var head = document.head;
-  var link = document.createElement("link");
+export function injectCssSrc(id: string, src: string) {
+  const head = document.head;
+  const link = document.createElement("link");
 
   link.id = id;
   link.type = "text/css";
@@ -34,32 +36,30 @@ export function injectCssSrc(id, src) {
   head.appendChild(link);
 }
 
-export function injectCss(rule) {
-  let variableCss = document.getElementById("variables");
-  let head = document.getElementsByTagName("head")[0];
+export function injectCss(rule: string) {
+  const variableCss = document.getElementById("variables");
+  const head = document.getElementsByTagName("head")[0];
   if (variableCss) {
     head.removeChild(variableCss);
   }
-  let css = document.createElement("style");
+  const css = document.createElement("style");
   css.type = "text/css";
   css.id = "variables";
-  // Support for IE
-  if (css.styleSheet) css.styleSheet.cssText = rule;
-  // Support for the rest
-  else css.appendChild(document.createTextNode(rule));
+  css.appendChild(document.createTextNode(rule));
   head.insertBefore(css, getRootStylesheet());
 }
 
 function getRootStylesheet() {
-  for (let sty of document.getElementsByTagName("style")) {
+  for (const sty of document.getElementsByTagName("style")) {
     if (sty.innerHTML.includes("#root")) {
       return sty;
     }
   }
+  return null;
 }
 
-export function changeSvgTheme(newAccent) {
-  var nodes = document.querySelectorAll('*[fill="#0560ff"]');
-  for (var n = 0; n < nodes.length; ++n)
+export function changeSvgTheme(newAccent: string) {
+  const nodes = document.querySelectorAll('*[fill="#0560ff"]');
+  for (let n = 0; n < nodes.length; ++n)
     nodes[n].setAttribute("fill", newAccent);
 }
