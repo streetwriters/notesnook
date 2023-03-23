c87e6092eea9d1cab514aeb5c2c7cb3cf744e5ef
Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>
2023-03-23 14:26:45 +0500

    web: removed ternary from css.ts
    Signed-off-by: Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>

--- apps/web/src/utils/css.ts
+++ apps/web/src/utils/css.ts
@@ -19,7 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 export function removeCss(id: string) {
   const link = document.getElementById(id);
-  link ? link.remove() : undefined;
+  if(link){
+    link.remove();
+  }
 }
 
 export function injectCssSrc(id: string, src: string) {
