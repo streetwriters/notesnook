8ee03adb27b1406891565cf8c08a1de5ad415d1d
Abdullah Atta <abdullahatta@streetwriters.co>
2023-05-24 16:01:23 +0500

    web: add support for previewing pdf attachments

--- apps/web/src/common/attachments.js
+++ apps/web/src/common/attachments.js
@@ -43,6 +43,29 @@ export async function downloadAttachment(hash) {
   });
 }
 
+export async function downloadAttachmentForPreview(hash) {
+  const attachment = db.attachments.attachment(hash);
+  if (!attachment) return;
+  const downloadResult = await db.fs.downloadFile(
+    attachment.metadata.hash,
+    attachment.metadata.hash,
+    attachment.chunkSize,
+    attachment.metadata
+  );
+  if (!downloadResult) throw new Error("Failed to download file.");
+
+  const key = await db.attachments.decryptKey(attachment.key);
+  if (!key) throw new Error("Invalid key for attachment.");
+
+  return await FS.decryptFile(attachment.metadata.hash, {
+    key,
+    iv: attachment.iv,
+    name: attachment.metadata.filename,
+    type: attachment.metadata.type,
+    isUploaded: !!attachment.dateUploaded
+  });
+}
+
 export async function checkAttachment(hash) {
   const attachment = db.attachments.attachment(hash);
   if (!attachment) return { failed: "Attachment not found." };
