c36d152c12ecbe4c7843a2ad3e9398ea359223cb
Ramiro Pruis <82285826+RamiroPruis@users.noreply.github.com>
2023-05-31 11:39:23 -0300

    editor: change enableSelection name property
    Signed-off-by: Ramiro Pruis<ramapruis@gmail.com>

--- packages/editor/src/extensions/react/types.ts
+++ packages/editor/src/extensions/react/types.ts
@@ -62,5 +62,5 @@ export type ReactNodeViewOptions<P> = {
   shouldUpdate?: ShouldUpdate;
   contentDOMFactory?: (() => ContentDOM) | boolean;
   wrapperFactory?: () => HTMLElement;
-  enableSelection?: boolean;
+  forceEnableSelection?: boolean;
 };
