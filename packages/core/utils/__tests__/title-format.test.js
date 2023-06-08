163dfac5d706cfc66728dc228cdc9c86b391a1a7
Abdullah Atta <abdullahatta@streetwriters.co>
2023-06-08 11:57:21 +0500

    core: add tests for title formatting

---
+++ packages/core/utils/__tests__/title-format.test.js
@@ -0,0 +1,51 @@
+/*
+This file is part of the Notesnook project (https://notesnook.com/)
+
+Copyright (C) 2023 Streetwriters (Private) Limited
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { formatTitle } from "../title-format";
+import MockDate from "mockdate";
+
+const templates = {
+  $time$: "11:25",
+  $date$: "DD-MM-YYYY",
+  $timestamp$: "DDMMYYYY1125",
+  $count$: "1",
+  $headline$: "HEADLINE"
+};
+
+const cases = [
+  ...Object.entries(templates).map(([key, value]) => {
+    return [`Note ${key}`, `Note ${value}`];
+  })
+];
+
+beforeAll(() => {
+  MockDate.set("2000-11-22 11:25");
+});
+
+afterAll(() => {
+  MockDate.reset();
+});
+
+describe("pairs should be equal", () => {
+  test.each(cases)("%s", (one, two) => {
+    expect(formatTitle(one, "[DD-MM-YYYY]", "[hh:mm]", "HEADLINE", 0)).toBe(
+      two
+    );
+  });
+});
