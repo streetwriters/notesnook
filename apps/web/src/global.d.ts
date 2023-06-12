9b9040774b0c41256072b12234adeb2db53db4d3
Abdullah Atta <abdullahatta@streetwriters.co>
2023-06-12 12:00:16 +0500

    web: fix editor theme not changing with app theme

--- apps/web/src/global.d.ts
+++ apps/web/src/global.d.ts
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import "vite/client";
 declare interface Window {
   os: NodeJS.Platform | "mas";
   NativeNNCrypto?: new () => import("@notesnook/crypto").NNCrypto;
