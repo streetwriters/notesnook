07bb35395c6e8fbf3460831ed0e7d20b30071fbd
Ammar Ahmed <40239442+ammarahm-ed@users.noreply.github.com>
2023-06-16 15:15:14 +0500

    clipper: fix duplicate notes created on save (#2722)
    * clipper: fix duplicate notes created on save
    
    * clipper(web): move singleton checks inside the fn
    
    ---------
    
    Co-authored-by: Abdullah Atta <abdullahatta@streetwriters.co>

--- extensions/web-clipper/src/content-scripts/nn.ts
+++ extensions/web-clipper/src/content-scripts/nn.ts
@@ -26,37 +26,47 @@ import {
   WEB_EXTENSION_CHANNEL_EVENTS
 } from "../common/bridge";
 
-browser.runtime.onConnect.addListener((port) => {
-  window.addEventListener("message", (ev) => {
-    const { type } = ev.data;
-    switch (type) {
-      case WEB_EXTENSION_CHANNEL_EVENTS.ON_CREATED:
-        if (ev.ports.length) {
-          const mainPort = ev.ports.at(0);
-          if (mainPort) {
-            expose(new BackgroundGateway(), mainPort);
-            const server: Remote<Server> = wrap<Server>(mainPort);
-            expose(
-              {
-                login: () => server.login(),
-                getNotes: () => server.getNotes(),
-                getNotebooks: () => server.getNotebooks(),
-                getTags: () => server.getTags(),
-                saveClip: (clip: Clip) => server.saveClip(clip)
-              },
-              createEndpoint(port)
-            );
-            port.postMessage({ success: true });
-          } else {
-            port.postMessage({ success: false });
-          }
-        }
-        break;
-    }
-  });
+declare global {
+  // eslint-disable-next-line no-var
+  var clipperBridgeConnected: boolean;
+}
 
-  window.postMessage({ type: WEB_EXTENSION_CHANNEL_EVENTS.ON_READY }, "*");
-});
+function attachOnConnectListener() {
+  if (globalThis.clipperBridgeConnected) return;
+  globalThis.clipperBridgeConnected = true;
+
+  browser.runtime.onConnect.addListener((port) => {
+    window.addEventListener("message", (ev) => {
+      const { type } = ev.data;
+      switch (type) {
+        case WEB_EXTENSION_CHANNEL_EVENTS.ON_CREATED:
+          if (ev.ports.length) {
+            const mainPort = ev.ports.at(0);
+            if (mainPort) {
+              expose(new BackgroundGateway(), mainPort);
+              const server: Remote<Server> = wrap<Server>(mainPort);
+              expose(
+                {
+                  login: () => server.login(),
+                  getNotes: () => server.getNotes(),
+                  getNotebooks: () => server.getNotebooks(),
+                  getTags: () => server.getTags(),
+                  saveClip: (clip: Clip) => server.saveClip(clip)
+                },
+                createEndpoint(port)
+              );
+              port.postMessage({ success: true });
+            } else {
+              port.postMessage({ success: false });
+            }
+          }
+          break;
+      }
+    });
+
+    window.postMessage({ type: WEB_EXTENSION_CHANNEL_EVENTS.ON_READY }, "*");
+  });
+}
 
 class BackgroundGateway implements Gateway {
   connect() {
@@ -66,3 +76,5 @@ class BackgroundGateway implements Gateway {
     };
   }
 }
+
+attachOnConnectListener();
