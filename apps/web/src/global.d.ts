87402251a552dc5a515bc7827e7a494a03aba395
Abdullah Atta <abdullahatta@streetwriters.co>
2023-06-17 11:14:00 +0500

    web: add types for svgr

--- apps/web/src/global.d.ts
+++ apps/web/src/global.d.ts
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import "vite/client";
+import "vite-plugin-svgr/client";
+
 declare global {
   interface Window {
     os?: NodeJS.Platform | "mas";
