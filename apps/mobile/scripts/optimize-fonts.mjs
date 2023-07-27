1a45c83a5f2dab87ff0099b8e2f20ec39b94918d
ammarahm-ed <ammarahmed6506@gmail.com>
2023-07-27 22:29:44 +0500

    mobile: fix icon loading

--- apps/mobile/scripts/optimize-fonts.mjs
+++ apps/mobile/scripts/optimize-fonts.mjs
@@ -18,13 +18,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import fg from "fast-glob";
-import { readFile, writeFile } from "fs/promises";
+import { Font } from "fonteditor-core";
+import { existsSync } from "fs";
+import { mkdir, readFile, writeFile } from "fs/promises";
 import Listr from "listr";
 import os from "os";
 import path from "path";
 import { fileURLToPath } from "url";
-import { existsSync } from "fs";
-import { Font } from "fonteditor-core";
+
+const EXTRA_ICON_NAMES = [
+  "menu",
+  "lock-open-outline",
+  "key-outline",
+  "pin-off-outline",
+  "pin-outline",
+  "star-off",
+  "star-outline",
+  "link-variant-remove",
+  "link-variant",
+  "bell",
+  "bell-off-outline",
+  "check",
+  "plus",
+  "magnify",
+  "plus"
+];
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -36,6 +54,14 @@ const GLYPH_MAP_PATH = path.join(
   "glyphmaps",
   "MaterialCommunityIcons.json"
 );
+const GLYPH_MAP_OLD_PATH = path.join(
+  ROOT_DIR,
+  "node_modules",
+  "react-native-vector-icons",
+  "glyphmaps",
+  "MaterialCommunityIcons.old.json"
+);
+
 const ICON_FONT_PATH = path.join(
   ROOT_DIR,
   "node_modules",
@@ -44,11 +70,21 @@ const ICON_FONT_PATH = path.join(
   "MaterialCommunityIcons.ttf"
 );
 
-if (!existsSync(GLYPH_MAP_PATH)) throw new Error("Glyph file not found.");
+if (!existsSync(GLYPH_MAP_OLD_PATH) && !existsSync(GLYPH_MAP_PATH))
+  throw new Error("Glyph file not found.");
 
-const glyphs = JSON.parse(await readFile(GLYPH_MAP_PATH, "utf-8"));
+if (!existsSync(GLYPH_MAP_OLD_PATH)) {
+  await writeFile(
+    GLYPH_MAP_OLD_PATH,
+    await readFile(GLYPH_MAP_PATH, "utf-8"),
+    "utf-8"
+  );
+}
+
+const glyphs = JSON.parse(await readFile(GLYPH_MAP_OLD_PATH, "utf-8"));
 const files = await fg("app/**/*.{js,jsx,ts,tsx}");
 const pattern = /.name="(.+?)"/gm;
+
 const glyphCodepoints = new Set();
 
 class SilentRenderer {
@@ -60,6 +96,7 @@ class SilentRenderer {
 
   end() {}
 }
+const MODIFIED_GLYPH_MAP = {};
 
 const tasks = new Listr([], {
   concurrent: os.cpus().length,
@@ -70,19 +107,38 @@ for (const filePath of files) {
     title: `Searching ${filePath}`,
     task: async () => {
       const file = await readFile(filePath, "utf-8");
-      for (const m of file.matchAll(pattern)) {
-        if (!glyphs[m[1]]) continue;
-        glyphCodepoints.add(glyphs[m[1]]);
+      const matches = [
+        ...file.matchAll(pattern),
+        ...file.matchAll(/.icon: "(.+?)"/gm)
+      ];
+      const icons = matches
+        .map((m) => m[1])
+        .filter((value) => value !== null && value !== undefined);
+      icons.push(...EXTRA_ICON_NAMES);
+
+      for (const icon of icons) {
+        if (!glyphs[icon]) continue;
+        MODIFIED_GLYPH_MAP[icon] = glyphs[icon];
+        glyphCodepoints.add(glyphs[icon]);
       }
     }
   });
 }
-await tasks.run();
 
+await tasks.run();
 const font = Font.create(await readFile(ICON_FONT_PATH), {
   type: "ttf",
   subset: Array.from(glyphCodepoints.values()),
   hinting: true
 });
 
-await writeFile("out.ttf", font.write({ type: "ttf", hinting: true }));
+if (!existsSync(path.join(ROOT_DIR, "native", "fonts"))) {
+  await mkdir(path.join(ROOT_DIR, "native", "fonts"));
+}
+
+await writeFile(
+  path.join(ROOT_DIR, "native", "fonts", "MaterialCommunityIcons.ttf"),
+  font.write({ type: "ttf", hinting: true })
+);
+
+await writeFile(GLYPH_MAP_PATH, JSON.stringify(MODIFIED_GLYPH_MAP), "utf-8");
