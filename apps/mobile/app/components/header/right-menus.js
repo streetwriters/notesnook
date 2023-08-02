75b5b67e31d79a81399aac1f4979b6655ef32966
ammarahm-ed <ammarahmed6506@gmail.com>
2023-08-02 13:52:14 +0500

    mobile: fix crash

--- apps/mobile/app/components/header/right-menus.js
+++ apps/mobile/app/components/header/right-menus.js
@@ -33,7 +33,7 @@ import { IconButton } from "../ui/icon-button";
 
 export const RightMenus = () => {
   const { colors } = useThemeColors();
-  const { contextMenuColors } = useThemeColors("contextMenu");
+  const { colors: contextMenuColors } = useThemeColors("contextMenu");
   const deviceMode = useSettingStore((state) => state.deviceMode);
   const rightButtons = useNavigationStore((state) => state.headerRightButtons);
   const currentScreen = useNavigationStore((state) => state.currentScreen.name);
