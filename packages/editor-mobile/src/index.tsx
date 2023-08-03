0e7ac37b9db74c6b1a5bb69771def991775cb578
ammarahm-ed <ammarahmed6506@gmail.com>
2023-08-03 11:18:07 +0500

    mobile: remove prism.css

--- packages/editor-mobile/src/index.tsx
+++ packages/editor-mobile/src/index.tsx
@@ -22,7 +22,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "@notesnook/editor/styles/katex.min.css";
-import "@notesnook/editor/styles/prism-theme.css";
 import "@notesnook/editor/styles/fonts.mobile.css";
 import "@notesnook/editor/styles/katex-fonts.mobile.css";
 import "@notesnook/editor/styles/styles.css";
