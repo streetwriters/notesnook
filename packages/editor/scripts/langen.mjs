f51f45aeb23dc6892d2ba9fdb183199ad8a9a399
Abdullah Atta <abdullahatta@streetwriters.co>
2023-08-12 20:38:47 +0500

    editor: move isomorphic-fetch import

--- packages/editor/scripts/langen.mjs
+++ packages/editor/scripts/langen.mjs
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Quick & dirty script to download & transform all language details from
  * the PrismJS repo.
  */
-import "isomorphic-fetch";
+
 import fs from "fs";
 import path from "path";
 
