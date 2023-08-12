ca9288acfe068b4ceeeda87a02f3b826a15140f6
Abdullah Atta <abdullahatta@streetwriters.co>
2023-08-12 20:35:36 +0500

    core: use isomorphic-fetch

--- packages/core/globals.setup.js
+++ packages/core/globals.setup.js
@@ -17,11 +17,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import "isomorphic-fetch";
 import crypto from "crypto";
 import dotenv from "dotenv";
-import fetch from "node-fetch";
 
-globalThis.fetch = fetch;
 require("abortcontroller-polyfill/dist/polyfill-patch-fetch");
 globalThis.crypto = crypto;
 dotenv.config();
