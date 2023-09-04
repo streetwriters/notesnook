caf76e9b850bfe54891c7dbd081624a3a0fe4533
Muhammad Ali <alihamuh@gmail.com>
2023-09-04 14:21:23 +0500

    editor: fix formatting issues when pasting from ChatGPT (#3217)
    Co-authored-by: Abdullah Atta <abdullahatta@streetwriters.co>

--- packages/editor/src/extensions/clipboard/tests/clipboard-dom-parser.test.ts
+++ packages/editor/src/extensions/clipboard/tests/clipboard-dom-parser.test.ts
@@ -18,7 +18,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { test } from "vitest";
-import { convertBrToSingleSpacedParagraphs } from "../clipboard-dom-parser";
+import {
+  formatCodeblocks,
+  convertBrToSingleSpacedParagraphs
+} from "../clipboard-dom-parser";
 
 const cases = [
   [`<p>line 1<br>line 2</p>`],
@@ -67,9 +70,40 @@ and yet, we somehow manage to get by.<br>
 
 for (const testCase of cases) {
   const [html, expected] = testCase;
-  test(`convert br tags to paragraphs (${testCase})`, (t) => {
+  test(`convert br tags to paragraphs`, (t) => {
     const element = new DOMParser().parseFromString(html, "text/html");
     convertBrToSingleSpacedParagraphs(element);
+    t.expect(element.body.innerHTML.trim()).toMatchSnapshot();
+  });
+}
+
+const codeBlocks = [
+  `<div>
+  <!--StartFragment--><p>Sure! Here's an implementation of a word counter for Thai that considers each syllable consisting of a consonant sound followed by a vowel sound as a word:</p><pre><div class="bg-black rounded-md mb-4"><div class="flex items-center relative text-gray-200 bg-gray-800 px-4 py-2 text-xs font-sans justify-between rounded-t-md"><span>javascript</span></div></div></pre><pre><div class="bg-black rounded-md mb-4"><div class="p-4 overflow-y-auto"><code class="!whitespace-pre hljs language-javascript"><span class="hljs-keyword">function</span> <span class="hljs-title function_">countThaiWords</span>(<span class="hljs-params">text</span>) {
+    <span class="hljs-comment">// define a regular expression to match Thai syllables</span>
+    <span class="hljs-keyword">const</span> thaiSyllableRegex = <span class="hljs-regexp">/[\u0E01-\u0E3A\u0E40-\u0E5B]+[\u0E00-\u0E7F]?/g</span>;
+  
+    <span class="hljs-comment">// count the number of matches of the Thai syllable regex in the text</span>
+    <span class="hljs-keyword">const</span> matches = text.<span class="hljs-title function_">match</span>(thaiSyllableRegex) || [];
+    <span class="hljs-keyword">const</span> wordCount = matches.<span class="hljs-property">length</span>;
+  
+    <span class="hljs-comment">// return the number of words</span>
+    <span class="hljs-keyword">return</span> wordCount;
+  }
+  
+  <span class="hljs-comment">// example usage</span>
+  <span class="hljs-keyword">const</span> text = <span class="hljs-string">"สวัสดีค่ะยินดีต้อนรับเข้าสู่โลกของฉัน"</span>;
+  <span class="hljs-keyword">const</span> wordCount = <span class="hljs-title function_">countThaiWords</span>(text);
+  <span class="hljs-variable language_">console</span>.<span class="hljs-title function_">log</span>(<span class="hljs-string">\`Word count: <span class="hljs-subst">\${wordCount}</span>\`</span>);
+  <span class="hljs-comment">// output: Word count: 9</span>
+  </code></div></div></pre><p>This implementation defines a regular expression to match Thai syllables, which are composed of one or more Thai characters. It then uses the <code>match</code> function of the <code>String</code> object to count the number of matches of the Thai syllable regex in the input text.</p><p>Note that this implementation assumes that each syllable consisting of a consonant sound followed by a vowel sound is considered a word in Thai. This may not always be accurate, as some words in Thai may consist of multiple syllables, and some syllables may be used as prefixes or suffixes to modify other words. However, this approach should work well for many cases where each syllable is considered a separate word.</p><!--EndFragment-->
+  
+  </div>`
+];
+for (const codeBlock of codeBlocks) {
+  test(`properly format codeblocks`, (t) => {
+    const element = new DOMParser().parseFromString(codeBlock, "text/html");
+    formatCodeblocks(element);
     t.expect(element.body.innerHTML.trim()).toMatchSnapshot();
   });
 }
