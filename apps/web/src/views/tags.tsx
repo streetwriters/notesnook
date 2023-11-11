4b7a6f3a6385e246a88bb62f2cdb03e48fb489fd
Abdullah Atta <abdullahatta@streetwriters.co>
2023-11-11 13:05:03 +0500

    web: implement virtualized search for all items

--- apps/web/src/views/tags.tsx
+++ apps/web/src/views/tags.tsx
@@ -21,18 +21,21 @@ import ListContainer from "../components/list-container";
 import { useStore, store } from "../stores/tag-store";
 import useNavigate from "../hooks/use-navigate";
 import Placeholder from "../components/placeholders";
+import { useSearch } from "../hooks/use-search";
+import { db } from "../common/db";
 
 function Tags() {
   useNavigate("tags", () => store.refresh());
   const tags = useStore((store) => store.tags);
   const refresh = useStore((store) => store.refresh);
+  const filteredItems = useSearch("tags", (query) => db.lookup.tags(query));
 
   if (!tags) return <Placeholder context="tags" />;
   return (
     <ListContainer
       group="tags"
       refresh={refresh}
-      items={tags}
+      items={filteredItems || tags}
       placeholder={<Placeholder context="tags" />}
     />
   );
