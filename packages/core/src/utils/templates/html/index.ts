1a14d37c72e03852ca910519fe07cde4a572d629
Abdullah Atta <abdullahatta@streetwriters.co>
2023-11-21 09:04:57 +0500

    core: fix build error

--- packages/core/src/utils/templates/html/index.ts
+++ packages/core/src/utils/templates/html/index.ts
@@ -116,7 +116,8 @@ async function preprocessHTML(templateData: TemplateData) {
 function hasRequire() {
   return (
     typeof require === "function" &&
-    // eslint-disable-next-line no-undef
+    // eslint-disable-next-line no-undef, @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     (typeof IS_DESKTOP_APP === "undefined" || !IS_DESKTOP_APP)
   );
 }
