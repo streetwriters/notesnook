576fe78e33f0cc3e6b9311917fd386cf1988f782
Muhammad Ali <alihamuh@gmail.com>
2023-11-21 08:18:28 +0500

    clipper: hide clip preview button when there's no clip data (#3772)

--- extensions/web-clipper/src/views/main.tsx
+++ extensions/web-clipper/src/views/main.tsx
@@ -306,7 +306,7 @@ export function Main() {
           </Button>
         ))}
 
-        {clipData && !isClipping && (
+        {clipData && clipData.data && !isClipping && (
           <Text
             variant="body"
             sx={{
@@ -322,7 +322,6 @@ export function Main() {
               }
             }}
             onClick={async () => {
-              if (!clipData) return;
               const winUrl = URL.createObjectURL(
                 new Blob(["\ufeff", clipData.data], { type: "text/html" })
               );
