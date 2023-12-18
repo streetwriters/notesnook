1efd717502562c182dd8dc3fc9425fbb6168c59d
Abdullah Atta <abdullahatta@streetwriters.co>
2023-12-18 16:20:52 +0500

    editor: refactoring

--- packages/editor/src/extensions/task-list/component.tsx
+++ packages/editor/src/extensions/task-list/component.tsx
@@ -18,36 +18,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { Box, Flex, Input, Text } from "@theme-ui/components";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { ToolButton } from "../../toolbar/components/tool-button";
 import { ReactNodeViewProps } from "../react";
-import { type TaskListAttributes } from "./task-list";
+import { toggleChildren, type TaskListAttributes } from "./task-list";
 import { replaceDateTime } from "../date-time";
 import { deleteCheckedItems, sortList } from "./utils";
 import TaskList from "@tiptap/extension-task-list";
 import TaskItem from "@tiptap/extension-task-item";
+import { useIsMobile } from "../../toolbar/stores/toolbar-store";
+import { Icons } from "../../toolbar/icons";
+import { Icon } from "@notesnook/ui";
 
 export function TaskListComponent(
   props: ReactNodeViewProps<TaskListAttributes>
 ) {
   const { editor, getPos, node, updateAttributes, forwardRef, pos } = props;
   const { title, textDirection, readonly, stats } = node.attrs;
-  const [selectAll, setSelectAll] = useState(false);
+  const isMobile = useIsMobile();
+  const checked = stats.total === stats.checked;
 
   const isNested = useMemo(() => {
     if (!pos) return false;
     return editor.state.doc.resolve(pos).parent.type.name === TaskItem.name;
   }, [editor.state.doc, pos]);
-
-  useEffect(() => {
-    let checked = true;
-
-    node?.forEach((_node) => {
-      if (!_node.attrs.checked) checked = false;
-    });
-    if (checked) setSelectAll(true);
-    else setSelectAll(false);
-  }, [node]);
 
   return (
     <>
@@ -79,6 +73,41 @@ export function TaskListComponent(
               transition: "width 250ms ease-out"
             }}
           />
+          {editor.isEditable ? (
+            <Icon
+              path={checked ? Icons.check : ""}
+              stroke="1px"
+              contentEditable={false}
+              tabIndex={1}
+              sx={{
+                border: "2px solid",
+                borderColor: checked ? "accent" : "icon",
+                borderRadius: "default",
+                p: "1px",
+                zIndex: 1,
+                // mt: isMobile ? "0.20ch" : "0.36ch",
+                marginInlineStart: 1,
+                cursor: editor.isEditable ? "pointer" : "unset",
+                ":hover": isMobile
+                  ? undefined
+                  : {
+                      borderColor: "accent"
+                    },
+                fontFamily: "inherit"
+              }}
+              onClick={() => {
+                const parentPos = getPos();
+                editor.current?.commands.command(({ tr }) => {
+                  const node = tr.doc.nodeAt(parentPos);
+                  if (!node) return false;
+                  toggleChildren(tr, node, !checked, parentPos);
+                  return true;
+                });
+              }}
+              color={checked ? "accent" : "icon"}
+              size={isMobile ? "1.70ch" : "1.46ch"}
+            />
+          ) : null}
           <Input
             readOnly={!editor.isEditable || readonly}
             value={title || ""}
@@ -86,7 +115,7 @@ export function TaskListComponent(
             sx={{
               flex: 1,
               p: 0,
-              px: 2,
+              px: 1,
               zIndex: 1,
               color: "var(--paragraph-secondary)",
               fontSize: "inherit",
@@ -107,36 +136,6 @@ export function TaskListComponent(
           />
           {editor.isEditable && (
             <>
-              <ToolButton
-                toggled={false}
-                title={`${selectAll ? "Uncheck" : "Check"} all taskitems`}
-                icon={selectAll ? "selectAllChecked" : "selectAllUnchecked"}
-                variant="small"
-                sx={{
-                  zIndex: 1
-                }}
-                onClick={() => {
-                  const parentPos = getPos();
-                  editor.current?.commands.command(({ tr }) => {
-                    const node = tr.doc.nodeAt(parentPos);
-                    if (!node) return false;
-                    node.forEach((_node, offset) => {
-                      if (selectAll) {
-                        tr.setNodeMarkup(parentPos + offset + 1, null, {
-                          checked: false
-                        });
-                        setSelectAll(false);
-                      } else {
-                        tr.setNodeMarkup(parentPos + offset + 1, null, {
-                          checked: true
-                        });
-                        setSelectAll(true);
-                      }
-                    });
-                    return true;
-                  });
-                }}
-              />
               <ToolButton
                 toggled={false}
                 title="Make tasklist readonly"
@@ -213,7 +212,7 @@ export function TaskListComponent(
             variant={"body"}
             sx={{
               ml: 1,
-              mr: 2,
+              mr: 1,
               color: "var(--paragraph-secondary)",
               flexShrink: 0,
               zIndex: 1,
