90cc89cd6e0095368f3ccf5e58ff10b0e3ad4f5c
Abdullah Atta <abdullahatta@streetwriters.co>
2023-12-26 16:02:27 +0500

    core: remove unnecessary console.log

--- packages/core/src/database/backup.ts
+++ packages/core/src/database/backup.ts
@@ -342,7 +342,6 @@ export default class Backup {
   }
 
   private async migrateData(data: BackupDataItem[], version: number) {
-    console.log(data);
     await this.db.transaction(async () => {
       for (let item of data) {
         // we do not want to restore deleted items
