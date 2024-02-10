e1e850f49340553ff6e96084a8dd1dc916ef3047
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-02-11 00:02:00 +0500

    mobile: fix share extension

--- apps/mobile/share/index.js
+++ apps/mobile/share/index.js
@@ -17,11 +17,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import { ScopedThemeProvider } from "@notesnook/theme";
 import React, { Fragment, useEffect, useState } from "react";
 import { Modal, Platform } from "react-native";
 import ShareView from "./share";
-import { ScopedThemeProvider } from "@notesnook/theme";
 import "./store";
+
 const Wrapper = Platform.OS === "android" ? Modal : Fragment;
 const outerProps =
   Platform.OS === "android"
