17f68b53152427fd9600eda1a5e386509e692703
Abdullah Atta <abdullahatta@streetwriters.co>
2024-02-26 20:29:23 +0500

    desktop: disable os encryption of keystore in portable app

--- apps/desktop/src/api/safe-storage.ts
+++ apps/desktop/src/api/safe-storage.ts
@@ -23,16 +23,22 @@ import { z } from "zod";
 const t = initTRPC.create();
 
 export const safeStorageRouter = t.router({
+  isEncryptionAvailable: t.procedure.query(() => {
+    return (
+      !process.env.PORTABLE_EXECUTABLE_DIR &&
+      safeStorage.isEncryptionAvailable()
+    );
+  }),
   /**
    * Takes a string and returns an encrypted base64 string
    */
-  encryptString: t.procedure.input(z.string()).query(async ({ input }) => {
+  encryptString: t.procedure.input(z.string()).query(({ input }) => {
     return safeStorage.encryptString(input).toString("base64");
   }),
   /**
    * Takes an encrypted base64 string and returns a string
    */
-  decryptString: t.procedure.input(z.string()).query(async ({ input }) => {
+  decryptString: t.procedure.input(z.string()).query(({ input }) => {
     return safeStorage.decryptString(Buffer.from(input, "base64"));
   })
 });
