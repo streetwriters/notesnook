cf5fac7d41260034d176f8fa0073e02f7b40fb47
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-03-16 08:03:36 +0500

    mobile: fix incorrect tab content by default

--- packages/editor-mobile/src/components/editor.tsx
+++ packages/editor-mobile/src/components/editor.tsx
@@ -134,7 +134,7 @@ const Tiptap = ({
       editorProps: {
         editable: () => !tab.readonly
       },
-      content: "<p>hello world<ins class='diffins'>[diffed text]</ins></p>",
+      content: globalThis.editorControllers[tab.id]?.content?.current,
       isMobile: true,
       doubleSpacedLines: settings.doubleSpacedLines,
       openLink: (url) => {
