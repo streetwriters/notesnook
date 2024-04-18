8dc4f7ed3df76b109fcbe6403b224b6deef0a5bd
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-04-18 15:28:43 +0500

    mobile: remove v3 beta notice on login page

--- apps/mobile/app/components/auth/login.js
+++ apps/mobile/app/components/auth/login.js
@@ -296,11 +296,6 @@ export const Login = ({ changeMode }) => {
                 </Paragraph>
               </TouchableOpacity>
             ) : null}
-
-            <Notice
-              type="alert"
-              text="After logging in from V3 beta, your account data will be migrated and you won't be able to use V2 clients to sync your notes."
-            />
           </View>
         </View>
       </View>
