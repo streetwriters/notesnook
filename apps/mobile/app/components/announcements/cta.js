d3c1c1959bd86a1b6657310d920653a4f112130c
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-05-09 15:14:39 +0500

    mobile: fix wording of push/pull sync

--- apps/mobile/app/components/announcements/cta.js
+++ apps/mobile/app/components/announcements/cta.js
@@ -20,15 +20,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import React from "react";
 import { Linking, View } from "react-native";
 //import SettingsBackupAndRestore from '../../screens/settings/backup-restore';
-import { eSendEvent, presentSheet } from "../../services/event-manager";
-import Sync from "../../services/sync";
 import { useThemeColors } from "@notesnook/theme";
-import { eCloseAnnouncementDialog, eCloseSheet } from "../../utils/events";
+import { eSendEvent, presentSheet } from "../../services/event-manager";
+import { eCloseAnnouncementDialog } from "../../utils/events";
 import { SIZE } from "../../utils/size";
 import { sleep } from "../../utils/time";
 import { PricingPlans } from "../premium/pricing-plans";
 import SheetProvider from "../sheet-provider";
-import { Progress } from "../sheets/progress";
 import { Button } from "../ui/button";
 import { allowedOnPlatform, getStyle } from "./functions";
 
@@ -55,13 +53,6 @@ export const Cta = ({ actions, style = {}, color, inline }) => {
             }}
           />
         )
-      });
-    } else if (item.type === "force-sync") {
-      eSendEvent(eCloseSheet);
-      await sleep(300);
-      Progress.present();
-      Sync.run("global", true, "full", () => {
-        eSendEvent(eCloseSheet);
       });
     }
   };
