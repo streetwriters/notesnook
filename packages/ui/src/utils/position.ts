bd0aaed863cc44d7e4381fbae936d651de7c14e4
Abdullah Atta <abdullahatta@streetwriters.co>
2024-07-19 23:22:47 +0500

    ui: add support for ssr

--- packages/ui/src/utils/position.ts
+++ packages/ui/src/utils/position.ts
@@ -27,13 +27,7 @@ type PositionData = {
 };
 
 const mousePosition: PositionData = { x: 0, y: 0, actualX: 0, actualY: 0 };
-window.addEventListener("mousemove", (e) => {
-  const { x, y, actualX, actualY } = getMousePosition(e);
-  mousePosition.x = x;
-  mousePosition.y = y;
-  mousePosition.actualX = actualX;
-  mousePosition.actualY = actualY;
-});
+attachMouseMoveListener();
 
 export type PositionOptions = {
   target?: HTMLElement | "mouse" | null;
@@ -49,6 +43,8 @@ export function getPosition(
   node: Node,
   options: PositionOptions
 ): { top: number; left: number } {
+  attachMouseMoveListener();
+
   let element: HTMLElement;
   if (node instanceof HTMLElement) {
     element = node;
@@ -179,4 +175,17 @@ export function getElementPosition(
   }
 
   return position;
+}
+
+let mouseMoveListenerAttached = false;
+function attachMouseMoveListener() {
+  if (mouseMoveListenerAttached || !("window" in globalThis)) return;
+  window.addEventListener("mousemove", (e) => {
+    const { x, y, actualX, actualY } = getMousePosition(e);
+    mousePosition.x = x;
+    mousePosition.y = y;
+    mousePosition.actualX = actualX;
+    mousePosition.actualY = actualY;
+  });
+  mouseMoveListenerAttached = true;
 }
