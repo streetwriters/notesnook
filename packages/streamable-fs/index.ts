e83993843239ca69ff5ec50ffb52471562fa0791
Abdullah Atta <abdullahatta@streetwriters.co>
2024-07-24 12:35:41 +0500

    fs: get rid of chunks property in file metadata

--- packages/streamable-fs/index.ts
+++ packages/streamable-fs/index.ts
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import FileHandle from "./src/filehandle";
 import { IFileStorage, IStreamableFS } from "./src/interfaces";
 import { File } from "./src/types";
+import { chunkPrefix } from "./src/utils";
 
 export class StreamableFS implements IStreamableFS {
   /**
@@ -37,17 +38,19 @@ export class StreamableFS implements IStreamableFS {
     const file: File = {
       filename,
       size,
-      type,
-      chunks: 0
+      type
     };
     await this.storage.setMetadata(filename, file);
-    return new FileHandle(this.storage, file);
+    return new FileHandle(this.storage, file, []);
   }
 
   async readFile(filename: string): Promise<FileHandle | undefined> {
     const file = await this.storage.getMetadata(filename);
     if (!file) return undefined;
-    return new FileHandle(this.storage, file);
+    const chunks = (await this.storage.listChunks(chunkPrefix(filename))).sort(
+      (a, b) => a.localeCompare(b, undefined, { numeric: true })
+    );
+    return new FileHandle(this.storage, file, chunks);
   }
 
   async exists(filename: string): Promise<boolean> {
