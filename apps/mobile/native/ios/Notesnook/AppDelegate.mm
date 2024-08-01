4030cbe500bfdf19421f905e139dab9f10f60d00
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-08-01 15:52:27 +0500

    mobile: upgrade to react native 0.74.4

--- apps/mobile/native/ios/Notesnook/AppDelegate.mm
+++ apps/mobile/native/ios/Notesnook/AppDelegate.mm
@@ -19,9 +19,16 @@
 }
 
 
+
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-#if DEBUG
+  return [self bundleURL];
+}
+ 
+- (NSURL *)bundleURL
+{
+  #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
