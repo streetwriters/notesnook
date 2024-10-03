ab141033435e703bf99647567bc7043e473b2860
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-10-03 10:13:24 +0500

    common: fix missing exports

--- packages/common/src/utils/index.ts
+++ packages/common/src/utils/index.ts
@@ -27,3 +27,5 @@ export * from "./resolve-items.js";
 export * from "./migrate-toolbar.js";
 export * from "./export-notes.js";
 export * from "./dataurl.js";
+export * from "./tab-history.js";
+export * from "./editor-sessions.js";
