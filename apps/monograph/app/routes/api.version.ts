0d4d13b9aa1af95a8a52fc750bfb04c08f88d916
Abdullah Atta <abdullahatta@streetwriters.co>
2024-10-14 12:26:16 +0500

    monograph: add /api/version endpoint for self hosting support

--- apps/monograph/app/routes/api.version.ts
+++ apps/monograph/app/routes/api.version.ts
@@ -17,13 +17,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-export const API_HOST =
-  import.meta.env.API_HOST ||
-  process.env.API_HOST ||
-  `https://api.notesnook.com`;
-export const PUBLIC_URL =
-  import.meta.env.PUBLIC_URL ||
-  process.env.PUBLIC_URL ||
-  `http://localhost:${import.meta.env.PORT || process.env.PORT || 5017}`;
-export const COMPATIBILITY_VERSION = 1;
-export const INSTANCE_NAME = process.env.INSTANCE_NAME || "default";
+import { COMPATIBILITY_VERSION, INSTANCE_NAME } from "../utils/env";
+
+export async function loader() {
+  return Response.json({
+    version: COMPATIBILITY_VERSION,
+    id: "monograph",
+    instance: INSTANCE_NAME
+  });
+}
