2b98b5d5f2ab34020fee74c59a2153b0232cda04
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-10-16 16:12:18 +0500

    mobile: load async scripts with script manager

--- apps/mobile/native/globals.js
+++ apps/mobile/native/globals.js
@@ -5,4 +5,21 @@ import "./polyfills/console-time.js"
 global.Buffer = require('buffer').Buffer;
 import '../app/common/logger/index';
 import { DOMParser } from './worker.js';
+import { ScriptManager, Script } from '@callstack/repack/client';
 global.DOMParser = DOMParser;
+
+ScriptManager.shared.addResolver(async (scriptId) => {
+    // `scriptId` will be either 'student' or 'teacher'
+  
+    // In dev mode, resolve script location to dev server.
+    if (__DEV__) {
+      return {
+        url: Script.getDevServerURL(scriptId),
+        cache: false,
+      };
+    }
+  
+    return {
+      url: Script.getFileSystemURL(scriptId)
+    };
+  });
