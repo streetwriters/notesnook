4d7bfb1be47f3dd7a44bbaf00fa8998d3cf929a3
Luis Kriner <luis@kriner.info>
2024-11-23 11:05:50 +0100

    editor: prevent creation of multiple notes in editor-store
    Signed-off-by: Luis Kriner <luis@kriner.info>

--- apps/web/src/stores/editor-store.ts
+++ apps/web/src/stores/editor-store.ts
@@ -871,12 +871,20 @@ class EditorStore extends BaseStore<EditorStore> {
   };
 
   newSession = () => {
-    this.addSession({
-      type: "new",
-      id: getId(),
-      context: useNoteStore.getState().context,
-      saveState: SaveState.NotSaved
-    });
+    const state = useEditorStore.getState();
+    if (state.sessions.some((session) => session.type === "new")) {
+      const invalidSession = state.sessions.find(
+        (session) => session.type === "new"
+      );
+      this.activateSession(invalidSession?.id);
+    } else {
+      this.addSession({
+        type: "new",
+        id: getId(),
+        context: useNoteStore.getState().context,
+        saveState: SaveState.NotSaved
+      });
+    }
   };
 
   closeSessions = (...ids: string[]) => {
