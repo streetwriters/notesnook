064a6b3916bd93de8ff7f160f3de7376d0c0f7d6
01zulfi <85733202+01zulfi@users.noreply.github.com>
2024-12-11 09:55:56 +0500

    editor: add code block language to local storage * also add plaintext language option * also fix spacing issues in language selector list
    Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>

--- packages/editor/src/extensions/code-block/component.tsx
+++ packages/editor/src/extensions/code-block/component.tsx
@@ -30,6 +30,7 @@ import { CodeBlockAttributes } from "./code-block.js";
 import Languages from "./languages.json";
 import { useThemeEngineStore } from "@notesnook/theme";
 import { strings } from "@notesnook/intl";
+import { config } from "../../utils/config.js";
 
 export function CodeblockComponent(
   props: ReactNodeViewProps<CodeBlockAttributes>
@@ -48,6 +49,9 @@ export function CodeblockComponent(
   const languageDefinition = Languages.find(
     (l) => l.filename === language || l.alias?.some((a) => a === language)
   );
+
+  const cachedLanguage =
+    config.get<(typeof Languages)[number]>("codeBlockLanguage");
 
   return (
     <>
@@ -155,7 +159,9 @@ export function CodeblockComponent(
             title={strings.changeLanguage()}
           >
             <Text variant={"subBody"} spellCheck={false}>
-              {languageDefinition?.title || "Plaintext"}
+              {languageDefinition?.title ||
+                cachedLanguage?.title ||
+                "Plaintext"}
             </Text>
           </Button>
 
@@ -213,8 +219,16 @@ export function CodeblockComponent(
         title={strings.selectLanguage()}
       >
         <LanguageSelector
-          selectedLanguage={languageDefinition?.filename || "Plaintext"}
+          selectedLanguage={
+            languageDefinition?.filename ||
+            cachedLanguage?.filename ||
+            "Plaintext"
+          }
           onLanguageSelected={(language) => {
+            config.set(
+              "codeBlockLanguage",
+              Languages.find((l) => l.filename === language)
+            );
             updateAttributes(
               { language },
               { addToHistory: true, preventUpdate: false }
@@ -287,7 +301,6 @@ function LanguageSelector(props: LanguageSelectorProps) {
               variant={"menuitem"}
               sx={{
                 textAlign: "left",
-                py: 1,
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "center"
