59474dcd5c032b5d227ec33cf667046de212adc9
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-12-27 21:12:54 +0500

    mobile: resizable and themeable android widget

--- apps/mobile/native/android/app/src/main/java/com/streetwriters/notesnook/NoteWidget.java
+++ apps/mobile/native/android/app/src/main/java/com/streetwriters/notesnook/NoteWidget.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
 /**
@@ -25,10 +26,28 @@ public class NoteWidget extends AppWidgetProvider {
     }
 
     @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+        updateAppWidget(context, appWidgetManager, appWidgetId, newOptions);
+    }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle options) {
+        int minWidth = options != null ? options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH) : 0;
+//        int minHeight = options != null ? options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT) : 0;
+
+        int layoutId = (minWidth < 100) ? R.layout.note_widget_icon : R.layout.note_widget;
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context, appWidgetManager, appWidgetId,null);
         }
     }
 
