9c7ff38f99af9a9f3a1fc252658788069e12658f
Ammar Ahmed <40239442+ammarahm-ed@users.noreply.github.com>
2025-03-06 15:46:03 +0500

    editor: fix image padding in readonly mode (#7743)
    * editor: fix image padding in readonly mode
    
    * editor: fix class name

--- packages/editor/src/extensions/image/component.tsx
+++ packages/editor/src/extensions/image/component.tsx
@@ -105,6 +105,7 @@ export function ImageComponent(
   return (
     <>
       <Box
+        className="image-container"
         sx={{
           ...getAlignmentStyles(node.attrs),
           height: float ? size.height : "unset",
