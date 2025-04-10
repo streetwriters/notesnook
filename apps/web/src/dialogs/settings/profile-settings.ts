d3ea51cc01610182441d8a2e94f8a1fd67d4d91d
01zulfi <85733202+01zulfi@users.noreply.github.com>
2025-04-10 10:56:05 +0500

    web: add delete data option for not logged in user Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>

--- apps/web/src/dialogs/settings/profile-settings.ts
+++ apps/web/src/dialogs/settings/profile-settings.ts
@@ -35,6 +35,7 @@ import { EmailChangeDialog } from "../email-change-dialog";
 import { RecoveryKeyDialog } from "../recovery-key-dialog";
 import { UserProfile } from "./components/user-profile";
 import { SettingsGroup } from "./types";
+import Config from "../../utils/config";
 
 export const ProfileSettings: SettingsGroup[] = [
   {
@@ -86,6 +87,29 @@ export const ProfileSettings: SettingsGroup[] = [
             variant: "secondary",
             action: async () => {
               if (await verifyAccount()) await RecoveryKeyDialog.show({});
+            }
+          }
+        ]
+      },
+      {
+        key: "delete-data-for-not-logged-in-user",
+        title: strings.deleteData(),
+        description: strings.deleteAccountDesc(),
+        keywords: [
+          strings.deleteData(),
+          strings.deleteAccount(),
+          strings.clear()
+        ],
+        isHidden: () => Boolean(useUserStore.getState().isLoggedIn),
+        components: [
+          {
+            type: "button",
+            variant: "error",
+            title: strings.deleteData(),
+            action: async () => {
+              Config.clear();
+              await db.reset();
+              window.location.reload();
             }
           }
         ]
