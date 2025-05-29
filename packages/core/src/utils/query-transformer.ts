0effa01ef76bb6365f28bc9cf6c42dcb7726f779
Ammar Ahmed <ammarahmed6506@gmail.com>
2025-05-29 11:09:47 +0500

    core: fix stop-words import

--- packages/core/src/utils/query-transformer.ts
+++ packages/core/src/utils/query-transformer.ts
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { EN_STOPWORDS } from "./stop-words";
+import { EN_STOPWORDS } from "./stop-words.js";
 
 type ASTNode = QueryNode | PhraseNode | OperatorNode;
 
