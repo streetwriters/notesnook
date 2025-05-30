9c2fa4eb306d9b6f8ff552fb36b373ccc1b87cf9
Abdullah Atta <abdullahatta@streetwriters.co>
2025-05-30 19:25:00 +0500

    core: use intl for showing search results count

--- packages/core/src/api/lookup.ts
+++ packages/core/src/api/lookup.ts
@@ -41,6 +41,7 @@ import { fuzzy } from "../utils/fuzzy.js";
 import { extractMatchingBlocks } from "../utils/html-parser.js";
 import { findOrAdd } from "../utils/array.js";
 import { Parser } from "htmlparser2";
+import { strings } from "@notesnook/intl";
 
 type SearchResults<T> = {
   sorted: (sortOptions?: SortOptions) => Promise<VirtualizedGrouping<T>>;
@@ -277,7 +278,21 @@ export default class Lookup {
           ids: results.map((c) => c.id),
           items: results
         };
-      }
+      },
+      () =>
+        new Map([
+          [
+            0,
+            {
+              index: 0,
+              group: {
+                id: "0",
+                title: strings.results(matches.ids.length),
+                type: "header"
+              }
+            }
+          ]
+        ])
     );
   }
 
