fc1cb190147e9425b5f030bc0654ee7156be51dd
Abdullah Atta <abdullahatta@streetwriters.co>
2025-05-30 19:24:19 +0500

    web: use plain list instead of tree for showing search results

--- apps/web/src/components/search-result/index.tsx
+++ apps/web/src/components/search-result/index.tsx
@@ -19,157 +19,210 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { HighlightedResult } from "@notesnook/core";
 import { Button, Flex, Text } from "@theme-ui/components";
-import React from "react";
+import React, { useState } from "react";
 import { useEditorStore } from "../../stores/editor-store";
 
 import ListItem from "../list-item";
 import { ChevronDown, ChevronRight } from "../icons";
 import { noteMenuItems } from "../note";
 import { db } from "../../common/db";
+import { MenuItem } from "@notesnook/ui";
 
 type SearchResultProps = {
   item: HighlightedResult;
   matchIndex?: number;
-  depth: number;
-  isExpandable: boolean;
-  isExpanded: boolean;
-  collapse: () => void;
-  expand: () => void;
 };
 
 function SearchResult(props: SearchResultProps) {
-  const { item, matchIndex, collapse, expand, isExpandable, isExpanded } =
-    props;
+  const { item, matchIndex } = props;
 
   const isOpened = useEditorStore((store) => store.isNoteOpen(item.id));
   const match = matchIndex !== undefined ? item.content[matchIndex] : undefined;
+  const isExpandable = !match && item.content.length > 0;
+  const [isExpanded, setIsExpanded] = useState(true);
 
+  if (!item.title.length && !item.content.length) return null;
   return (
-    <ListItem
-      isFocused={match ? false : isOpened}
-      isCompact={!match}
-      item={item}
-      menuItems={async (item, ids) => {
-        const note = await db.notes.note(item.id);
-        if (!note) return [];
-
-        const colors = await db.relations
-          .to({ type: "note", id: item.id }, "color")
-          .resolve();
-        return noteMenuItems(note, ids, {
-          locked: !!(
-            await db
-              .sql()
-              .selectFrom("content")
-              .where("noteId", "in", [note.id])
-              .select(["noteId", "locked"])
-              .executeTakeFirst()
-          )?.locked,
-          color: colors[0]
-        });
-      }}
-      onClick={() => {
-        useEditorStore.getState().openSession(item.id, {
-          rawContent: item.rawContent,
-          force: true,
-          activeSearchResultIndex: findSelectedMatchIndex(item, matchIndex || 0)
-        });
-      }}
-      onMiddleClick={() => {
-        useEditorStore.getState().openSession(item.id, {
-          openInNewTab: true,
-          rawContent: item.rawContent,
-          activeSearchResultIndex: findSelectedMatchIndex(item, matchIndex || 0)
-        });
-      }}
-      title={
-        <Flex sx={{ alignItems: "center", gap: "small" }}>
-          {isExpandable ? (
-            <Button
-              variant="secondary"
-              sx={{ bg: "transparent", p: 0, borderRadius: 100 }}
-              onClick={(e) => {
-                e.stopPropagation();
-                isExpanded ? collapse() : expand();
+    <Flex sx={{ flexDirection: "column" }}>
+      <ListItem
+        isFocused={isOpened}
+        item={item}
+        menuItems={menuItems}
+        onClick={() => {
+          useEditorStore.getState().openSession(item.id, {
+            rawContent: item.rawContent,
+            force: true
+          });
+        }}
+        onMiddleClick={() => {
+          useEditorStore.getState().openSession(item.id, {
+            openInNewTab: true,
+            rawContent: item.rawContent
+          });
+        }}
+        title={
+          <Flex sx={{ alignItems: "center", gap: "small" }}>
+            {isExpandable ? (
+              <Button
+                variant="secondary"
+                sx={{
+                  bg: "transparent",
+                  p: 0,
+                  borderRadius: 100,
+                  flexShrink: 0
+                }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsExpanded((s) => !s);
+                }}
+              >
+                {isExpanded ? (
+                  <ChevronDown
+                    size={14}
+                    color={isOpened ? "icon-selected" : "icon"}
+                  />
+                ) : (
+                  <ChevronRight
+                    size={14}
+                    color={isOpened ? "icon-selected" : "icon"}
+                  />
+                )}
+              </Button>
+            ) : null}
+            <Flex
+              sx={{
+                alignItems: "center",
+                justifyContent: "space-between",
+                flex: 1
               }}
             >
-              {isExpanded ? (
-                <ChevronDown
-                  size={14}
-                  color={isOpened ? "icon-selected" : "icon"}
-                />
-              ) : (
-                <ChevronRight
-                  size={14}
-                  color={isOpened ? "icon-selected" : "icon"}
-                />
-              )}
-            </Button>
-          ) : null}
-          <Text
-            data-test-id={`title`}
-            variant={"body"}
-            color={isOpened ? "paragraph-selected" : "paragraph"}
-            sx={{
-              ...(match
-                ? {
-                    whiteSpace: "pre-wrap"
+              <Text
+                data-test-id={`title`}
+                variant={"body"}
+                color={isOpened ? "paragraph-selected" : "paragraph"}
+                sx={{
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  fontWeight: "medium",
+                  display: "block",
+                  ".match": {
+                    bg: "accent-secondary",
+                    color: "accentForeground-secondary"
                   }
-                : {
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis"
-                  }),
-              fontWeight: match ? "body" : "medium",
-              display: "block",
-              ".match": {
-                bg: "accent-secondary",
-                color: "accentForeground-secondary"
-              }
-            }}
-          >
-            {match
-              ? match.map((match) => (
-                  <>
-                    <span>{match.prefix}</span>
-                    <span className="match">{match.match}</span>
-                    {match.suffix ? <span>{match.suffix}</span> : null}
-                  </>
-                ))
-              : item.title.map((match) => (
+                }}
+              >
+                {item.title.map((match) => (
                   <>
                     <span>{match.prefix}</span>
                     <span className="match">{match.match}</span>
                     {match.suffix ? <span>{match.suffix}</span> : null}
                   </>
                 ))}
-          </Text>
-        </Flex>
-      }
-      footer={
-        match ? undefined : (
-          <Text variant="subBody">
-            {item.content?.reduce((count, next) => next.length + count, 0) || 0}
-          </Text>
-        )
-      }
-      sx={{
-        ...(match
-          ? {
-              px: 1,
-              borderBottom: "1px solid var(--border)"
-            }
-          : {
-              my: "small",
-              borderRadius: "default"
-            }),
-        mx: 1
-      }}
-    />
+              </Text>
+
+              <Text variant="subBody">
+                {item.content?.reduce(
+                  (count, next) => next.length + count,
+                  0
+                ) || 0}
+              </Text>
+            </Flex>
+          </Flex>
+        }
+        sx={{
+          my: "small",
+          borderRadius: "default",
+          mx: 1
+        }}
+      />
+      {isExpanded
+        ? item.content.map((match, index) => (
+            <ListItem
+              key={`${item.id}${index}`}
+              isFocused={false}
+              item={item}
+              menuItems={menuItems}
+              onClick={() => {
+                useEditorStore.getState().openSession(item.id, {
+                  rawContent: item.rawContent,
+                  force: true,
+                  activeSearchResultIndex: findSelectedMatchIndex(
+                    item,
+                    matchIndex || 0
+                  )
+                });
+              }}
+              onMiddleClick={() => {
+                useEditorStore.getState().openSession(item.id, {
+                  openInNewTab: true,
+                  rawContent: item.rawContent,
+                  activeSearchResultIndex: findSelectedMatchIndex(
+                    item,
+                    matchIndex || 0
+                  )
+                });
+              }}
+              title={
+                <Text
+                  data-test-id={`title`}
+                  variant={"body"}
+                  color={isOpened ? "paragraph-selected" : "paragraph"}
+                  sx={{
+                    whiteSpace: "pre-wrap",
+                    fontWeight: "body",
+                    display: "block",
+                    ".match": {
+                      bg: "accent-secondary",
+                      color: "accentForeground-secondary"
+                    }
+                  }}
+                >
+                  {match.map((match) => (
+                    <>
+                      <span>{match.prefix}</span>
+                      <span className="match">{match.match}</span>
+                      {match.suffix ? <span>{match.suffix}</span> : null}
+                    </>
+                  ))}
+                </Text>
+              }
+              sx={{
+                px: 1,
+                borderBottom: "1px solid var(--border)",
+                mx: 1
+              }}
+            />
+          ))
+        : null}
+    </Flex>
   );
 }
 
 export default React.memo(SearchResult);
+
+async function menuItems(
+  item: HighlightedResult,
+  ids?: string[]
+): Promise<MenuItem[]> {
+  const note = await db.notes.note(item.id);
+  if (!note) return [];
+
+  const colors = await db.relations
+    .to({ type: "note", id: item.id }, "color")
+    .resolve();
+  return noteMenuItems(note, ids, {
+    locked: !!(
+      await db
+        .sql()
+        .selectFrom("content")
+        .where("noteId", "in", [note.id])
+        .select(["noteId", "locked"])
+        .executeTakeFirst()
+    )?.locked,
+    color: colors[0]
+  });
+}
 
 function findSelectedMatchIndex(item: HighlightedResult, matchIndex: number) {
   let activeIndex = 0;
