542a6284357a1c84e7b9006c9af2ead19179fa75
01zulfi <85733202+01zulfi@users.noreply.github.com>
2025-06-05 11:44:04 +0500

    web: add move notebook dialog (#8099)
    Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>
    Co-authored-by: Abdullah Atta <abdullahatta@streetwriters.co>

--- apps/web/src/dialogs/add-tags-dialog.tsx
+++ apps/web/src/dialogs/add-tags-dialog.tsx
@@ -19,37 +19,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { useEffect, useState } from "react";
 import { Flex, Text } from "@theme-ui/components";
-import {
-  CheckCircleOutline,
-  CheckRemove,
-  CircleEmpty
-} from "../components/icons";
 import { db } from "../common/db";
 import Dialog from "../components/dialog";
 import { useStore, store } from "../stores/tag-store";
 import { store as notestore } from "../stores/note-store";
 import { FilteredList } from "../components/filtered-list";
 import { ItemReference, Tag } from "@notesnook/core";
-import { create } from "zustand";
 import { VirtualizedGrouping } from "@notesnook/core";
 import { ResolvedItem } from "@notesnook/common";
 import { BaseDialogProps, DialogManager } from "../common/dialog-manager";
 import { strings } from "@notesnook/intl";
-
-type SelectedReference = {
-  id: string;
-  new: boolean;
-  op: "add" | "remove";
-};
-
-interface ISelectionStore {
-  selected: SelectedReference[];
-  setSelected(refs: SelectedReference[]): void;
-}
-export const useSelectionStore = create<ISelectionStore>((set) => ({
-  selected: [],
-  setSelected: (selected) => set({ selected: selected.slice() })
-}));
+import {
+  SelectedCheck,
+  SelectedReference,
+  selectMultiple,
+  useSelectionStore
+} from "./move-note-dialog";
 
 type AddTagsDialogProps = BaseDialogProps<boolean> & { noteIds: string[] };
 export const AddTagsDialog = DialogManager.register(function AddTagsDialog(
@@ -91,8 +76,9 @@ export const AddTagsDialog = DialogManager.register(function AddTagsDialog(
       positiveButton={{
         text: strings.done(),
         onClick: async () => {
+          const { selected } = useSelectionStore.getState();
           for (const id of noteIds) {
-            for (const item of useSelectionStore.getState().selected) {
+            for (const item of selected) {
               const tagRef: ItemReference = { type: "tag", id: item.id };
               const noteRef: ItemReference = { id, type: "note" };
               if (item.op === "add") await db.relations.add(tagRef, noteRef);
@@ -166,47 +152,15 @@ function TagItem(props: { tag: Tag }) {
       }}
       onClick={() => {
         const { selected, setSelected } = useSelectionStore.getState();
-
-        const copy = selected.slice();
-        const index = copy.findIndex((item) => item.id === tag.id);
-        const isNew = copy[index] && copy[index].new;
-        if (isNew) {
-          copy.splice(index, 1);
-        } else if (index > -1) {
-          copy[index] = {
-            ...copy[index],
-            op: copy[index].op === "add" ? "remove" : "add"
-          };
-        } else {
-          copy.push({ id: tag.id, new: true, op: "add" });
-        }
-        setSelected(copy);
+        setSelected(selectMultiple(tag, selected));
       }}
     >
       <Flex sx={{ alignItems: "center" }}>
-        <SelectedCheck size={20} id={tag.id} />
-        <Text
-          className="title"
-          data-test-id="notebook-title"
-          variant="subtitle"
-          sx={{ fontWeight: "body", color: "paragraph" }}
-        >
+        <SelectedCheck size={18} item={tag} />
+        <Text className="title" data-test-id="tag-title" variant="body">
           #{tag.title}
         </Text>
       </Flex>
     </Flex>
-  );
-}
-
-function SelectedCheck({ id, size = 20 }: { id: string; size?: number }) {
-  const selected = useSelectionStore((store) => store.selected);
-  const selectedTag = selected.find((item) => item.id === id);
-
-  return selectedTag?.op === "add" ? (
-    <CheckCircleOutline size={size} sx={{ mr: 1 }} color="accent" />
-  ) : selectedTag?.op === "remove" ? (
-    <CheckRemove size={size} sx={{ mr: 1 }} color="icon-error" />
-  ) : (
-    <CircleEmpty size={size} sx={{ mr: 1, opacity: 0.4 }} />
   );
 }
