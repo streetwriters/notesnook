a19a28fb1058290d851f206c0da36e52287eada9
Abdullah Atta <abdullahatta@streetwriters.co>
2025-07-14 12:35:04 +0500

    common: add is feature available utility

---
+++ apps/web/src/common/toasts.ts
@@ -0,0 +1,34 @@
+/*
+This file is part of the Notesnook project (https://notesnook.com/)
+
+Copyright (C) 2023 Streetwriters (Private) Limited
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { FeatureId, FeatureResult } from "@notesnook/common";
+import { showToast } from "../utils/toast";
+import { BuyDialog } from "../dialogs/buy-dialog";
+
+export function showFeatureNotAllowedToast<TId extends FeatureId>(
+  result: FeatureResult<TId> | undefined
+) {
+  if (!result) return;
+  showToast("error", result.error, [
+    {
+      text: "Upgrade",
+      onClick: () => BuyDialog.show({ plan: result.availableOn })
+    }
+  ]);
+}
