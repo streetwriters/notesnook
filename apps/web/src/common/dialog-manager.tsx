31bb78bfca5369790054860f1bb1985fbd38fa44
Abdullah Atta <abdullahatta@streetwriters.co>
2025-08-04 11:54:38 +0500

    web: add `onBeforeOpen` hook to dialog manager

--- apps/web/src/common/dialog-manager.tsx
+++ apps/web/src/common/dialog-manager.tsx
@@ -24,6 +24,17 @@ export interface BaseDialogProps<T = unknown> {
   onClose: (result: T) => void;
 }
 
+type PropsWithoutOnClose<Props extends BaseDialogProps<any>> = Omit<
+  Props,
+  "onClose"
+>;
+
+type DialogOptions<Props extends BaseDialogProps<any>> = {
+  onBeforeOpen?: (
+    props: PropsWithoutOnClose<Props>
+  ) => boolean | Promise<boolean>;
+};
+
 class _DialogManager {
   private openedDialogs: Map<React.JSXElementConstructor<any>, () => void> =
     new Map();
@@ -83,10 +94,15 @@ class _DialogManager {
   }
 
   register<Props extends BaseDialogProps<any>>(
-    component: React.ComponentType<Props>
+    component: React.ComponentType<Props>,
+    options?: DialogOptions<Props>
   ) {
     return {
-      show: (props: Omit<Props, "onClose">) => this.open(component, props),
+      show: async (props: PropsWithoutOnClose<Props>) => {
+        if (options?.onBeforeOpen && !(await options?.onBeforeOpen?.(props)))
+          return false;
+        return await this.open(component, props);
+      },
       close: () => {
         const dialog = this.openedDialogs.get(component);
         if (!dialog) return;
