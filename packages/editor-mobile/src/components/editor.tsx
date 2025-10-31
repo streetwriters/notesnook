0dfc1dfa52863677e6a7c1d08f8ac64583d49461
Ammar Ahmed <ammarahmed6506@gmail.com>
2025-10-31 20:02:43 +0500

    mobile: release v3.3.7

--- packages/editor-mobile/src/components/editor.tsx
+++ packages/editor-mobile/src/components/editor.tsx
@@ -582,7 +582,8 @@ const Tiptap = ({
                   alignItems: "center",
                   padding: "0px 16px",
                   paddingBottom: "3px",
-                  boxSizing: "border-box"
+                  boxSizing: "border-box",
+                  minHeight: "28px"
                 }}
               >
                 <StatusBar
