c1579a0c4b96d69007069b11b161df8c7ba8d319
01zulfi <85733202+01zulfi@users.noreply.github.com>
2025-11-25 11:45:00 +0500

    core: support monograph.publishUrl field Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>

--- packages/core/src/database/migrations.ts
+++ packages/core/src/database/migrations.ts
@@ -404,6 +404,14 @@ export class NNMigrationProvider implements MigrationProvider {
             .addColumn("password", "text")
             .execute();
         }
+      },
+      "a-2025-11-26": {
+        async up(db) {
+          await db.schema
+            .alterTable("monographs")
+            .addColumn("publishUrl", "text", COLLATE_NOCASE)
+            .execute();
+        }
       }
     };
   }
