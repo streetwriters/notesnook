756bb82fb7838a998530393b917d42ab72adee9c
Abdullah Atta <abdullahatta@streetwriters.co>
2026-01-03 14:36:41 +0500

    server(themes): sync themes every 1 hour (roughly)

--- servers/themes/src/server.ts
+++ servers/themes/src/server.ts
@@ -32,6 +32,9 @@ server.listen(PORT, HOST);
 console.log(`Server started successfully on: http://${HOST}:${PORT}/`);
 
 syncThemes();
+setInterval(() => {
+  syncThemes();
+}, 1000 * 60 * 60); // every hour
 
 if (import.meta.hot) {
   import.meta.hot.on("vite:beforeFullReload", () => {
