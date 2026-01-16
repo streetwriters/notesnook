fa4f4e7bea58eb62b68ae0d84a119c86de98b13d
01zulfi <85733202+01zulfi@users.noreply.github.com>
2026-01-16 19:24:21 +0500

    web: add command palette button in status bar (#7977)
    Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>

--- apps/web/src/components/icons/index.tsx
+++ apps/web/src/components/icons/index.tsx
@@ -227,7 +227,8 @@ import {
   mdiArrowCollapseRight,
   mdiNoteEditOutline,
   mdiArrowUp,
-  mdiInbox
+  mdiInbox,
+  mdiConsoleLine
 } from "@mdi/js";
 import { useTheme } from "@emotion/react";
 import { Theme } from "@notesnook/theme";
@@ -452,6 +453,7 @@ export const ImageDownload = createIcon(mdiImage);
 export const Billboard = createIcon(mdiBillboard);
 export const Cellphone = createIcon(mdiCellphone);
 export const CellphoneLock = createIcon(mdiCellphoneLock);
+export const ConsoleLine = createIcon(mdiConsoleLine);
 export const FileLock = createIcon(mdiFileLockOutline);
 export const ShieldLock = createIcon(mdiShieldLockOutline);
 export const ImageMultiple = createIcon(mdiImageMultipleOutline);
