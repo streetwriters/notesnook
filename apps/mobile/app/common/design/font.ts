6ee2e777c28042c21823c7f7c60d0682349c66e5
Ammar Ahmed <ammarahmed6506@gmail.com>
2026-04-15 10:49:58 +0500

    mobile: add style guide baseline

---
+++ apps/mobile/app/common/design/font.ts
@@ -0,0 +1,24 @@
+export const FontSizes = {
+  XXS: 10,
+  XS: 12,
+  SM: 14,
+  MD: 16,
+  LG: 18,
+  XL: 20,
+  XXL: 32
+};
+
+export const FontFamily = {
+  REGULAR: "Inter-Regular", // 400
+  MEDIUM: "Inter-Medium", // 500
+  SEMI_BOLD: "Inter-SemiBold", // 600
+  BOLD: "Inter-Bold" // 700
+};
+
+export const getLineHeight = (
+  fontSize: keyof typeof FontSizes,
+  type: 1 | 2
+) => {
+  if (type === 1) return FontSizes[fontSize];
+  if (type === 2) return (FontSizes[fontSize] / 100) * 150;
+};
