86f15334a77e4780532d0b6f1a886c7029c379ae
Ammar Ahmed <ammarahmed6506@gmail.com>
2026-04-15 10:49:58 +0500

    mobile: add style guide baseline

---
+++ apps/mobile/app/common/design/spacing.ts
@@ -0,0 +1,21 @@
+export const Spacing = {
+  LEVEL_0: 4,
+  LEVEL_1: 8,
+  LEVEL_2: 12,
+  LEVEL_3: 16,
+  LEVEL_4: 20,
+  LEVEL_5: 24,
+  LEVEL_6: 28,
+  LEVEL_7: 32,
+  LEVEL_8: 40
+};
+
+export const Radius = {
+  XXS: 4,
+  XS: 8,
+  S: 12,
+  MD: 16,
+  LG: 20,
+  LG_2: 32,
+  XXL: 40
+};
