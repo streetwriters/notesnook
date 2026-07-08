e636b71ef214bdf550c5cfbac281be2f381d5bc3
01zulfi <85733202+01zulfi@users.noreply.github.com>
2026-07-08 10:24:08 +0500

    ui: rewrap @theme-ui's button component (#10063)
    Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -21,3 +21,4 @@ export * from "./icon/index.js";
 export * from "./menu/index.js";
 export * from "./popup-presenter/index.js";
 export * from "./scroll-container/index.js";
+export * from "./button.js";
