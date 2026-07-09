ec2fb30bfee9570a199095ab03efdedf075b461e
01zulfi <85733202+01zulfi@users.noreply.github.com>
2026-07-09 12:53:36 +0500

    ui: rewrap @theme-ui's remaining component (#10073)
    Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>

--- packages/ui/src/components/textarea.tsx
+++ packages/ui/src/components/textarea.tsx
@@ -17,22 +17,31 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-export * from "./icon/index.js";
-export * from "./menu/index.js";
-export * from "./popup-presenter/index.js";
-export * from "./scroll-container/index.js";
-export * from "./button.js";
-export * from "./box.js";
-export * from "./flex.js";
-export * from "./slider.js";
-export * from "./text.js";
-export * from "./label.js";
-export * from "./textarea.js";
-export * from "./input.js";
-export * from "./image.js";
-export * from "./switch.js";
-export * from "./checkbox.js";
-export * from "./link.js";
-export * from "./grid.js";
-export * from "./progress.js";
-export * from "./radio.js";
+import React from "react";
+import {
+  Textarea as ThemeUITextarea,
+  TextareaProps as ThemeUITextareaProps
+} from "@theme-ui/components";
+import { Theme } from "@notesnook/theme";
+import {
+  RestrictedColorProps,
+  RestrictedSpaceProps,
+  RestrictedSxProp
+} from "../utils/types.js";
+
+export interface TextareaProps
+  extends Omit<
+      ThemeUITextareaProps,
+      "variant" | "sx" | keyof RestrictedSpaceProps | keyof RestrictedColorProps
+    >,
+    RestrictedSpaceProps,
+    RestrictedColorProps {
+  variant?: keyof Theme["forms"];
+  sx?: RestrictedSxProp;
+}
+
+export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (props, ref) => {
+    return <ThemeUITextarea ref={ref} {...(props as any)} />;
+  }
+);
