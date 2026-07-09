a7b464971d53041bd9e70ee5723b8f8e45e47ee8
01zulfi <85733202+01zulfi@users.noreply.github.com>
2026-07-09 11:28:50 +0500

    ui: rewrap @theme-ui's remaining component Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>

--- packages/ui/src/components/text.tsx
+++ packages/ui/src/components/text.tsx
@@ -17,22 +17,31 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-export * from "./icon/index.js";
-export * from "./menu/index.js";
-export * from "./popup-presenter/index.js";
-export * from "./scroll-container/index.js";
-export * from "./button.js";
-export * from "./box.js";
-export * from "./flex.js";
-export * from "./slider.js";
-export * from "./text.js";
-export * from "./label.js";
-export * from "./textarea.js";
-export * from "./input.js";
-export * from "./image.js";
-export * from "./switch.js";
-export * from "./checkbox.js";
-export * from "./link.js";
-export * from "./grid.js";
-export * from "./progress.js";
-export * from "./radio.js";
+import React from "react";
+import {
+  Text as ThemeUIText,
+  TextProps as ThemeUITextProps
+} from "@theme-ui/components";
+import { Theme } from "@notesnook/theme";
+import {
+  RestrictedColorProps,
+  RestrictedSpaceProps,
+  RestrictedSxProp
+} from "../utils/types.js";
+
+export interface TextProps
+  extends Omit<
+      ThemeUITextProps,
+      "variant" | "sx" | keyof RestrictedSpaceProps | keyof RestrictedColorProps
+    >,
+    RestrictedSpaceProps,
+    RestrictedColorProps {
+  variant?: keyof Theme["text"];
+  sx?: RestrictedSxProp;
+}
+
+export const Text = React.forwardRef<HTMLDivElement, TextProps>(
+  (props, ref) => {
+    return <ThemeUIText ref={ref} {...(props as any)} />;
+  }
+);
