06ef592d108672094b3d28b85c01b2842d2c885d
kashaf-ansari-dev <kashafansari3108@gmail.com>
2026-08-19 17:54:24 +0500

    mobile: ui-fixes
    Signed-off-by: kashaf-ansari-dev <kashafansari3108@gmail.com>

--- apps/mobile/app/components/ui/sheet/index.jsx
+++ apps/mobile/app/components/ui/sheet/index.jsx
@@ -160,7 +160,7 @@ const SheetWrapper = ({
         {bottomPadding ? (
           <View
             style={{
-              height: 10
+              height: Platform.OS === "android" ? bottomInsets : 10
             }}
           />
         ) : null}
