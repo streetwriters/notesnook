5a179a4dfa37685394c968723628ec99d4f1456b
Ammar Ahmed <ammarahmed6506@gmail.com>
2026-08-25 14:20:18 +0500

    mobile: fix dateFormat taking timeFormat value

--- apps/mobile/app/stores/use-setting-store.ts
+++ apps/mobile/app/stores/use-setting-store.ts
@@ -264,7 +264,7 @@ export const useSettingStore = create<SettingStore>((set, get) => ({
     set({
       dayFormat: db.settings.getDayFormat(),
       timeFormat: db.settings.getTimeFormat(),
-      dateFormat: db.settings?.getTimeFormat(),
+      dateFormat: db.settings?.getDateFormat(),
       weekFormat: db.settings.getWeekFormat(),
       vaultLockAfter: db.settings.getVaultLockAfter(),
       inboxEnabled: await db.user.hasInboxKeys()
