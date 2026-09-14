5bc72428c8168be07f8494843bba13e76c3c2d7c
kashaf-ansari-dev <kashafansari3108@gmail.com>
2026-09-14 13:47:37 +0500

    intl: share i18n instance to avoid duplicate instances
    Signed-off-by: kashaf-ansari-dev <kashafansari3108@gmail.com>

--- packages/intl/src/index.ts
+++ packages/intl/src/index.ts
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 export type { Messages } from "@lingui/core";
 export { strings } from "./strings";
-export { setI18nGlobal } from "./setup";
+export { i18n, setI18nGlobal } from "./setup";
 export { AVAILABLE_LANGUAGES, getSupportedLocale } from "./languages";
 export type { Language } from "./languages";
 export {
