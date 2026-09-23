18f2356573e628f6db55541106309fa2f0d306f7
kashaf-ansari-dev <kashafansari3108@gmail.com>
2026-09-23 13:14:12 +0500

    editor: localize downloader error
    Signed-off-by: kashaf-ansari-dev <kashafansari3108@gmail.com>

--- packages/editor/src/toolbar/popups/image-upload.tsx
+++ packages/editor/src/toolbar/popups/image-upload.tsx
@@ -89,7 +89,7 @@ export function ImageUploadPopup(props: ImageUploadPopupProps) {
               borderRadius: "default"
             }}
           >
-            {strings.failedToDownloadImage(error.toLowerCase())}
+            {strings.failedToDownloadImage(error)}
           </Text>
         ) : (
           <Text
--- packages/editor/src/utils/downloader.ts
+++ packages/editor/src/utils/downloader.ts
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { DataURL } from "@notesnook/common";
+import { strings } from "@notesnook/intl";
 
 export type DownloadOptions = {
   corsHost?: string;
@@ -77,7 +78,8 @@ export async function downloadImage(url: string, options?: DownloadOptions) {
     cache: "force-cache",
     signal: options?.signal
   });
-  if (!response.ok) throw new Error(`invalid status code ${response.status}`);
+  if (!response.ok)
+    throw new Error(strings.invalidStatusCode(response.status));
 
   let contentType = response.headers.get("Content-Type");
 
