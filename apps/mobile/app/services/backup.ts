a988fa132bfb5aa9ef0efc3c84cb04a7ea36b3d7
kashaf-ansari-dev <kashafansari3108@gmail.com>
2026-09-23 12:38:45 +0500

    mobile: backup modal
    Signed-off-by: kashaf-ansari-dev <kashafansari3108@gmail.com>

--- apps/mobile/app/services/backup.ts
+++ apps/mobile/app/services/backup.ts
@@ -248,7 +248,7 @@ async function run(
     })) {
       if (file.type === "file") {
         updateProgress({
-          progress: `Writing backup chunk of size... ${file?.data?.length}`
+          progress: strings.writingBackupChunk(file?.data?.length || 0)
         });
         await RNFetchBlob.fs.writeFile(
           `${zipSourceFolder}/${file.path}`,
@@ -257,7 +257,7 @@ async function run(
         );
       } else if (file.type === "attachment") {
         updateProgress({
-          progress: `Saving attachments in backup... ${file.hash}`
+          progress: strings.savingAttachmentsInBackup(file.hash)
         });
         if (await FileStorage.exists(file.hash)) {
           await RNFetchBlob.fs
@@ -404,7 +404,9 @@ const checkAndRun = async () => {
   if (await checkBackupRequired(settings?.reminder)) {
     try {
       await run();
-    } catch (e) {}
+    } catch (e) {
+      /* empty */
+    }
   }
 };
 
